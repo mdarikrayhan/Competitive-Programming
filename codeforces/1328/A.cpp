876ad2ad3adf1c86acdbec499d34289f560bdc92
Md. Arik Rayhan <mdarikrayhan@gmail.com>
2022-01-05 21:26:00 +0600

    Add solution for problem `A - Divisibility Problem` Link: https://codeforces.com/contest/1328/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1328/submission/141748276

--- codeforces/1328/A.cpp
+++ codeforces/1328/A.cpp
@@ -1,34 +1,31 @@
+/***************************************************/
+/*https://codeforces.com/problemset/problem/1328/A  */
+/*            Author : Md. Arik Rayhan             */
+/*        Github : github.com/mdarikrayhan         */
+/***************************************************/
 #include <bits/stdc++.h>
 using namespace std;
-const char nl = '\n';
-void solve()
-{
-    int a, b;
-    cin >> a >> b;
-    if (a % b == 0)
-    {
-        cout << 0 << nl;
-    }
-    else if ((a % b) > b)
-    {
-
-        cout << a % b - b << nl;
-    }
-    else
-    {
-        cout << b - a % b << nl;
-    }
-}
-
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    int T = 1;
-    cin >> T;
-    while (T--)
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
     {
-        solve();
+
+        int a, b;
+        cin >> a >> b;
+        if (a % b == 0)
+        {
+            cout << 0 << endl;
+        }
+        else if ((a % b) > b)
+        {
+
+            cout << a % b - b << endl;
+        }
+        else
+        {
+            cout << b - a % b << endl;
+        }
     }
-    return 0;
 }
