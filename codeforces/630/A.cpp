506dd53254acb26f28b6dc558394a780f3cf94e7
Md. Arik Rayhan <mdarikrayhan@gmail.com>
2022-01-09 08:13:00 +0600

    Add solution for problem `A - Again Twenty Five!` Link: https://codeforces.com/contest/630/problem/A Tags: number theory, *800 Ref: https://codeforces.com/contest/630/submission/142041631

---
+++ codeforces/630/A.cpp
@@ -0,0 +1,12 @@
+/***************************************************/
+/*https://codeforces.com/problemset/problem/630/A  */
+/*            Author : Md. Arik Rayhan             */
+/*        Github : github.com/mdarikrayhan         */
+/***************************************************/
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    cout<<25;
+}
