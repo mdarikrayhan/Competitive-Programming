39e23b1cf76ddd3348aff6b9693df7ecc38a9d75
Md. Arik Rayhan <mdarikrayhan@gmail.com>
2022-01-10 20:51:00 +0600

    Add solution for problem `A - Plus One on the Subset` Link: https://codeforces.com/contest/1624/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1624/submission/142211092

--- codeforces/1624/A.cpp
+++ codeforces/1624/A.cpp
@@ -1,28 +1,24 @@
+/***************************************************/
+/*https://codeforces.com/problemset/problem/1624/A */
+/*            Author : Md. Arik Rayhan             */
+/*        Github : github.com/mdarikrayhan         */
+/***************************************************/
 #include <bits/stdc++.h>
 using namespace std;
-const char nl = '\n';
-void solve()
-{
-    int n;
-    cin >> n;
-    long long int arr[100];
-    for (int i = 0; i < n; i++)
-    {
-        cin >> arr[i];
-    }
-    sort(arr, arr + n);
-    cout << arr[n - 1] - arr[0] << "\n";
-}
-
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    int T = 1;
-    cin >> T;
-    while (T--)
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
     {
-        solve();
+        int n;
+        cin >> n;
+        long long int arr[100];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+        sort(arr, arr + n);
+        cout << arr[n - 1] - arr[0] << "\n";
     }
-    return 0;
 }
