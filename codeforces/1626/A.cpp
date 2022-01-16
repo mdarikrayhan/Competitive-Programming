d0947744cd9b3b552b5e9c40487a492827b54603
Md. Arik Rayhan <mdarikrayhan@gmail.com>
2022-01-16 21:07:00 +0600

    Add solution for problem `A - Equidistant Letters` Link: https://codeforces.com/contest/1626/problem/A Tags: constructive algorithms, sortings, *800 Ref: https://codeforces.com/contest/1626/submission/142995559

---
+++ codeforces/1626/A.cpp
@@ -0,0 +1,17 @@
+/***************************************************/
+/*https://codeforces.com/problemset/problem/1626/A */
+/*            Author : Md. Arik Rayhan             */
+/*        Github : github.com/mdarikrayhan         */
+/***************************************************/
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        string s;
+        cin>>s;
+        sort(s.begin(),s.end());
+        cout<<s<<endl;
+    }
+}
