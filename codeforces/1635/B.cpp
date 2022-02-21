9d8ec4ed6087aa1a8123ffeb48fc1bef0fa8e94b
Md. Arik Rayhan <mdarikrayhan@gmail.com>
2022-02-21 11:37:00 +0600

    Add solution for problem `B - Avoid Local Maximums` Link: https://codeforces.com/contest/1635/problem/B Tags: greedy, *800 Ref: https://codeforces.com/contest/1635/submission/147140831

---
+++ codeforces/1635/B.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef complex<ld> cd;
+
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef pair<ld, ld> pd;
+
+typedef vector<int> vi;
+typedef vector<ld> vd;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<cd> vcd;
+
+template <class T>
+using pq = priority_queue<T>;
+template <class T>
+using pqg = priority_queue<T, vector<T>, greater<T>>;
+
+#define FOR(i, a, b) for (int i = a; i < (b); i++)
+#define F0R(i, a) for (int i = 0; i < (a); i++)
+#define FORd(i, a, b) for (int i = (b)-1; i >= a; i--)
+#define F0Rd(i, a) for (int i = (a)-1; i >= 0; i--)
+#define trav(a, x) for (auto &a : x)
+#define uid(a, b) uniform_int_distribution<int>(a, b)(rng)
+
+#define sz(x) (int)(x).size()
+#define mp make_pair
+#define pb push_back
+#define f first
+#define s second
+#define lb lower_bound
+#define ub upper_bound
+#define all(x) x.begin(), x.end()
+#define ins insert
+const char nl = '\n';
+void solve()
+{
+    int n;
+    cin >> n;
+    int arr[n + 10];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    int cnt = 0;
+    for (int i = 1; i < n - 1; i++)
+    {
+        if (arr[i] > arr[i - 1] && arr[i] > arr[i + 1])
+        {
+            cnt++;
+            if (i == n - 2)
+            {
+                arr[i + 1] = arr[i];
+            }
+            else
+                arr[i + 1] = max(arr[i], arr[i + 2]);
+        }
+    }
+    cout << cnt << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int T = 1;
+    cin >> T;
+    while (T--)
+    {
+        solve();
+    }
+    return 0;
+}
