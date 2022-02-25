89c342980dc7812c15372e1d61da716421a35a28
Md. Arik Rayhan <mdarikrayhan@gmail.com>
2022-02-25 09:28:00 +0600

    Add solution for problem `A - The New Year: Meeting Friends` Link: https://codeforces.com/contest/723/problem/A Tags: implementation, math, sortings, *800 Ref: https://codeforces.com/contest/723/submission/147621972

---
+++ codeforces/723/A.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef complex<ld> cd;
+
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef pair<ld, ld> pd;
+
+typedef vector<int> vi;
+typedef vector<ld> vd;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<cd> vcd;
+
+template <class T>
+using pq = priority_queue<T>;
+template <class T>
+using pqg = priority_queue<T, vector<T>, greater<T>>;
+
+#define FOR(i, a, b) for (int i = a; i < (b); i++)
+#define F0R(i, a) for (int i = 0; i < (a); i++)
+#define FORd(i, a, b) for (int i = (b)-1; i >= a; i--)
+#define F0Rd(i, a) for (int i = (a)-1; i >= 0; i--)
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) (a*(b/gcd(a,b))
+
+#define sz(x) (int)(x).size()
+#define mp make_pair
+#define pb push_back
+#define f first
+#define s second
+#define lb lower_bound
+#define ub upper_bound
+#define all(x) x.begin(), x.end()
+#define ins insert
+
+template <class T>
+bool ckmin(T &a, const T &b)
+{
+    return b < a ? a = b, 1 : 0;
+}
+template <class T>
+bool ckmax(T &a, const T &b) { return a < b ? a = b, 1 : 0; }
+const char nl = '\n';
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n=3;
+    vi v;
+    F0R(i,n){
+        int a;
+        cin>>a;
+        v.pb(a);
+    }
+    sort(all(v));
+    cout<<v[2]-v[0];
+    return 0;
+}
