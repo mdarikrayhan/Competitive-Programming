b2dcd925cb30eedd18044549d787fe0d976c1a86
Md. Arik Rayhan <mdarikrayhan@gmail.com>
2022-06-28 10:46:00 +0600

    Add solution for problem `B - Lucky String` Link: https://codeforces.com/contest/110/problem/B Tags: constructive algorithms, strings, *1100 Ref: https://codeforces.com/contest/110/submission/162008840

--- codeforces/110/B.cpp
+++ codeforces/110/B.cpp
@@ -1,5 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef complex<ld> cd;
+
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef pair<ld, ld> pd;
+
+typedef vector<int> vi;
+typedef vector<ld> vd;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<cd> vcd;
+
+template <class T>
+using pq = priority_queue<T>;
+template <class T>
+using pqg = priority_queue<T, vector<T>, greater<T>>;
+
+#define FOR(i, a, b) for (int i = a; i < (b); i++)
+#define F0R(i, a) for (int i = 0; i < (a); i++)
+#define FORd(i, a, b) for (int i = (b)-1; i >= a; i--)
+#define F0Rd(i, a) for (int i = (a)-1; i >= 0; i--)
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) (a*(b/gcd(a,b))
+
+#define sz(x) (int)(x).size()
+#define mp make_pair
+#define pb push_back
+#define f first
+#define s second
+#define lb lower_bound
+#define ub upper_bound
+#define all(x) x.begin(), x.end()
+#define ins insert
+
+template <class T>
+bool ckmin(T &a, const T &b)
+{
+    return b < a ? a = b, 1 : 0;
+}
+template <class T>
+bool ckmax(T &a, const T &b) { return a < b ? a = b, 1 : 0; }
 const char nl = '\n';
 
 int main()
@@ -8,37 +53,24 @@ int main()
     cin.tie(NULL);
     int n;
     cin >> n;
-    if (n == 1)
+    if (n < 4)
     {
-        cout<<'a'<<nl;
-    }
-    else if (n == 2)
-    {
-        cout<<"ab"<<nl;
-    }
-    else if (n == 3)
-    {
-        cout<<"abc"<<nl;
+        for (int i = 0; i < n; i++)
+        {
+            char a = 97 + i;
+            cout << a;
+        }
     }
     else
     {
         for (int i = 0; i < n / 4; i++)
         {
-            cout<<"abcd";
+            cout << "abcd";
         }
-
-        int t = n % 4;
-        if (t == 1)
+        for (int i = 0; i < n%4; i++)
         {
-            cout<<'a'<<nl;
-        }
-        else if (t == 2)
-        {
-            cout<<"ab"<<nl;
-        }
-        else if (t == 3)
-        {
-            cout<<"abc"<<nl;
+            char a = 97 + i;
+            cout << a;
         }
     }
     return 0;
