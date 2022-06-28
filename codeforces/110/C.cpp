ca52a8926a7045b9ae0e3ba651cb0dfd72478cb1
Md. Arik Rayhan <mdarikrayhan@gmail.com>
2022-06-28 13:34:00 +0600

    Add solution for problem `C - Lucky Sum of Digits` Link: https://codeforces.com/contest/110/problem/C Tags: implementation, math, *1000 Ref: https://codeforces.com/contest/110/submission/162020106

--- codeforces/110/C.cpp
+++ codeforces/110/C.cpp
@@ -5,7 +5,7 @@ int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    int n, n7 = 0, n4 = 0, mod7, mod4;
+    int n, n7 = 0,n4=0, mod7;
     cin >> n;
     if (n < 4 || n == 5 || n == 6 || n == 9 || n == 10)
     {
@@ -14,20 +14,71 @@ int main()
     }
     mod7 = n % 7;
     n7 = n / 7;
-    mod4 = (mod7 % 4);
-    n4 = mod7 / 4;
+    /*if (mod7 == 0)
+    {
+        for (int i = 0; i < n7; i++)
+        {
+            cout << "7";
+        }
+    }
+    if (mod7 == 1)
+    {
+        cout << "44";
+        for (int i = 0; i < n7 - 1; i++)
+        {
+            cout << "7";
+        }
+    }
+    if (mod7 == 2)
+    {
+        cout << "4444";
+        for (int i = 0; i < n7 - 2; i++)
+        {
+            cout << "7";
+        }
+    }
+    if (mod7 == 3)
+    {
 
-    for (int i = 0; i < n4; i++)
+        cout << "444444";
+        for (int i = 0; i < n7 - 3; i++)
+        {
+            cout << "7";
+        }
+    }*/
+    if(mod7<4){
+        n4=mod7*2;
+        for(int i=0;i<n4;i++){
+            cout<<"4";
+        }
+        for(int i=0;i<n7-mod7;i++){
+            cout<<"7";
+        }
+    }
+    if (mod7 == 4)
     {
         cout << "4";
+        for (int i = 0; i < n7; i++)
+        {
+            cout << "7";
+        }
     }
-    for (int i = 0; i < mod4 * 2; i++)
+    if (mod7 == 5)
     {
-        cout << "4";
+        cout << "444";
+        for (int i = 0; i < n7 - 1; i++)
+        {
+            cout << "7";
+        }
     }
-    for (int i = 0; i < n7 - mod4; i++)
+    if (mod7 == 6)
     {
-        cout << "7";
+        cout << "44444";
+        for (int i = 0; i < n7 - 2; i++)
+        {
+            cout << "7";
+        }
     }
+
     return 0;
 }
