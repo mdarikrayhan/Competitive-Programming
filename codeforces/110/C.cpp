b5cd25b019ec2bc4fce18b31fd161075a19c4794
Md. Arik Rayhan <mdarikrayhan@gmail.com>
2022-06-28 14:45:00 +0600

    Add solution for problem `C - Lucky Sum of Digits` Link: https://codeforces.com/contest/110/problem/C Tags: implementation, math, *1000 Ref: https://codeforces.com/contest/110/submission/162025900

--- codeforces/110/C.cpp
+++ codeforces/110/C.cpp
@@ -1,23 +1,20 @@
 #include <iostream>
-using namespace std;
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
     int n;
-    cin >> n;
+    std::cin >> n;
     if (n == 0 || n == 1 || n == 2 || n == 3 || n == 5 || n == 6 || n == 9 || n == 10)
     {
-        cout << "-1";
+        std::cout << "-1";
         return 0;
     }
-    for (int i = 0; i < ((n % 7) / 4)+(((n % 7) % 4) * 2); i++)
+    for (int i = 0; i < ((n % 7) / 4) + (((n % 7) % 4) * 2); i++)
     {
-        cout << "4";
+        std::cout << "4";
     }
     for (int i = 0; i < ((n / 7) - ((n % 7) % 4)); i++)
     {
-        cout << "7";
+        std::cout << "7";
     }
     return 0;
 }
