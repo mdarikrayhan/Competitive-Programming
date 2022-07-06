3f6a6bc32da4891eba9536a6d30e2406579289b4
Md. Arik Rayhan <mdarikrayhan@gmail.com>
2022-07-06 18:02:00 +0600

    Add solution for problem `B - Patchouli's Magical Talisman` Link: https://codeforces.com/contest/1688/problem/B Tags: bitmasks, constructive algorithms, greedy, sortings, *800 Ref: https://codeforces.com/contest/1688/submission/162998400

--- codeforces/1688/B.cpp
+++ codeforces/1688/B.cpp
@@ -1,19 +1,106 @@
-#include "bits/stdC++.h"
+#include <bits/stdc++.h>
 using namespace std;
-#define all(x) (x).begin(),(x).end()
+
+typedef long long ll;
+typedef long double ld;
+typedef complex<ld> cd;
+
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef pair<ld, ld> pd;
+
+typedef vector<int> vi;
+typedef vector<ld> vd;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<cd> vcd;
+
+template <class T>
+using pq = priority_queue<T>;
+template <class T>
+using pqg = priority_queue<T, vector<T>, greater<T>>;
+
+#define FOR(i, a, b) for (int i = a; i < (b); i++)
+#define F0R(i, a) for (int i = 0; i < (a); i++)
+#define FORd(i, a, b) for (int i = (b)-1; i >= a; i--)
+#define F0Rd(i, a) for (int i = (a)-1; i >= 0; i--)
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) (a*(b/gcd(a,b))
+
+#define sz(x) (int)(x).size()
+#define mp make_pair
+#define pb push_back
+#define ff first
+#define ss second
+#define lb lower_bound
+#define ub upper_bound
+#define all(x) x.begin(), x.end()
+#define ins insert
+
+bool IsPrime(int n)
+{
+    if (n == 2 || n == 3)
+        return true;
+    if (n <= 1 || n % 2 == 0 || n % 3 == 0)
+        return false;
+    for (int i = 5; i * i <= n; i += 6)
+    {
+        if (n % i == 0 || n % (i + 2) == 0)
+            return false;
+    }
+    return true;
+}
+
+template <class T>
+bool ckmin(T &a, const T &b) { return b < a ? a = b, 1 : 0; }
+template <class T>
+bool ckmax(T &a, const T &b) { return a < b ? a = b, 1 : 0; }
+const char nl = '\n';
+void solve()
+{
+    int n;
+    cin >> n;
+    int even = 0, odd = 0, mn = INT_MAX;
+    for (int i = 0; i < n; i++)
+    {
+        int a;
+        cin >> a;
+        if (a & 1)
+        {
+            odd++;
+        }
+        else
+        {
+            even++;
+            int count = 0;
+            while (!(a & 1))
+            {
+                count++;
+                a/=2;
+            }
+            mn = min(count, mn);
+        }
+    }
+    if (odd != 0)
+    {
+        cout << even << nl;
+    }
+    else
+    {
+        cout<<mn-1+even<<nl;
+    }
+}
+
 int main()
 {
-	ios::sync_with_stdio(0);cin.tie(0);
-	int T;
-	cin>>T;
-	while (T--)
-	{
-		int n,r;
-		cin>>n;
-		vector<int> a(n);
-		for (int &x:a) cin>>x,x=__builtin_ffs(x)-1;
-		r=max(*min_element(all(a))-1,0);
-		for (int x:a) r+=(x>0);
-		cout<<r<<'\n';
-	}
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int T = 1;
+    cin >> T;
+    while (T--)
+    {
+        solve();
+    }
+    return 0;
 }
