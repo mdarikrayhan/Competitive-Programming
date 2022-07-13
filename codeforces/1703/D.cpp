c76bdd095d14f205dbd2183366e02bd9c07a17b7
Md. Arik Rayhan <mdarikrayhan@gmail.com>
2022-07-13 18:59:00 +0600

    Add solution for problem `D - Double Strings` Link: https://codeforces.com/contest/1703/problem/D Tags: brute force, data structures, strings Ref: https://codeforces.com/contest/1703/submission/164035733

---
+++ codeforces/1703/D.cpp
@@ -0,0 +1,134 @@
+/***************************************************/
+/*            Author : Md. Arik Rayhan             */
+/*        Github : github.com/mdarikrayhan         */
+/***************************************************/
+#include <bits/stdc++.h>
+using namespace std;
+const char nl = '\n';
+
+typedef long long ll;
+typedef long double ld;
+typedef complex<ld> cd;
+
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef pair<ld, ld> pd;
+
+typedef vector<int> vi;
+typedef vector<ld> vd;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<cd> vcd;
+
+#define rep(i, a, b) for (int i = a; i < (b); i++)
+#define per(i, a, b) for (int i = (b)-1; i >= a; i--)
+#define print_map(m)                                                     \
+    for (const auto &[key, value] : m)                                   \
+    {                                                                    \
+        cout << '[' << key << ']' << ' ' << '=' << ' ' << value << '\n'; \
+    }
+#define print_vector(v)                          \
+    int vadnfiv = 0;                             \
+    for (const auto &value : v)                  \
+    {                                            \
+        cout << vadnfiv << ' ' << value << '\n'; \
+        vadnfiv++;                               \
+    }
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) (a*(b/gcd(a,b))
+
+#define sz(x) (int)(x).size()
+#define mp make_pair
+#define pb push_back
+#define ff first
+#define ss second
+#define lb lower_bound
+#define ub upper_bound
+#define all(x) x.begin(), x.end()
+#define ins insert
+#define fastio                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+#define multicase         \
+    int NoOfTestCase = 1; \
+    cin >> NoOfTestCase;  \
+    for (int testcaseno = 1; testcaseno <= NoOfTestCase; testcaseno++)
+bool isPrime(int n)
+{
+    if (n == 2 || n == 3)
+    {
+        return true;
+    }
+    if (n <= 1 || n % 2 == 0 || n % 3 == 0)
+    {
+        return false;
+    }
+    for (int i = 5; i * i <= n; i += 6)
+    {
+        if (n % i == 0 || n % (i + 2) == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+bool isPalindrome(string S)
+{
+    string P = S;
+    reverse(P.begin(), P.end());
+    if (S == P)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+template <class T>
+bool ckmin(T &a, const T &b) { return b < a ? a = b, 1 : 0; }
+template <class T>
+bool ckmax(T &a, const T &b) { return a < b ? a = b, 1 : 0; }
+
+int main()
+{
+    fastio
+        multicase
+    {
+        int n;
+        cin >> n;
+        map<string, int> mp1;
+        string s[n];
+        rep(i, 0, n)
+        {
+            cin >> s[i];
+            mp1[s[i]]++;
+        }
+        string ans;
+        for (int i = 0; i < n; i++)
+        {
+            string t = s[i];
+            string r;
+            int flag = 0;
+            for (int j = 0; j < t.size() - 1; j++)
+            {
+                r += t[j];
+                string u = t.substr(j + 1, t.size());
+                if (r == u and mp1[u] > 1)
+                    flag = 1;
+                else if (mp1[u] > 0 and mp1[r])
+                    flag = 1;
+            }
+            if (flag)
+                ans += '1';
+            else
+                ans += '0';
+        }
+        cout << ans << nl;
+    }
+
+    return 0;
+}
