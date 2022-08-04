e98cb1e777d357db264b6aa73f43bce195616f78
Md. Arik Rayhan <mdarikrayhan@gmail.com>
2022-08-04 21:51:00 +0600

    Add solution for problem `A - 2-3 Moves` Link: https://codeforces.com/contest/1716/problem/A Tags: greedy, math, *800 Ref: https://codeforces.com/contest/1716/submission/166991582

---
+++ codeforces/1716/A.py
@@ -0,0 +1,8 @@
+for _ in range (int (input())) :
+    arik185319=int (input() )
+    if arik185319==1:
+        print (2)
+    elif arik185319%3==0:
+        print(arik185319//3)
+    else:
+        print ((arik185319//3)+1)
