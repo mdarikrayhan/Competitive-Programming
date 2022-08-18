ede3c7d8ab3f277639eb4d71e771dc809fb578d6
Md. Arik Rayhan <mdarikrayhan@gmail.com>
2022-08-18 23:17:00 +0600

    Add solution for problem `C - Corners` Link: https://codeforces.com/contest/1720/problem/C Tags: greedy, implementation Ref: https://codeforces.com/contest/1720/submission/168874937

--- codeforces/1720/C.cpp
+++ codeforces/1720/C.cpp
@@ -5,22 +5,126 @@
 #include <bits/stdc++.h>
 using namespace std;
 const char nl = '\n';
+
+typedef long long ll;
+typedef long double ld;
+typedef complex<ld> cd;
+
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef pair<ld, ld> pd;
+
+typedef vector<int> vi;
+typedef vector<ld> vd;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<cd> vcd;
+
+#define rep(i, a, b) for (int i = a; i < (b); i++)
+#define per(i, a, b) for (int i = (b)-1; i >= a; i--)
+#define print_map(m)                                                     \
+    for (const auto &[key, value] : m)                                   \
+    {                                                                    \
+        cout << '[' << key << ']' << ' ' << '=' << ' ' << value << '\n'; \
+    }
+#define print_vector(v)                          \
+    int vadnfiv = 0;                             \
+    for (const auto &value : v)                  \
+    {                                            \
+        cout << vadnfiv << ' ' << value << '\n'; \
+        vadnfiv++;                               \
+    }
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) (a * (b / gcd(a, b)))
+
+#define start_time                            \
+    using std::chrono::duration;              \
+    using std::chrono::duration_cast;         \
+    using std::chrono::high_resolution_clock; \
+    using std::chrono::milliseconds;          \
+    auto t1111 = high_resolution_clock::now();
+#define end_time                                            \
+    auto t2222 = high_resolution_clock::now();              \
+    duration<double, std::milli> ms_double = t2222 - t1111; \
+    std::cout << ms_double.count() << 'm' << 's' << nl;
+#define sz(x) (int)(x).size()
+#define mp make_pair
+#define pb push_back
+#define ff first
+#define ss second
+#define lb lower_bound
+#define ub upper_bound
+#define all(x) x.begin(), x.end()
+#define ins insert
+#define fastio                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+#define multicase         \
+    int NoOfTestCase = 1; \
+    cin >> NoOfTestCase;  \
+    for (int testcaseno = 1; testcaseno <= NoOfTestCase; testcaseno++)
+bool isPrime(int n)
+{
+    if (n == 2 || n == 3)
+    {
+        return true;
+    }
+    if (n <= 1 || n % 2 == 0 || n % 3 == 0)
+    {
+        return false;
+    }
+    for (int i = 5; i * i <= n; i += 6)
+    {
+        if (n % i == 0 || n % (i + 2) == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+bool isPalindrome(string S)
+{
+    string P = S;
+    reverse(P.begin(), P.end());
+    if (S == P)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+template <class T>
+bool ckmin(T &a, const T &b) { return b < a ? a = b, 1 : 0; }
+template <class T>
+bool ckmax(T &a, const T &b) { return a < b ? a = b, 1 : 0; }
+
 int main()
 {
-    int T;
-    cin>>T;
-    while(T--)
+    fastio
+        multicase
     {
-        int n, m,no1=0,flag=0;
+        int n, m,no0=0,no1=0,flag=0;
         cin >> n >> m;
-        char a[n+5][m+5];
+        int a[n+5][m+5];
+        string s[n+5];
+        for(int i=0;i<n;i++){
+            cin >> s[i];
+        }
         for (int i = 0; i < n; i++)
         {
             for (int j = 0; j < m; j++)
             {
-                cin>>a[i][j];
-                if(a[i][j]=='1')
+                a[i][j]=s[i][j];
+                if(a[i][j]=='0')
                 {
+                    no0++;
+                }
+                else{
                     no1++;
                 }
             }
@@ -28,11 +132,19 @@ int main()
 
     for(int i=0;i<n-1;i++){
         for(int j=0;j<m-1;j++){
-            if(a[i][j]=='0'&&(a[i+1][j+1]=='0'||a[i+1][j]=='0'||a[i][j+1]=='0')){
+            if(a[i][j]=='0'&&a[i+1][j+1]=='0'){
                 flag=1;
                 break;
             }
             else if(a[i+1][j]=='0'&&a[i][j+1]=='0'){
+                flag=1;
+                break;
+            }
+            else if(a[i][j]=='0'&&a[i][j+1]=='0'){
+                flag=1;
+                break;
+            }
+            else if(a[i][j]=='0'&&a[i+1][j]=='0'){
                 flag=1;
                 break;
             }
