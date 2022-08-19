3cef13930ded2d1bd60937d6d7c8793696acddf2
Md. Arik Rayhan <mdarikrayhan@gmail.com>
2022-08-19 10:19:00 +0600

    Add solution for problem `C - Corners` Link: https://codeforces.com/contest/1720/problem/C Tags: greedy, implementation Ref: https://codeforces.com/contest/1720/submission/168917030

--- codeforces/1720/C.cpp
+++ codeforces/1720/C.cpp
@@ -1,45 +1,40 @@
+/***************************************************/
+/*            Author : Md. Arik Rayhan             */
+/*        Github : github.com/mdarikrayhan         */
+/***************************************************/
 #include <bits/stdc++.h>
 using namespace std;
-char s[505][505];
-int w[505][505];
+const char nl = '\n';
 int main()
 {
-    int t;
-    scanf("%d", &t);
-    while (t--)
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int T;
+    cin>>T;
+    while(T--)
     {
-        int n, m, ans = 0, k = 0, flag = 0;
-        scanf("%d%d", &n, &m);
-        for (int i = 0; i < n; i++)
-        {
-            scanf("\n%s", s[i]);
+    int n, m, sum = 0;
+    cin >> n >> m;
+    string s;
+    int a[n+5][m+5];
+    for (int i = 0; i < n; ++i) {
+        cin >> s;
+        for (int j = 0; j < m; ++j) {
+            a[i][j] = s[j] - '0';
+            sum += a[i][j];
         }
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = 0; j < m; j++)
-            {
-                ans += (w[i][j] = s[i][j] - '0');
-            }
-        }
-        for (int i = 0; i < n - 1; i++)
-        {
-            for (int j = 0; j < m - 1; j++)
-            {
-                k = w[i][j] + w[i + 1][j] + w[i][j + 1] + w[i + 1][j + 1];
-                if (k <= 2)
-                {
-                    flag = 1;
-                    break;
-                }
-            }
-            if (flag == 1)
-                break;
-        }
-        if (flag == 1)
-            printf("%d\n", ans);
-        else if(ans==(n*m))
-            printf("%d\n", ans-2);
-        else
-            printf("%d\n", ans - 1);
     }
+    int minn = INT_MAX;
+    for (int i = 0; i < n - 1; ++i) {
+        for (int j = 0; j < m - 1; ++j) {
+            int cnt = a[i][j] + a[i + 1][j] + a[i][j + 1] + a[i + 1][j + 1];
+            if (cnt == 0) continue;
+            minn = min(minn, max(1, cnt - 1));
+        }
+    }
+    if (sum == 0) cout << "0\n";
+    else cout << 1 + sum - minn << "\n";
+    }
+    return 0;
 }
