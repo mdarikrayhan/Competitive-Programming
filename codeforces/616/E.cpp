2d6dceaaf101efbc2036bfd48746983124d9a483
Md. Arik Rayhan <mdarikrayhan@gmail.com>
2022-08-20 18:03:00 +0600

    Add solution for problem `E - Sum of Remainders` Link: https://codeforces.com/contest/616/problem/E Tags: implementation, math, number theory, *2200 Ref: https://codeforces.com/contest/616/submission/169077071

--- codeforces/616/E.cpp
+++ codeforces/616/E.cpp
@@ -1,32 +1,74 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-const ll mod = 1000000007;
-ll l, r, x, ans;
-ll dls2(ll i)
+typedef long long li;
+const li mod = 1000000007;
+
+inline void normal(li &a)
 {
-    return ((i % mod) * ((i + 1) % mod) / 2) % mod;
+    a %= mod;
+    (a < 0) && (a += mod);
 }
-ll dls(ll l, ll r)
+
+inline li mul(li a, li b)
 {
-    return (dls2(r) - dls2(l - 1)) % mod;
+    a %= mod, b %= mod;
+    normal(a), normal(b);
+    return (a * b) % mod;
 }
-void solve()
+
+inline li add(li a, li b)
 {
-    ans += (mod - (dls(l, r) * x) % mod);
-    ans %= mod;
+    a %= mod, b %= mod;
+    normal(a), normal(b);
+    return (a + b) % mod;
 }
+
+inline li sub(li a, li b)
+{
+    a %= mod, b %= mod;
+    normal(a), normal(b);
+    a -= b;
+    normal(a);
+    return a;
+}
+
+inline li sum(li n) { return mul(mul(n, n + 1), (mod + 1) / 2); }
+inline li sum(li lf, li rg) { return sub(sum(rg), sum(lf - 1)); }
+
+inline li calcDiv(li n, li m)
+{
+    m = min(m, n);
+
+    li ans = 0;
+    li minVal = m;
+    for (li i = 1; i * i <= n; i++)
+    {
+        li lf = n / (i + 1), rg = n / i;
+        rg = min(rg, m);
+        if (lf >= rg)
+            continue;
+        minVal = lf; // interval (lf, rg]
+        ans = add(ans, mul(i, sum(lf + 1, rg)));
+    }
+
+    for (int i = 1; i <= minVal; i++)
+    {
+        ans = add(ans, mul(n / i, i));
+    }
+    return ans;
+}
+
+inline li calcMod(li n, li m)
+{
+    li ans = mul(n, m);
+    ans = sub(ans, calcDiv(n, m));
+    return ans;
+}
+
 int main()
 {
-    ll n, m;
+    long long n, m;
     cin >> n >> m;
-    ans = (n % mod) * (m % mod) % mod;
-    m = min(m, n);
-    for (l = 1; l <= m; l = r + 1)
-    {
-        x = n / l;
-        r = min(m, n / x);
-        solve();
-    }
-    cout << ans << endl;
+    cout << calcMod(n, m) << endl;
+    return 0;
 }
