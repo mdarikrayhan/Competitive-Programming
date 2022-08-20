b9c157b669140a11ca9ce355956a9bdc2691c3f6
Md. Arik Rayhan <mdarikrayhan@gmail.com>
2022-08-20 19:02:00 +0600

    Add solution for problem `E - Sum of Remainders` Link: https://codeforces.com/contest/616/problem/E Tags: implementation, math, number theory, *2200 Ref: https://codeforces.com/contest/616/submission/169082677

---
+++ codeforces/616/E.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const ll mod = 1000000007;
+ll dls(ll i)
+{
+    return ((i % mod) * ((i + 1) % mod) / 2) % mod;
+}
+int main()
+{
+    ll n, m,ans;
+    cin >> n >> m;
+    ans = (n % mod) * (m % mod) % mod;
+    m = min(m, n);
+    for (ll l = 1,r=0; l <= m; l = r + 1)
+    {
+        ll x = n / l;
+        r = min(m, n / x);
+
+        //cout<<l<<" "<<r<<endl;
+
+        ll a=((r % mod) * ((r + 1) % mod) / 2) % mod;
+        ll b=(((l-1) % mod) * (l % mod) / 2) % mod;
+        ll y = (a-b)%mod;
+
+        ans += (mod - (y * x) % mod);
+        ans %= mod;
+    }
+    cout << ans << endl;
+}
