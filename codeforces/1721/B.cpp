7879b81a3273ac28ee80e1470a4c5c530358fe63
Md. Arik Rayhan <mdarikrayhan@gmail.com>
2022-08-28 08:31:00 +0600

    Add solution for problem `B - Deadly Laser` Link: https://codeforces.com/contest/1721/problem/B Tags: implementation, *1000 Ref: https://codeforces.com/contest/1721/submission/169927576

--- codeforces/1721/B.cpp
+++ codeforces/1721/B.cpp
@@ -1,5 +1,6 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
+const char nl = '\n';
 int main()
 {
     ios_base::sync_with_stdio(0);
@@ -14,15 +15,15 @@ int main()
         ans=m+n-2;
         if ((sy - d > 1) && (d + sx < n))
         {
-            cout<<ans<<'\n';
+            cout<<ans<<nl;
         }
         else if ((sx - d > 1) && (d + sy < m))
         {
-            cout<<ans<<'\n';
+            cout<<ans<<nl;
         }
         else
         {
-            cout << "-1" << '\n';
+            cout << "-1" << nl;
         }
     }
     return 0;
