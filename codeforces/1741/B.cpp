1149c5531946a1274485d84fdb4a56184550052a
mdarikrayhan <mdarikrayhan@gmail.com>
2022-11-06 17:42:00 +0600

    Add solution for problem `B - Funny Permutation` Link: https://codeforces.com/contest/1741/problem/B Tags: constructive algorithms, math, *800 Ref: https://codeforces.com/contest/1741/submission/179538605

--- codeforces/1741/B.cpp
+++ codeforces/1741/B.cpp
@@ -1,23 +1,29 @@
-#include<bits/stdc++.h>
+#include <iostream>
 using namespace std;
-
-void solve(){
-	int n; cin >> n;
-	if(n & 1 and n < 5) {
-	    cout << "-1\n";
-	    return;
-	}
-	cout << n << " " << n - 1 << " ";
-	for(int i = 1; i <= n - 2; i++) cout << i << " ";
-	cout << "\n";
-	
-
-}
-int32_t main(){
-	cin.tie(0) -> sync_with_stdio(0);
-	int t; cin >> t;
-	while(t--){
-		solve();
-	}
-	
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        if (n == 2)
+        {
+            cout << 2 << ' ' << 1 << '\n';
+        }
+        else if (n == 3)
+            cout << -1 << '\n';
+        else
+        {
+            cout << n - 1 << ' ' << n << ' ';
+            for (int i = 1; i < n - 1; i++)
+                cout << i << ' ';
+            cout << '\n';
+        }
+    }
+    return 0;
 }
