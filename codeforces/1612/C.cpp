a03e0f4674c2080fe462deba28a5adb396f571a5
mdarikrayhan <mdarikrayhan@gmail.com>
2022-11-15 10:47:00 +0600

    Add solution for problem `C - Chat Ban` Link: https://codeforces.com/contest/1612/problem/C Tags: binary search, math, *1300 Ref: https://codeforces.com/contest/1612/submission/181039214

--- codeforces/1612/C.cpp
+++ codeforces/1612/C.cpp
@@ -11,8 +11,8 @@ typedef long double ld;
 typedef complex<ld> cd;
 
 typedef pair<int, int> pi;
-typedef pair<ll,ll> pl;
-typedef pair<ld,ld> pd;
+typedef pair<ll, ll> pl;
+typedef pair<ld, ld> pd;
 
 typedef vector<int> vi;
 typedef vector<ld> vd;
@@ -21,15 +21,33 @@ typedef vector<pi> vpi;
 typedef vector<pl> vpl;
 typedef vector<cd> vcd;
 
-#define rep(i,a,b) for (int i=a; i<(b); i++)
-#define per(i,a,b) for (int i = (b)-1; i >= a; i--)
-#define print_map(m) for (const auto &[key, value] : m){cout << '[' << key << ']' << ' ' << '=' << ' ' << value << '\n';}
-#define print_vector(v)int vadnfiv=0;for (const auto &value : v){cout <<vadnfiv<<' '<<value<< '\n';vadnfiv++;}
-#define gcd(a,b) __gcd(a,b)
-#define lcm(a,b) (a*(b/gcd(a,b)))
+#define rep(i, a, b) for (int i = a; i < (b); i++)
+#define per(i, a, b) for (int i = (b)-1; i >= a; i--)
+#define print_map(m)                                                     \
+    for (const auto &[key, value] : m)                                   \
+    {                                                                    \
+        cout << '[' << key << ']' << ' ' << '=' << ' ' << value << '\n'; \
+    }
+#define print_vector(v)                          \
+    int vadnfiv = 0;                             \
+    for (const auto &value : v)                  \
+    {                                            \
+        cout << vadnfiv << ' ' << value << '\n'; \
+        vadnfiv++;                               \
+    }
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) (a * (b / gcd(a, b)))
 
-#define start_time using std::chrono::duration;using std::chrono::duration_cast;using std::chrono::high_resolution_clock;using std::chrono::milliseconds;auto t1111 = high_resolution_clock::now();
-#define end_time auto t2222 = high_resolution_clock::now();duration<double, std::milli> ms_double = t2222 - t1111;std::cout << ms_double.count() << 'm'<<'s'<<nl;
+#define start_time                            \
+    using std::chrono::duration;              \
+    using std::chrono::duration_cast;         \
+    using std::chrono::high_resolution_clock; \
+    using std::chrono::milliseconds;          \
+    auto t1111 = high_resolution_clock::now();
+#define end_time                                            \
+    auto t2222 = high_resolution_clock::now();              \
+    duration<double, std::milli> ms_double = t2222 - t1111; \
+    std::cout << ms_double.count() << 'm' << 's' << nl;
 #define sz(x) (int)(x).size()
 #define mp make_pair
 #define pb push_back
@@ -39,41 +57,104 @@ typedef vector<cd> vcd;
 #define ub upper_bound
 #define all(x) x.begin(), x.end()
 #define ins insert
-#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-#define multicase int NoOfTestCase = 1;cin >> NoOfTestCase;for(int testcaseno=1;testcaseno<=NoOfTestCase;testcaseno++)
-bool isPrime(int n){if (n == 2 || n == 3){return true;}if (n <= 1 || n % 2 == 0 || n % 3 == 0){return false;}for (int i = 5; i * i <= n; i += 6){if (n % i == 0 || n % (i + 2) == 0){return false;}}return true;}
-bool isPalindrome(string S){string P = S;reverse(P.begin(), P.end());if (S == P) {return true;}else {return false;}}
-bool isPowerof(long long num, long long base){if (num <= 0){return false;}if (num % base == 0){return isPowerof(num / base, base);}if (num == 1){return true;}return false;}
-
-template<class T> bool ckmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
-template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
-
-int main() {
-fastio
-multicase
+#define fastio                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+#define multicase         \
+    int NoOfTestCase = 1; \
+    cin >> NoOfTestCase;  \
+    for (int testcaseno = 1; testcaseno <= NoOfTestCase; testcaseno++)
+bool isPrime(int n)
 {
-        long long k,x,a,b,mid,tmp;
-		cin>>k>>x;
-		if(k*k<=x){
-			cout<<2*k-1<<"\n";
-		}else{
-			a=1;
-			b=2*k-1;
-			while(a<=b){
-				mid=(a+b)/2;
-				if(mid>k){
-					tmp=(1+k)*k/2+(3*k-mid-1)*(mid-k)/2;
-				}else{
-					tmp=(1+mid)*mid/2;
-				}
-				if(tmp<x){
-					a=mid+1;
-				}else{
-					b=mid-1;
-				}
-			}
-			cout<<a<<"\n";	
-		}
+    if (n == 2 || n == 3)
+    {
+        return true;
+    }
+    if (n <= 1 || n % 2 == 0 || n % 3 == 0)
+    {
+        return false;
+    }
+    for (int i = 5; i * i <= n; i += 6)
+    {
+        if (n % i == 0 || n % (i + 2) == 0)
+        {
+            return false;
+        }
+    }
+    return true;
 }
-return 0;
+bool isPalindrome(string S)
+{
+    string P = S;
+    reverse(P.begin(), P.end());
+    if (S == P)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+bool isPowerof(long long num, long long base)
+{
+    if (num <= 0)
+    {
+        return false;
+    }
+    if (num % base == 0)
+    {
+        return isPowerof(num / base, base);
+    }
+    if (num == 1)
+    {
+        return true;
+    }
+    return false;
+}
+
+template <class T>
+bool ckmin(T &a, const T &b) { return b < a ? a = b, 1 : 0; }
+template <class T>
+bool ckmax(T &a, const T &b) { return a < b ? a = b, 1 : 0; }
+
+unsigned long long nsum(unsigned long long n)
+{
+    return (n * (n + 1)) / 2;
+}
+int main()
+{
+    fastio
+        multicase
+    {
+        unsigned long long k, x, ans1, ans2;
+        cin >> k >> x;
+        ans1 = ans2 = 2 * k - 1;
+        unsigned long long i = 1;
+        bool over = false;
+        while (i <= ans2)
+        {
+            int mid = (i + ans2) >> 1;
+            if (mid >= k)
+            {
+                over = (nsum(k) + nsum(k - 1) - nsum(2 * k - 1 - mid) >= x);
+            }
+            else
+            {
+                over = (nsum(mid) >= x);
+            }
+            if (over)
+            {
+                ans1 = mid;
+                ans2 = mid - 1;
+            }
+            else
+            {
+                i = mid + 1;
+            }
+        }
+        cout << ans1 << nl;
+    }
+    return 0;
 }
