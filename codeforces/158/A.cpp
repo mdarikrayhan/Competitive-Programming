8c6e6574afbff01aaab6661868e54f084ca92e34
mdarikrayhan <mdarikrayhan@gmail.com>
2023-03-03 17:40:00 +0600

    Add solution for problem `A - Next Round` Link: https://codeforces.com/contest/158/problem/A Tags: *special problem, implementation, *800 Ref: https://codeforces.com/contest/158/submission/195799427

--- codeforces/158/A.cpp
+++ codeforces/158/A.cpp
@@ -1,16 +1,32 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-int n, k, a, b = 0, mas[10000];
-int main() {
-    cin >> n >> k;
-    for (int i = 1; i <= n; i++) {
-        cin >> a;
-        mas[i] = a;
+const char nl = '\n';
+int main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+cout.tie(NULL);
+int T = 1;
+
+for (int i = 1; i <= T; i++)
+{
+    int n,k;
+    cin>>n>>k;
+    vector<int> v(n);
+    for(int i=0;i<n;i++)
+    {
+        cin>>v[i];
     }
-    for (int j = 1; j <= n; j++) {
-        if (mas[j] >= mas[k] && mas[j] > 0) {
-            b++;
+    int num = v[k-1];
+    int count = 0;
+    for(int i=0;i<n;i++)
+    {
+        if(v[i]>=num && v[i]>0)
+        {
+            count++;
         }
     }
-    cout << b;
+    cout<<count<<nl;
+}
+return 0;
 }
