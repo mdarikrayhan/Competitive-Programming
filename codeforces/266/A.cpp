acc13a7fd3690d950ee879eed895306ea6f40750
mdarikrayhan <mdarikrayhan@gmail.com>
2023-03-18 15:21:00 +0600

    Add solution for problem `A - Stones on the Table` Link: https://codeforces.com/contest/266/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/266/submission/197887512

--- codeforces/266/A.cpp
+++ codeforces/266/A.cpp
@@ -1,17 +1,23 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
+const char nl = '\n';
 int main()
 {
-    int n = 0, count = 0;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int n;
     cin >> n;
     string s;
     cin >> s;
-    for (int i = 0; i < n; i++)
+    int ans = 0;
+    for (int i = 0; i < n - 1; i++)
     {
         if (s[i] == s[i + 1])
         {
-            count++;
+            ans++;
         }
     }
-    cout << count;
+    cout << ans << nl;
+    return 0;
 }
