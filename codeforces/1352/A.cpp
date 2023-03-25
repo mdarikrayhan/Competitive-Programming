7b917aff22487e0959a05d41d4878b293ebc5c8a
mdarikrayhan <mdarikrayhan@gmail.com>
2023-03-25 23:50:00 +0600

    Add solution for problem `A - Sum of Round Numbers` Link: https://codeforces.com/contest/1352/problem/A Tags: implementation, math, *800 Ref: https://codeforces.com/contest/1352/submission/199133916

--- codeforces/1352/A.cpp
+++ codeforces/1352/A.cpp
@@ -1,45 +1,48 @@
 #include <bits/stdc++.h>
 using namespace std;
 const char nl = '\n';
-void solve()
-{
-    int n;
-    cin >> n;
-    vector<int> number;
-    vector<int>num;
-    while (n)
-    {
-        number.push_back(n % 10);
-        if(n%10!=0){
-            num.push_back(n%10);
-        }
-        n = n / 10;
-    }
-    cout<<num.size()<<nl;
-    for (int i = 0; i < number.size(); i++)
-    {
-        if (number[i] > 0)
-        {
-            cout << number[i];
-            for (int j = 0; j < i; j++)
-            {
-                cout << 0;
-            }
-            cout << " ";
-        }
-    }
-    cout << nl;
-}
-
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
+    cout.tie(NULL);
     int T = 1;
     cin >> T;
-    while (T--)
+    for (int i = 1; i <= T; i++)
     {
-        solve();
+        int n;
+        cin >> n;
+        vector<int> ans;
+        if(n%10!=0)
+        {
+            ans.push_back(n%10);
+            n-=n%10;
+        }
+        if(n%100!=0)
+        {
+            ans.push_back(n%100);
+            n-=n%100;
+        }
+        if(n%1000!=0)
+        {
+            ans.push_back(n%1000);
+            n-=n%1000;
+        }
+        if(n%10000!=0)
+        {
+            ans.push_back(n%10000);
+            n-=n%10000;
+        }
+        if(n!=0)
+        {
+            ans.push_back(n);
+        }
+        cout<<ans.size()<<nl;
+        for(int i=0;i<ans.size();i++)
+        {
+            cout<<ans[i]<<" ";
+        }
+        cout<<nl;
     }
     return 0;
 }
