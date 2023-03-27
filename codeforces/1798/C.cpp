8c52977c7741cb7a11eee0a665430902e040d514
mdarikrayhan <mdarikrayhan@gmail.com>
2023-03-27 20:16:00 +0600

    Add solution for problem `C - Candy Store` Link: https://codeforces.com/contest/1798/problem/C Tags: greedy, math, number theory Ref: https://codeforces.com/contest/1798/submission/199420470

--- codeforces/1798/C.cpp
+++ codeforces/1798/C.cpp
@@ -1,7 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 const char nl = '\n';
-int main()
+#define int long long
+#define vi vector<int>
+int32_t main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
@@ -12,7 +14,7 @@ int main()
     {
         int n;
         cin >> n;
-        vector<long long> a(n), b(n), c(n);
+        vi a(n), b(n), c(n);
         for (int i = 0; i < n; i++)
         {
             cin >> a[i] >> b[i];
@@ -22,18 +24,18 @@ int main()
         {
             c[i] = a[i] * b[i];
         }
-        long long gcd = c[0];
-        long long lcm = b[0];
-        long long ans = 0;
-        for (int i = 1; i < n; i++)
+        int gcd = c[0];
+        int lcm = b[0];
+        int ans = 0;
+        for (int i = 0; i < n - 1; i++)
         {
-            gcd = __gcd(gcd, c[i]);
-            lcm = (lcm * b[i]) / (__gcd(lcm, b[i]));
+            gcd = __gcd(gcd, c[i + 1]);
+            lcm = (lcm * b[i + 1]) / (__gcd(lcm, b[i + 1]));
             if (gcd % lcm)
             {
                 ans++;
-                gcd = c[i];
-                lcm = b[i];
+                gcd = c[i + 1];
+                lcm = b[i + 1];
             }
         }
         cout << ans + 1 << endl;
