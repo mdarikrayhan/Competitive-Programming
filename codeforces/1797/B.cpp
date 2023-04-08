1142cca2c20d17e0f1ebafd9fac34dfb68ff1434
mdarikrayhan <mdarikrayhan@gmail.com>
2023-04-08 23:08:00 +0600

    Add solution for problem `B - Li Hua and Pattern` Link: https://codeforces.com/contest/1797/problem/B Tags: constructive algorithms, greedy Ref: https://codeforces.com/contest/1797/submission/201356509

---
+++ codeforces/1797/B.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+const char nl = '\n';
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int T = 1;
+    cin >> T;
+    while (T--)
+    {
+        long long n, k;
+        cin >> n >> k;
+        long long arr[n + 5][n + 5];
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                cin >> arr[i][j];
+            }
+        }
+        long long count = 0;
+        for (int i = 0, p = n - 1; i < n / 2, p >= n / 2; i++, p--)
+        {
+            for (int j = 0, q = n - 1; j < n, q >= 0; j++, q--)
+            {
+                if (arr[i][j] != arr[p][q])
+                {
+                    count++;
+                    arr[i][j] = arr[p][q];
+                }
+            }
+        }
+
+        if (((count <= k) && (((k - count) % 2 == 0) || n & 1)))
+        {
+            cout << "YES" << nl;
+        }
+        else
+        {
+            cout << "NO" << nl;
+        }
+    }
+    return 0;
+}
