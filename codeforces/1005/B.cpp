57a4d75c1590c653ddb5e24026bdf0d23651d636
mdarikrayhan <mdarikrayhan@gmail.com>
2023-04-16 21:25:00 +0600

    Add solution for problem `B - Delete from the Left` Link: https://codeforces.com/contest/1005/problem/B Tags: brute force, implementation, strings, *900 Ref: https://codeforces.com/contest/1005/submission/202384078

--- codeforces/1005/B.cpp
+++ codeforces/1005/B.cpp
@@ -1,23 +1,37 @@
+/***************************************************/
+/*                  Md. HAFIZ AHMED                */
+/*               DEPARTMENT OF CSE,BAUET           */
+/***************************************************/
 #include <bits/stdc++.h>
 using namespace std;
-int main()
+#define ll long long
+#define pb push_back
+#define nl '\n'
+#define successfull return 0
+
+int32_t main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    string s, s1;
-    cin >> s >> s1;
-    if (s.length() < s1.length())
-        swap(s, s1);
-    std::reverse(s.begin(), s.end());
-    std::reverse(s1.begin(), s1.end());
-    int x = 0;
-    for (int i = 0; i < s1.length(); i++)
+    ios_base::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+
+    string s, t;
+    cin >> s >> t;
+    int n = s.size();
+    for (int i = 0; i < n; ++i)
     {
-        if (s[i] == s1[i])
-            x++;
+
+        if (s.back() == t.back())
+        {
+            s.pop_back();
+            t.pop_back();
+        }
         else
+        {
             break;
+        }
     }
-    cout << s1.length() + s.length() - (2 * x) << "\n";
+
+    cout << s.size() + t.size() << nl;
+
+    successfull;
 }
