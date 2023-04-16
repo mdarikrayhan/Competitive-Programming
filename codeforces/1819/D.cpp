ba531cb0dfcd91a5d84cdc8f05ca76a3e57889fb
mdarikrayhan <mdarikrayhan@gmail.com>
2023-04-16 18:46:00 +0600

    Add solution for problem `D - Misha and Apples` Link: https://codeforces.com/contest/1819/problem/D Tags: brute force, data structures, dp, two pointers, *2800 Ref: https://codeforces.com/contest/1819/submission/202366944

--- codeforces/1819/D.cpp
+++ codeforces/1819/D.cpp
@@ -1,29 +1,54 @@
-#include<cstdio>
-#include<iostream>
+# include <bits/stdc++.h>
 using namespace std;
-const int o=2e5+10;
-int T,n,m,lst[o],col[o],asd,sz[o],v[o],s[o],ans;
-int main(){
-	for(scanf("%d",&T);T--;printf("%d\n",ans),ans=0){
-		scanf("%d%d",&n,&m);++asd;v[0]=1;
-		for(int i=1,K,lst0=0,l=1,mx;i<=n+1;s[i]=s[i-1]+v[i],++i){
-			if(i>n){
-				for(int j=n,res=0;j>=l;--j){
-					res=min(res+sz[j],m);
-					if(!sz[j]) res=m;
-					if(v[j-1]) ans=max(ans,res);
+int lst[200010],que[200010];
+bool flag[200010],vis[200010];
+vector<int> a[200010];
+inline void chkmax(int &x,const int &y){x=max(x,y);}
+int main()
+{
+	flag[0]=1;
+	int T,n,m;
+	cin>>T;
+	while(T--)
+	{
+		int sz,t=0,l=0,r=0,ans=0;
+		scanf("%d%d",&n,&m);
+		que[r++]=1;
+		for(int i=1;i<=n;i++)
+		{
+			scanf("%d",&sz);
+			a[i].resize(sz);
+			if(sz)
+			{
+				for(int &j:a[i])
+				{
+					scanf("%d",&j);
+					flag[i]|=(l<r && que[l]<=lst[j]);
+					while(l<r && que[l]<=lst[j]) l++;
+					lst[j]=i;
 				}
-				continue;
+				flag[i]|=(l<r && t>=que[l]);
+				if(flag[i]) que[r++]=i+1;
 			}
-			scanf("%d",&K);sz[i]=K;mx=v[i]=0;
-			if(!K){lst0=i;v[i]=1;continue;}
-			for(int a;K--;){
-				scanf("%d",&a);
-				if(col[a]^asd) col[a]=asd,lst[a]=0;
-				mx=max(mx,lst[a]);lst[a]=i;
-			}
-			if(lst0||mx) if(l==1||s[max(lst0,mx)-1]>s[l-2]) v[i]=1;
-			l=max(l,mx+1);
+			else t=i,flag[i]=1,que[r++]=i+1;
+		}
+		bool have0=0;
+		for(int i=n,cnt=0;i;i--)
+		{
+			bool ok=1;
+			for(int j:a[i])
+				if(vis[j]) ok=0;
+				else vis[j]=1,cnt++;
+			if(!ok) break;
+			have0|=a[i].empty();
+			if(flag[i-1]) chkmax(ans,have0?m:cnt);
+		}
+		printf("%d\n",ans);
+		for(int i=1;i<=n;i++)
+		{
+			flag[i]=0;
+			for(int j:a[i]) lst[j]=vis[j]=0;
+			a[i].clear();
 		}
 	}
 	return 0;
