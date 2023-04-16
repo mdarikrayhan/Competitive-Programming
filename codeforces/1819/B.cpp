0d54946bc42f15159115938b199c77f9b7688c4a
mdarikrayhan <mdarikrayhan@gmail.com>
2023-04-16 18:47:00 +0600

    Add solution for problem `B - The Butcher` Link: https://codeforces.com/contest/1819/problem/B Tags: geometry, greedy, implementation, sortings, two pointers, *1900 Ref: https://codeforces.com/contest/1819/submission/202367084

--- codeforces/1819/B.cpp
+++ codeforces/1819/B.cpp
@@ -1,53 +1,57 @@
 #include<bits/stdc++.h>
-
 using namespace std;
-#define int long long
-int n;
-
-bool solve(multiset<pair<int,int>> a ,multiset<pair<int,int>> b,int n,int m){
-	while(!a.empty()){
-		auto [ax,ay]=*a.rbegin();
-		auto [by,bx]=*b.rbegin();
-		if(ax==n){
-			m-=ay;
-			a.erase(a.find({ax,ay}));
-			b.erase(b.find({ay,ax}));
-		}
-		else if(by==m){
-			n-=bx;
-			a.erase(a.find({bx,by}));
-			b.erase(b.find({by,bx}));
-		}
-		else return 0;
-	}
-	return 1;
+const int N=2e5+5;
+int T;
+int n,cnt;
+bool book[N];
+long long sum,w,h,answ[3];
+long long a[N],b[N];
+struct Node
+{
+    long long val;
+    int id;
+}x[N],y[N];
+bool cmp(Node x,Node y)
+{
+    return x.val>y.val;
 }
-
-signed main(){
-	int _;
-	cin>>_;
-	while(_--){
-		cin>>n;
-		int sz=0;
-		multiset<pair<int,int>> a,b;
-		set<pair<int,int>> c;
-		for(int i=1;i<=n;i++) {
-			int aa,bb;
-			cin>>aa>>bb;
-			a.insert({aa,bb});
-			b.insert({bb,aa});
-			sz+=aa*bb;
-		}
-		int N=a.rbegin()->first,M=sz/N;
-		if(N*M==sz){
-			if(solve(a,b,N,M)) c.insert({N,M});
-		}
-		M=b.rbegin()->first,N=sz/M;
-		if(N*M==sz){
-			if(solve(a,b,N,M)) c.insert({N,M});
-		}
-		cout<<c.size()<<endl;
-		for(auto [x,y]:c) cout<<x<<" "<<y<<endl;
-	}
-	return 0;
-} 
+void check()
+{
+    int p=1,q=1;
+    long long w1=w;
+    while(w&&h)
+    {
+        while(p<=n&&book[x[p].id]) p++;
+        while(q<=n&&book[y[q].id]) q++;
+        if(x[p].val==w) h-=b[x[p].id],book[x[p].id]=1,p++;
+        else if(y[q].val==h) w-=a[y[q].id],book[y[q].id]=1,q++;
+        else break;
+    }
+    while(p<=n&&book[x[p].id]) p++;
+    while(q<=n&&book[y[q].id]) q++;
+    if(p==n+1&&q==n+1) answ[++cnt]=w1;
+    for(int i=1;i<=n;i++) book[i]=0;
+}
+void solve()
+{
+    cnt=sum=0;
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++) scanf("%lld%lld",&a[i],&b[i]),sum+=a[i]*b[i],x[i].id=y[i].id=i,x[i].val=a[i],y[i].val=b[i];
+    sort(x+1,x+n+1,cmp);
+    sort(y+1,y+n+1,cmp);
+    int p=1,q=1;
+    w=x[1].val;
+    if(sum%w==0) h=sum/w,check();
+    h=y[1].val;
+    if(sum%h==0) w=sum/h,check();
+    if(cnt==2&&answ[1]==answ[2]) cnt--;
+    printf("%d\n",cnt);
+    for(int i=1;i<=cnt;i++) printf("%lld %lld\n",answ[i],sum/answ[i]);
+    return;
+}
+int main()
+{
+    scanf("%d",&T);
+    while(T--) solve();
+    return 0;
+}
