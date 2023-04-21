e0a07b2186b70aa0fe143693ab9fee92589a8bfe
mdarikrayhan <mdarikrayhan@gmail.com>
2023-04-21 18:10:00 +0600

    Add solution for problem `D - Black Cells` Link: https://codeforces.com/contest/1821/problem/D Tags: binary search, brute force, greedy, math Ref: https://codeforces.com/contest/1821/submission/202950700

--- codeforces/1821/D.cpp
+++ codeforces/1821/D.cpp
@@ -1,30 +1,30 @@
 #include<bits/stdc++.h>
+#define int long long
 using namespace std;
-const int N=1e6+10;
-int a[N],b[N];
-struct node{
-	int sum;
-	int top;
-}e[N];
-int main(){
+const int N=2e5+5,Inf=1e18;
+int n,k,l[N],r[N];
+signed main(){
+	std::ios::sync_with_stdio(false);
+	cin.tie(0),cout.tie(0);
 	int t;
 	cin>>t;
 	while(t--){
-		int n,k;
 		cin>>n>>k;
-		for(int i=1;i<=n;i++)cin>>a[i];
-		for(int i=1;i<=n;i++)cin>>b[i];
-		int sum=0,ans=0x3f3f3f3f,cnt=0;
-		for(int i=1;i<=n;i++){
-			if(b[i]==a[i])cnt++;
-			sum+=b[i]-a[i]+1;
-			if(sum>=k){
-				int tt=sum-k;
-				ans=min(ans,(b[i]-tt)+i*2-min(tt,cnt));
+		int res=0;
+		for(int i=1;i<=n;++i)cin>>l[i];
+		for(int i=1;i<=n;++i)cin>>r[i],res+=r[i]-l[i]+1;
+		if(res<k){cout<<"-1\n";continue;}
+		if(!k){cout<<"0\n";continue;}
+		int s=0,cnt=0,ans=Inf;
+		priority_queue<int,vector<int>,greater<int>>q;
+		for(int i=1;i<=n;++i){
+			s+=r[i]-l[i]+1,++cnt,q.push(r[i]-l[i]+1);
+			if(s>=k){
+				while(s-q.top()>=k)ans=min(ans,r[i]-(s-k)+cnt*2),--cnt,s-=q.top(),q.pop();
+				ans=min(ans,r[i]-(s-k)+cnt*2);
 			}
 		}
-		if(ans==0x3f3f3f3f)puts("-1");
-		else cout<<ans<<endl;
+		cout<<ans<<"\n";
 	}
 	return 0;
 }
