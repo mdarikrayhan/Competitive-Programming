9208523a241563f29af9f3d0388d79932d1cc61e
mdarikrayhan <mdarikrayhan@gmail.com>
2023-04-23 12:52:00 +0600

    Add solution for problem `A - Magnets` Link: https://codeforces.com/contest/344/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/344/submission/203117530

--- codeforces/344/A.cpp
+++ codeforces/344/A.cpp
@@ -1,19 +1,2 @@
-#include <bits/stdc++.h>
-using namespace std;
-int main()
-{
-    int n, i, c = 0;
-    cin >> n;
-    int a[100000];
-    i = 0;
-    for (i = 0; i < n; i++)
-        cin >> a[i];
-
-    for (i = 0; i < n; i++)
-    {
-        if (a[i] != a[i + 1])
-            c++;
-    }
-    cout << c << endl;
-    return 0;
-}
+#import<cstdio>
+int x,y,n,s;main(){for(scanf("%d",&n);n--;s+=x!=y,x=y)scanf("%d",&y);printf("%d",s);}
