f2b4774e22819993ad829141532b292e3b6cc157
mdarikrayhan <mdarikrayhan@gmail.com>
2023-04-23 11:12:00 +0600

    Add solution for problem `A - Vanya and Fence` Link: https://codeforces.com/contest/677/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/677/submission/203105986

--- codeforces/677/A.cpp
+++ codeforces/677/A.cpp
@@ -1,23 +1,2 @@
-/**************************************/
-/*      Author : Md. Arik Rayhan      */
-/*  Github : github.com/mdarikrayhan  */
-/**************************************/
-#include <bits/stdc++.h>
-using namespace std;
-int main(){
-    int n,h,width=0;
-    cin>>n>>h;
-    int arr[n];
-    for(int i=0;i<n;++i){
-        cin>>arr[i];
-    }
-    for(int i=0;i<n;++i){
-        if(arr[i]<=h){
-            width++;
-        }
-        else{
-            width+=2;
-        }
-    }
-    cout<<width;
-}
+#include<iostream>
+main(){int n,h,x;std::cin>>n>>h;while(std::cin>>x)if(x>h)n++;std::cout<<n;}
