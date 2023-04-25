253d9b286f275c23c9acda9f7e751c513a44d89c
mdarikrayhan <mdarikrayhan@gmail.com>
2023-04-26 05:35:00 +0600

    Add solution for problem `A - YES or YES?` Link: https://codeforces.com/contest/1703/problem/A Tags: brute force, implementation, strings, *800 Ref: https://codeforces.com/contest/1703/submission/203498001

--- codeforces/1703/A.cpp
+++ codeforces/1703/A.cpp
@@ -1,42 +1,13 @@
-/***************************************************/
-/*            Author : Md. Arik Rayhan             */
-/*        Github : github.com/mdarikrayhan         */
-/***************************************************/
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-const char nl = '\n';
-
-#define sz(x) (int)(x).size()
-#define mp make_pair
-#define pb push_back
-#define ff first
-#define ss second
-#define lb lower_bound
-#define ub upper_bound
-#define all(x) x.begin(), x.end()
-#define ins insert
-#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-#define multicase int NoOfTestCase = 1;cin >> NoOfTestCase;for(int testcaseno=1;testcaseno<=NoOfTestCase;testcaseno++)
-bool isPrime(int n){if (n == 2 || n == 3){return true;}if (n <= 1 || n % 2 == 0 || n % 3 == 0){return false;}for (int i = 5; i * i <= n; i += 6){if (n % i == 0 || n % (i + 2) == 0){return false;}}return true;}
-bool isPalindrome(string S){string P = S;reverse(P.begin(), P.end());if (S == P) {return true;}else {return false;}}
-
-template<class T> bool ckmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
-template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
-
-int main() {
-fastio
-multicase
+int main()
 {
-string s;
-cin>>s;
-
-if(s=="yes"||s=="Yes"||s=="yEs"||s=="yeS"||s=="YEs"||s=="yES"||s=="YeS"||s=="YES"){
-    cout<<"YES"<<nl;
-}
-else{
-    cout<<"NO"<<nl;
-}
-}
-
-return 0;
-}
+int t;
+cin>>t;
+while(t--){
+char x[10];
+cin>>x;
+if(!strcmpi(x,"yes")) cout<<"YES"<<endl;
+else
+cout<<"NO"<<endl;
+}}
