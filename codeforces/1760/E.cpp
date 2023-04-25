725cd4431621b74892a965a45d6dc69ede3377ac
mdarikrayhan <mdarikrayhan@gmail.com>
2023-04-25 20:51:00 +0600

    Add solution for problem `E - Binary Inversions` Link: https://codeforces.com/contest/1760/problem/E Tags: data structures, greedy, math, *1100 Ref: https://codeforces.com/contest/1760/submission/203451304

--- codeforces/1760/E.cpp
+++ codeforces/1760/E.cpp
@@ -1,169 +1,27 @@
-/***************************************************/
-/*            Author : Md. Arik Rayhan             */
-/*        Github : github.com/mdarikrayhan         */
-/***************************************************/
-#include <bits/stdc++.h>
+#include<iostream>
+#include<algorithm>
 using namespace std;
-const char nl = '\n';
-
-typedef long long ll;
-typedef long double ld;
-typedef complex<ld> cd;
-
-typedef pair<int, int> pi;
-typedef pair<ll, ll> pl;
-typedef pair<ld, ld> pd;
-
-typedef vector<int> vi;
-typedef vector<ld> vd;
-typedef vector<ll> vl;
-typedef vector<pi> vpi;
-typedef vector<pl> vpl;
-typedef vector<cd> vcd;
-
-#define rep(i, a, b) for (int i = a; i < (b); i++)
-#define per(i, a, b) for (int i = (b)-1; i >= a; i--)
-#define print_map(m)                                                     \
-    for (const auto &[key, value] : m)                                   \
-    {                                                                    \
-        cout << '[' << key << ']' << ' ' << '=' << ' ' << value << '\n'; \
-    }
-#define print_vector(v)                          \
-    int vadnfiv = 0;                             \
-    for (const auto &value : v)                  \
-    {                                            \
-        cout << vadnfiv << ' ' << value << '\n'; \
-        vadnfiv++;                               \
-    }
-#define gcd(a, b) __gcd(a, b)
-#define lcm(a, b) (a * (b / gcd(a, b)))
-
-#define start_time                            \
-    using std::chrono::duration;              \
-    using std::chrono::duration_cast;         \
-    using std::chrono::high_resolution_clock; \
-    using std::chrono::milliseconds;          \
-    auto t1111 = high_resolution_clock::now();
-#define end_time                                            \
-    auto t2222 = high_resolution_clock::now();              \
-    duration<double, std::milli> ms_double = t2222 - t1111; \
-    std::cout << ms_double.count() << 'm' << 's' << nl;
-#define sz(x) (int)(x).size()
-#define mp make_pair
-#define pb push_back
-#define ff first
-#define ss second
-#define lb lower_bound
-#define ub upper_bound
-#define all(x) x.begin(), x.end()
-#define ins insert
-#define fastio                        \
-    ios_base::sync_with_stdio(false); \
-    cin.tie(NULL);                    \
-    cout.tie(NULL);
-#define multicase         \
-    int NoOfTestCase = 1; \
-    cin >> NoOfTestCase;  \
-    for (int testcaseno = 1; testcaseno <= NoOfTestCase; testcaseno++)
-bool isPrime(int n)
-{
-    if (n == 2 || n == 3)
-    {
-        return true;
-    }
-    if (n <= 1 || n % 2 == 0 || n % 3 == 0)
-    {
-        return false;
-    }
-    for (int i = 5; i * i <= n; i += 6)
-    {
-        if (n % i == 0 || n % (i + 2) == 0)
-        {
-            return false;
-        }
-    }
-    return true;
-}
-bool isPalindrome(string S)
-{
-    string P = S;
-    reverse(P.begin(), P.end());
-    if (S == P)
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
-}
-bool isPowerof(long long num, long long base)
-{
-    if (num <= 0)
-    {
-        return false;
-    }
-    if (num % base == 0)
-    {
-        return isPowerof(num / base, base);
-    }
-    if (num == 1)
-    {
-        return true;
-    }
-    return false;
-}
-
-template <class T>
-bool ckmin(T &a, const T &b) { return b < a ? a = b, 1 : 0; }
-template <class T>
-bool ckmax(T &a, const T &b) { return a < b ? a = b, 1 : 0; }
-
+//int a[200005];
+bool f[200005];
 int main()
 {
-    fastio
-        multicase
-    {
-        ll n, ans = 0;
-        cin >> n;
-        ll arr1[n], arr2[n], arr3[n];
-        arr1[0] = 0;
-        for (int i = 1; i <= n; i++)
-        {
-            cin >> arr3[i];
-            arr1[i] = arr1[i - 1] + arr3[i];
-        }
-        arr2[n + 1] = 0;
-        for (int i = n; i > 0; i--)
-        {
-            arr2[i] = arr2[i + 1] + (1 - arr3[i]);
-        }
-        for (int i = 1; i <= n; i++)
-        {
-            if (arr3[i] == 1)
-            {
-                ans += 1LL * arr2[i];
-            }
-        }
-
-        ll res = ans;
-        for (int i = 1; i <= n; i++)
-        {
-            long long tmp = ans;
-            if (arr3[i] == 0)
-            {
-                tmp -= arr1[i];
-                tmp += arr2[i] - 1;
-            }
-            else
-            {
-                tmp -= arr2[i];
-                tmp += arr1[i] - 1;
-            }
-            res = max(res, tmp);
-        }
-        cout<<res<<nl;
-        sort(arr3 + 1, arr3 + n + 1);
-    }
-    return 0;
+	int t,n,x,k,d;
+	cin>>t;
+	while(t--)
+	{
+		k=0;
+		long long sum=0;
+		cin>>n;
+		int b=n+1,e=0;
+		for(int i=1;i<=n;i++)
+		{
+			cin>>f[i];
+			k=f[i]+k;
+			if(!f[i]) sum+=k;
+			if(b==n+1&&!f[i]) b=i;
+			if(f[i]) e=i;
+		}
+		d=max(e-1+k-n,n-k-b);
+		cout<<sum+max(0,d)<<endl;
+	}
 }
