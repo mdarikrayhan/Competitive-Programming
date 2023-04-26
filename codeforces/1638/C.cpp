ba1b35b9378ca5a020de22379eb2d1a189a9bf87
mdarikrayhan <mdarikrayhan@gmail.com>
2023-04-27 02:12:00 +0600

    Add solution for problem `C - Inversion Graph` Link: https://codeforces.com/contest/1638/problem/C Tags: data structures, dsu, graphs, math, *1300 Ref: https://codeforces.com/contest/1638/submission/203596570

--- codeforces/1638/C.cpp
+++ codeforces/1638/C.cpp
@@ -1,36 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
-const char nl = '\n';
-void solve() {
-       int n;
-       cin>>n;
-       int ar[n+3];
-       for(int i=1;i<=n;i++)cin>>ar[i];
-       int ans = 1;
-       int val = ar[1];
-       int cnt = 1;
-       for(int i=2;i<=n;i++)
-       {
-           if(ar[i]>val)
-           {
-               if(cnt==val){
-                  ans++;
-               }
-               val = ar[i];
-           }
- 
-           cnt++;
-       }
-       cout<<ans<<endl;
-}
-
-int main() {
-ios_base::sync_with_stdio(false);
-cin.tie(NULL);
-int T = 1;
-cin >> T;
-while(T--) {
-solve();
-}
-return 0;
+main() { 
+  int t, n, x, m, r, i;
+  cin >> t;
+  while(t--){cin>>n;m=r=0;for(i=1;i<=n;i++){cin>>x;m=max(m, x);if(m==i)r++;}cout<<r<<'\n';}
 }
