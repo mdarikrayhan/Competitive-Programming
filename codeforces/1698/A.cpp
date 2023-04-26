e05fb25b344aa80f884d532d72e18101b65cdd69
mdarikrayhan <mdarikrayhan@gmail.com>
2023-04-26 06:22:00 +0600

    Add solution for problem `A - XOR Mixup` Link: https://codeforces.com/contest/1698/problem/A Tags: bitmasks, brute force, *800 Ref: https://codeforces.com/contest/1698/submission/203499918

--- codeforces/1698/A.cpp
+++ codeforces/1698/A.cpp
@@ -1,48 +1,7 @@
-#include <bits/stdc++.h>
-using namespace std;
-const char nl = '\n';
-void solve()
-{
-    int n, xo = -1;
-    cin >> n;
-    int arr[n];
-    for (int i = 0; i < n; i++)
-    {
-        cin >> arr[i];
-    }
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            if (j != i)
-            {
-                if (xo == -1)
-                {
-                    xo = arr[j];
-                }
-                else
-                {
-                    xo = (xo ^ arr[j]);
-                }
-            }
-        }
-        if (xo == arr[i])
-        {
-            break;
-        }
-    }
-    cout << xo << nl;
-}
+#include <iostream>
+#define I std::cin>>
 
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    int T = 1;
-    cin >> T;
-    while (T--)
-    {
-        solve();
-    }
-    return 0;
+int main() {
+	int x,o,r;
+	for (I x; x--; std::cout<<r<<'\n') for (I o; o--; I r);
 }
