b4adc678b9c0f0459ac7631346ef08bf53912519
mdarikrayhan <mdarikrayhan@gmail.com>
2023-04-26 06:33:00 +0600

    Add solution for problem `B - Rising Sand` Link: https://codeforces.com/contest/1698/problem/B Tags: constructive algorithms, greedy, implementation, *800 Ref: https://codeforces.com/contest/1698/submission/203500405

--- codeforces/1698/B.cpp
+++ codeforces/1698/B.cpp
@@ -1,82 +1,16 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-
-typedef long long ll;
-typedef long double ld;
-typedef complex<ld> cd;
-
-typedef pair<int, int> pi;
-typedef pair<ll, ll> pl;
-typedef pair<ld, ld> pd;
-
-typedef vector<int> vi;
-typedef vector<ld> vd;
-typedef vector<ll> vl;
-typedef vector<pi> vpi;
-typedef vector<pl> vpl;
-typedef vector<cd> vcd;
-
-template <class T>
-using pq = priority_queue<T>;
-template <class T>
-using pqg = priority_queue<T, vector<T>, greater<T>>;
-
-#define FOR(i, a, b) for (int i = a; i < (b); i++)
-#define F0R(i, a) for (int i = 0; i < (a); i++)
-#define FORd(i, a, b) for (int i = (b)-1; i >= a; i--)
-#define F0Rd(i, a) for (int i = (a)-1; i >= 0; i--)
-#define gcd(a, b) __gcd(a, b)
-#define lcm(a, b) (a*(b/gcd(a,b))
-
-#define sz(x) (int)(x).size()
-#define mp make_pair
-#define pb push_back
-#define f first
-#define s second
-#define lb lower_bound
-#define ub upper_bound
-#define all(x) x.begin(), x.end()
-#define ins insert
-
-template <class T>
-bool ckmin(T &a, const T &b)
-{
-    return b < a ? a = b, 1 : 0;
-}
-template <class T>
-bool ckmax(T &a, const T &b) { return a < b ? a = b, 1 : 0; }
-const char nl = '\n';
-void solve()
-{
-    int n, k;
-    cin >> n >> k;
-    int count = 0;
-    int arr[n];
-    for (int i = 0; i < n; i++)
-    {
-        cin >> arr[i];
-    }
-    for (int i = 1; i < n - 1; i++)
-    {
-        if (arr[i] > arr[i - 1] + arr[i + 1])
-            count++;
-    }
-    if (k == 1)
-    {
-        count = (n + 1) / 2 - 1;
-    }
-    cout << count << endl;
-}
-
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    int T = 1;
-    cin >> T;
-    while (T--)
-    {
-        solve();
-    }
-    return 0;
-}
+int main(){
+	int t;
+	cin>>t;
+while(t--){
+int n,k;
+cin>>n>>k;
+int a[n];
+for(int i=0;i<n;i++)
+cin>>a[i];int c=0;
+for(int i=1;i<n-1;i++){
+	if(a[i]>a[i-1]+a[i+1]) c++;}
+if(k==1) cout<<(n-1)/2<<endl;
+else cout<<c<<endl;
+}}
