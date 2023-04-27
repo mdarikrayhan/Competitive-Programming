967fcd4e2dd092b93f7990428689a5f404e259e6
mdarikrayhan <mdarikrayhan@gmail.com>
2023-04-27 07:36:00 +0600

    Add solution for problem `C - Omkar and Waterslide` Link: https://codeforces.com/contest/1392/problem/C Tags: greedy, implementation, *1200 Ref: https://codeforces.com/contest/1392/submission/203613522

---
+++ codeforces/1392/C.cpp
@@ -0,0 +1,2 @@
+#include<bits/stdc++.h>
+using namespace std;int a[3333333];int main(){int t;cin>>t;while(t--){int n;cin>>n;for(int i=1;i<=n;i++)cin>>a[i];long long ans=0;for(int i=1;i<=n;i++)ans+=max(0,a[i-1]-a[i]);cout<<ans<<'\n';}return 0;}
