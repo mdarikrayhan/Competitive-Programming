0fb3c8441b2ac242897e0a5bd26d4c61bc8255e5
mdarikrayhan <mdarikrayhan@gmail.com>
2023-04-27 07:42:00 +0600

    Add solution for problem `C - Move Brackets` Link: https://codeforces.com/contest/1374/problem/C Tags: greedy, strings, *1000 Ref: https://codeforces.com/contest/1374/submission/203613847

---
+++ codeforces/1374/C.cpp
@@ -0,0 +1,2 @@
+#include<bits/stdc++.h>
+using namespace std;int main(){int t;cin>>t;while(t--){int n;cin>>n;string s;cin>>s;int a=0;for(int i=0; i<n; i++){if(s[i]=='(')a++;else{if(a>0&&s[i]==')'){a--;}}}cout<<a<<endl;}}
