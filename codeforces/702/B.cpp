b0d34539b939d16770a8cc29852f5efec122e6d1
mdarikrayhan <mdarikrayhan@gmail.com>
2024-01-25 17:19:00 +0600

    Add solution for problem `B - Powers of Two` Link: https://codeforces.com/contest/702/problem/B Tags: brute force, data structures, implementation, math, *1500 Ref: https://codeforces.com/contest/702/submission/243253685

--- codeforces/702/B.cpp
+++ codeforces/702/B.cpp
@@ -1,32 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
-const char nl = '\n';
-#define ll long long
+map<int, int> f;
+int i, n, x;
+long long r;
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
-    ll n, x, ans = 0;
-    unordered_map<int, int> mp;
-    vector<int> p2(31);
-    for (int i = 0; i < 31; ++i)
-    {
-        p2[i] = 1 << i;
-    }
-
     cin >> n;
     while (n--)
     {
         cin >> x;
-        for (auto i : p2)
+        for (i = 0; i < 31; ++i)
         {
-            ans += mp[i - x];
+            r += f[(1 << i) - x];
         }
-        mp[x]++;
+        f[x]++;
     }
-    cout << ans << nl;
-
-    return 0;
+    cout << r;
 }
