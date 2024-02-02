888195c20b1cc6329ae02343ea0c8b8ada6b64cf
mdarikrayhan <mdarikrayhan@gmail.com>
2024-02-02 10:46:00 +0600

    Add solution for problem `A - Don't Try to Count` Link: https://codeforces.com/contest/1881/problem/A Tags: brute force, strings, *800 Ref: https://codeforces.com/contest/1881/submission/244494476

--- codeforces/1881/A.cpp
+++ codeforces/1881/A.cpp
@@ -3,8 +3,8 @@
 /*        Github : github.com/mdarikrayhan         */
 /***************************************************/
 
-#pragma GCC optimization("O3")
-#pragma GCC optimization("unroll-loops")
+#pragma GCC optimization ("O3")
+#pragma GCC optimization ("unroll-loops")
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -37,6 +37,7 @@ typedef unordered_map<ll, ll> umll;
 typedef unordered_map<char, int> umci;
 typedef unordered_map<string, int> umsi;
 
+
 #define YES cout << "YES" << nl;
 #define NO cout << "NO" << nl;
 
@@ -55,36 +56,12 @@ typedef unordered_map<string, int> umsi;
 #define gcd(a, b) __gcd(a, b)
 #define lcm(a, b) (a * (b / gcd(a, b)))
 
-#define start_time                            \
-    using std::chrono::duration;              \
-    using std::chrono::duration_cast;         \
-    using std::chrono::high_resolution_clock; \
-    using std::chrono::milliseconds;          \
-    auto t1111 = high_resolution_clock::now();
-#define end_time                                            \
-    auto t2222 = high_resolution_clock::now();              \
-    duration<double, std::milli> ms_double = t2222 - t1111; \
-    std::cout << ms_double.count() << 'm' << 's' << nl;
-#define spring                              \
-    for (int num = 0; num < 9185319; num++) \
-    {                                       \
-        string a = "Arik";                  \
-        a.pop_back();                       \
-        a.pop_back();                       \
-        a.pop_back();                       \
-        a.pop_back();                       \
-    }
-#define fastio                        \
-    ios_base::sync_with_stdio(false); \
-    cin.tie(NULL);                    \
-    cout.tie(NULL);
-#define fastIO            \
-    spring sievefactor(); \
-    fastio
-#define multicase         \
-    int NoOfTestCase = 1; \
-    cin >> NoOfTestCase;  \
-    for (int testcaseno = 1; testcaseno <= NoOfTestCase; testcaseno++)
+#define start_time using std::chrono::duration;using std::chrono::duration_cast; using std::chrono::high_resolution_clock; using std::chrono::milliseconds; auto t1111 = high_resolution_clock::now();
+#define end_time auto t2222 = high_resolution_clock::now(); duration<double, std::milli> ms_double = t2222 - t1111;std::cout << ms_double.count() << 'm' << 's' << nl;
+#define spring for (int num = 0; num < 9185319; num++){string a = "Arik";a.pop_back();a.pop_back();a.pop_back();a.pop_back();}
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define fastIO spring sievefactor(); fastio
+#define multicase int NoOfTestCase = 1;cin >> NoOfTestCase;for (int testcaseno = 1; testcaseno <= NoOfTestCase; testcaseno++)
 
 #define deb(x) cout << #x << "=" << x << endl
 #define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
@@ -118,20 +95,17 @@ void __print(queue<T> q);
 template <typename T, typename... U>
 void __print(priority_queue<T, U...> q);
 template <typename A>
-void __print(const A &x)
-{
+void __print(const A &x) {
     bool first = true;
     cerr << '{';
-    for (const auto &i : x)
-    {
+    for (const auto &i : x) {
         cerr << (first ? "" : ","), __print(i);
         first = false;
     }
     cerr << '}';
 }
 template <typename A, typename B>
-void __print(const pair<A, B> &p)
-{
+void __print(const pair<A, B> &p) {
     cerr << '(';
     __print(p.first);
     cerr << ',';
@@ -139,21 +113,16 @@ void __print(const pair<A, B> &p)
     cerr << ')';
 }
 template <typename... A>
-void __print(const tuple<A...> &t)
-{
+void __print(const tuple<A...> &t) {
     bool first = true;
     cerr << '(';
-    apply([&first](const auto &...args)
-          { ((cerr << (first ? "" : ","), __print(args), first = false), ...); },
-          t);
+    apply([&first](const auto &...args) { ((cerr << (first ? "" : ","), __print(args), first = false), ...); }, t);
     cerr << ')';
 }
 template <typename T>
-void __print(stack<T> s)
-{
+void __print(stack<T> s) {
     vector<T> debugVector;
-    while (!s.empty())
-    {
+    while (!s.empty()) {
         T t = s.top();
         debugVector.push_back(t);
         s.pop();
@@ -162,11 +131,9 @@ void __print(stack<T> s)
     __print(debugVector);
 }
 template <typename T>
-void __print(queue<T> q)
-{
+void __print(queue<T> q) {
     vector<T> debugVector;
-    while (!q.empty())
-    {
+    while (!q.empty()) {
         T t = q.front();
         debugVector.push_back(t);
         q.pop();
@@ -174,25 +141,21 @@ void __print(queue<T> q)
     __print(debugVector);
 }
 template <typename T, typename... U>
-void __print(priority_queue<T, U...> q)
-{
+void __print(priority_queue<T, U...> q) {
     vector<T> debugVector;
-    while (!q.empty())
-    {
+    while (!q.empty()) {
         T t = q.top();
         debugVector.push_back(t);
         q.pop();
     }
     __print(debugVector);
 }
-
+ 
 template <typename T>
-void __print(const T arr[], int size)
-{
+void __print(const T arr[], int size) {
     bool first = true;
     cerr << '[';
-    for (int i = 0; i < size; ++i)
-    {
+    for (int i = 0; i < size; ++i) {
         cerr << (first ? "" : ",");
         __print(arr[i]);
         first = false;
@@ -201,126 +164,93 @@ void __print(const T arr[], int size)
 }
 void _print() { cerr << "]\n"; }
 template <typename Head, typename... Tail>
-void _print(const Head &H, const Tail &...T)
-{
+void _print(const Head &H, const Tail &...T) {
     __print(H);
     if (sizeof...(T))
         cerr << ", ";
     _print(T...);
 }
-
+ 
 #ifndef ONLINE_JUDGE
-#define debug(...)                                                  \
-    cerr << "Line:" << __LINE__ << " [" << #__VA_ARGS__ << "] = ["; \
-    _print(__VA_ARGS__)
-#define debug_array(arr, size)                             \
-    cerr << "Line:" << __LINE__ << " [" << #arr << "] = "; \
-    __print(arr, size)
+#define debug(...) cerr << "Line:" << __LINE__ << " [" << #__VA_ARGS__ << "] = ["; _print(__VA_ARGS__)
+#define debug_array(arr, size) cerr << "Line:" << __LINE__ << " [" << #arr << "] = "; __print(arr, size)
 #else
 #define debug(...)
 #define debug_array(...)
 #endif
 
-// Bitwise Sieve
+//Bitwise Sieve
 const int pmxsz = 100000000;
 int status[(pmxsz / 32) + 2];
-// int prime[5761455 + 5], noofprime = 0;
+//int prime[5761455 + 5], noofprime = 0;
 inline bool Bit_Check(int N, int pos) { return (bool)(N & (1 << pos)); }
 inline int Bit_Set(int N, int pos) { return N = N | (1 << pos); }
 inline bool PrimeCheck(int i) { return 1 ^ (bool)(Bit_Check(status[i >> 5], i & 31)); }
 inline void PrimeSet(int i) { status[i >> 5] = Bit_Set(status[i >> 5], i & 31); }
-inline void Mark(int i, int N)
-{
-    for (int j = i * i; j <= N; j += (i << 1))
-        PrimeSet(j);
-}
-// void sieve(int N=100000000) { int i, j, sqrtN; sqrtN = int(sqrt(N)); for (i = 5; i <= sqrtN; i += 6) { if (PrimeCheck(i)) { Mark(i, N); } if (PrimeCheck(i + 2)) { Mark(i + 2, N); } } prime[noofprime++] = 2; prime[noofprime++] = 3; for (i = 5; i <= N; i += 6) { if (PrimeCheck(i)) { prime[noofprime++] = i; } if (PrimeCheck(i + 2)) { prime[noofprime++] = i + 2; } } }
+inline void Mark(int i, int N) { for (int j = i * i; j <= N; j += (i << 1)) PrimeSet(j); }
+//void sieve(int N=100000000) { int i, j, sqrtN; sqrtN = int(sqrt(N)); for (i = 5; i <= sqrtN; i += 6) { if (PrimeCheck(i)) { Mark(i, N); } if (PrimeCheck(i + 2)) { Mark(i + 2, N); } } prime[noofprime++] = 2; prime[noofprime++] = 3; for (i = 5; i <= N; i += 6) { if (PrimeCheck(i)) { prime[noofprime++] = i; } if (PrimeCheck(i + 2)) { prime[noofprime++] = i + 2; } } }
 
-// Single Prime Check using Miller Rabin
-ull binpower(ull base, ull e, ull mod)
-{
-    ull result = 1;
-    base %= mod;
-    while (e)
-    {
-        if (e & 1)
-            result = (u128)result * base % mod;
-        base = (u128)base * base % mod;
-        e >>= 1;
-    }
-    return result;
-}
-bool check_composite(ull n, ull a, ull d, int s)
-{
-    ull x = binpower(a, d, n);
-    if (x == 1 || x == n - 1)
-        return false;
-    for (int r = 1; r < s; r++)
-    {
-        x = (u128)x * x % n;
-        if (x == n - 1)
-            return false;
-    }
-    return true;
-};
-bool MillerRabin(ull n)
-{
-    if (n < 2)
-        return false;
-    int r = 0;
-    ull d = n - 1;
-    while ((d & 1) == 0)
-    {
-        d >>= 1;
-        r++;
-    }
-    for (int a : {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37})
-    {
-        if (n == a)
-            return true;
-        if (check_composite(n, a, d, r))
-            return false;
-    }
-    return true;
-}
+//Single Prime Check using Miller Rabin
+ull binpower(ull base, ull e, ull mod) { ull result = 1; base %= mod; while (e) { if (e & 1) result = (u128)result * base % mod; base = (u128)base * base % mod; e >>= 1; } return result; }
+bool check_composite(ull n, ull a, ull d, int s) { ull x = binpower(a, d, n); if (x == 1 || x == n - 1) return false; for (int r = 1; r < s; r++) { x = (u128)x * x % n; if (x == n - 1) return false; } return true; };
+bool MillerRabin(ull n) { if (n < 2) return false; int r = 0; ull d = n - 1; while ((d & 1) == 0) { d >>= 1; r++; } for (int a : {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37}) { if (n == a) return true; if (check_composite(n, a, d, r)) return false; } return true; }
 
-// Prime Factorization using SPFs and Sieve
-// const int MAXN = 10e6+5; int spf[MAXN]; vector<int> factor[MAXN]; inline vector<int> getFactorization(int x) { vector<int> ret; while (x != 1) { ret.push_back(spf[x]); x = x / spf[x]; } return ret; }
-// void sievefactor() { spf[1] = 1; for (int i = 2; i <= MAXN; i++) { spf[i] = i; } for (int i = 4; i <= MAXN; i += 2) { spf[i] = 2; } for (int i = 3; i * i < MAXN; i++) { if (spf[i] == i) { for (int j = i * i; j < MAXN; j += i) if (spf[j] == j) { spf[j] = i; } } } for (int i = 1; i <= MAXN; i++) { factor[i] = getFactorization(i); } }
+//Prime Factorization using SPFs and Sieve
+//const int MAXN = 10e6+5; int spf[MAXN]; vector<int> factor[MAXN]; inline vector<int> getFactorization(int x) { vector<int> ret; while (x != 1) { ret.push_back(spf[x]); x = x / spf[x]; } return ret; }
+//void sievefactor() { spf[1] = 1; for (int i = 2; i <= MAXN; i++) { spf[i] = i; } for (int i = 4; i <= MAXN; i += 2) { spf[i] = 2; } for (int i = 3; i * i < MAXN; i++) { if (spf[i] == i) { for (int j = i * i; j < MAXN; j += i) if (spf[j] == j) { spf[j] = i; } } } for (int i = 1; i <= MAXN; i++) { factor[i] = getFactorization(i); } }
 
 ull rangesum(ll L, ll R) { return ((L + R) * (abs(R - L) + 1)) / 2; }
-bool isPalindrome(string S)
-{
-    string P = S;
-    reverse(P.begin(), P.end());
-    return S == P ? true : false;
-}
+bool isPalindrome(string S) { string P = S; reverse(P.begin(), P.end()); return S == P ? true : false; }
 bool isPowerof(ll num, ll base) { return (num > 0 && num % base == 0) ? isPowerof(num / base, base) : num == 1; }
 bool isPowerofTwo(ll num) { return (num > 0 && (num & (num - 1)) == 0) ? true : false; }
-int isSubstring(string main, string sub) { return main.find(sub) != string::npos ? main.find(sub) : -1; }
 
 template <class T>
 bool ckmin(T &a, const T &b) { return b < a ? a = b, 1 : 0; }
 template <class T>
 bool ckmax(T &a, const T &b) { return a < b ? a = b, 1 : 0; }
 
+int isSubstring(string s1, string s2)
+{
+    // using find method to check if s1 is
+    // a substring of s2
+    if (s2.find(s1) != string::npos)
+        return s2.find(s1);
+    return -1;
+}
+
 void solve()
 {
-    int n, m, ans = 0;
+    int n, m;
     cin >> n >> m;
     string s1, s2;
     cin >> s1 >> s2;
-    while (s1.size() <= 2 * n * m)
+    int ans = 0;
+    if(isSubstring(s2, s1) != -1)
     {
-        if (isSubstring(s1, s2) != -1)
-        {
-            cout << ans << nl;
-            return;
-        }
-        s1 += s1;
-        ans++;
+        cout<<ans<<nl;
+        return;
     }
-    cout << -1 << nl;
+    bool flag = false;    
+    do
+    {   
+        ans++;
+        s1 += s1;
+        //cout<<ans<<" "<<s1<<" "<<s2<<nl;
+        
+        if(isSubstring(s2, s1) != -1)
+        {
+            flag = true;
+            break;
+        }
+    }while((s1.size() <= s2.size()+m));
+    if(flag)
+    {
+        cout<<ans<<nl;
+    }
+    else
+    {
+        cout<<-1<<nl;
+    }
 }
 
 int32_t main()
