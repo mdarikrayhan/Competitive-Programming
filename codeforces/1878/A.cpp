797948139af3e96073e4e6820b631341f973f224
mdarikrayhan <mdarikrayhan@gmail.com>
2024-02-02 11:06:00 +0600

    Add solution for problem `A - How Much Does Daytona Cost?` Link: https://codeforces.com/contest/1878/problem/A Tags: greedy, *800 Ref: https://codeforces.com/contest/1878/submission/244495798

--- codeforces/1878/A.cpp
+++ codeforces/1878/A.cpp
@@ -180,15 +180,15 @@ void _print(const Head &H, const Tail &...T) {
 #endif
 
 //Bitwise Sieve
-// const int pmxsz = 100000000;
-// int status[(pmxsz / 32) + 2];
-// int prime[5761455 + 5], noofprime = 0;
-// inline bool Bit_Check(int N, int pos) { return (bool)(N & (1 << pos)); }
-// inline int Bit_Set(int N, int pos) { return N = N | (1 << pos); }
-// inline bool PrimeCheck(int i) { return 1 ^ (bool)(Bit_Check(status[i >> 5], i & 31)); }
-// inline void PrimeSet(int i) { status[i >> 5] = Bit_Set(status[i >> 5], i & 31); }
-// inline void Mark(int i, int N) { for (int j = i * i; j <= N; j += (i << 1)) PrimeSet(j); }
-// void sieve(int N=100000000) { int i, j, sqrtN; sqrtN = int(sqrt(N)); for (i = 5; i <= sqrtN; i += 6) { if (PrimeCheck(i)) { Mark(i, N); } if (PrimeCheck(i + 2)) { Mark(i + 2, N); } } prime[noofprime++] = 2; prime[noofprime++] = 3; for (i = 5; i <= N; i += 6) { if (PrimeCheck(i)) { prime[noofprime++] = i; } if (PrimeCheck(i + 2)) { prime[noofprime++] = i + 2; } } }
+const int pmxsz = 100000000;
+int status[(pmxsz / 32) + 2];
+//int prime[5761455 + 5], noofprime = 0;
+inline bool Bit_Check(int N, int pos) { return (bool)(N & (1 << pos)); }
+inline int Bit_Set(int N, int pos) { return N = N | (1 << pos); }
+inline bool PrimeCheck(int i) { return 1 ^ (bool)(Bit_Check(status[i >> 5], i & 31)); }
+inline void PrimeSet(int i) { status[i >> 5] = Bit_Set(status[i >> 5], i & 31); }
+inline void Mark(int i, int N) { for (int j = i * i; j <= N; j += (i << 1)) PrimeSet(j); }
+//void sieve(int N=100000000) { int i, j, sqrtN; sqrtN = int(sqrt(N)); for (i = 5; i <= sqrtN; i += 6) { if (PrimeCheck(i)) { Mark(i, N); } if (PrimeCheck(i + 2)) { Mark(i + 2, N); } } prime[noofprime++] = 2; prime[noofprime++] = 3; for (i = 5; i <= N; i += 6) { if (PrimeCheck(i)) { prime[noofprime++] = i; } if (PrimeCheck(i + 2)) { prime[noofprime++] = i + 2; } } }
 
 //Single Prime Check using Miller Rabin
 ull binpower(ull base, ull e, ull mod) { ull result = 1; base %= mod; while (e) { if (e & 1) result = (u128)result * base % mod; base = (u128)base * base % mod; e >>= 1; } return result; }
