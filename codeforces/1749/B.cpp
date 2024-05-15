4392a3e01c98b4695c5405c62f69f4b29ed7a4a2
mdarikrayhan <mdarikrayhan@gmail.com>
2024-05-15 10:24:00 +0600

    Add solution for problem `B - Death's Blessing` Link: https://codeforces.com/contest/1749/problem/B Tags: greedy, *900 Ref: https://codeforces.com/contest/1749/submission/261001716

--- codeforces/1749/B.cpp
+++ codeforces/1749/B.cpp
@@ -1,2 +1,47 @@
-#include<bits/stdc++.h>
-using namespace std;int T;main(){cin>>T;while(T--){long long int i,n,mx=0,x,ans=0;cin>>n;for(i=0;i<n;i++)cin>>x,ans+=x;for(i=0;i<n;i++)cin>>x,ans+=x,mx=max(mx,x);cout<<ans-mx<<"\n";}}
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ull     unsigned long long
+#define ll      long long
+#define all     s.begin(), s.end()
+#define pb      push_back
+#define mp      make_pair
+
+void solve(){
+   int n;
+   cin >> n;
+   vector<int> v(n), v2(n);
+   int maximum = 0;
+   ull cnt = 0;
+   ull cnt2 = 0;
+
+   for(int i = 0; i < n; i++){
+        cin >> v[i];
+        cnt += v[i];
+   }
+
+   for(int i = 0; i < n; i++){
+    cin >> v2[i];
+    if(v2[i] > maximum){
+        maximum = v2[i];
+    }
+    cnt2 += v2[i];
+   }
+
+   cnt2 -= maximum;
+   cout << cnt + cnt2;
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+        cout << endl;
+    }
+    return 0;
+}
+
+// CPJ
