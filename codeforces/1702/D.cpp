b9f95b764267e28ad3265e0c4a4fb0b5f27158f2
mdarikrayhan <mdarikrayhan@gmail.com>
2024-05-15 14:14:00 +0600

    Add solution for problem `D - Not a Cheap String` Link: https://codeforces.com/contest/1702/problem/D Tags: greedy, *1000 Ref: https://codeforces.com/contest/1702/submission/261021987

--- codeforces/1702/D.cpp
+++ codeforces/1702/D.cpp
@@ -1,32 +1,41 @@
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
-int main()
-{
-long long i,h,n,t;
-char k;
-string s;
-cin>>t;
-while(t--)
-{
-h=0;
-k='a';
-cin>>s>>n;
-while(k<='z')
-{
-for(i=0;i<s.size();i++)
-{
-if(s[i]==k)
-{
-h=h+(s[i]-'a'+1);
-if(h>n)
-{
-s.erase(s.begin()+i);
-i--;
+
+auto solve(){
+    string s;cin >> s;
+    int p;cin >> p;
+    map< char , int > freq;
+    int cost = 0;
+
+    for(auto &it : s) freq[ it ]++ , cost = cost + ( it - 'a' + 1 );
+
+    for (char i = 'z'; i >= 'a' ; --i) {
+        if( cost <= p )
+            goto jump;
+        while( freq[ i ] > 0 ){
+            cost = cost - ( i - 'a' + 1 );
+            freq[ i ] --;
+            if( cost <= p )
+                goto jump;
+        }
+
+    }
+    jump:;
+
+
+    for (int i = 0; s[ i ] ; ++i) {
+
+        if( freq[ s[ i ] ] ){
+            cout << s[ i ];
+            freq[ s[ i ] ] --;
+        }
+
+    }
+    cout << '\n';
 }
-}
-}
-k++;
-}
-cout<<s<<endl;
-}
+
+signed main(){
+    ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+    int tt = 1;cin >> tt ;
+    while ( tt-- ) solve();
 }
