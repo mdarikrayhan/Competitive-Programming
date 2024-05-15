63b2bd9ef1152b6a7dee22bab1f248f2f2b0e666
mdarikrayhan <mdarikrayhan@gmail.com>
2024-05-15 14:15:00 +0600

    Add solution for problem `D - X-Sum` Link: https://codeforces.com/contest/1676/problem/D Tags: brute force, greedy, implementation, *1000 Ref: https://codeforces.com/contest/1676/submission/261022172

--- codeforces/1676/D.cpp
+++ codeforces/1676/D.cpp
@@ -1,28 +1,61 @@
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
-int T;
-int N,M;
-int A[200][200];
-main()
-{
+#define ll long long
+#define yes cout << "YES\n";
+#define no  cout << "NO\n";
+#define f first
+#define s second
+#define pb push_back
 
-cin>>T;
-for(;T--;)
-{
-cin>>N>>M;
-for(int i=0;i<N;i++)for(int j=0;j<M;j++)cin>>A[i][j];
-int ans=0;
-for(int i=0;i<N;i++)for(int j=0;j<M;j++)
-{
-int now=-A[i][j];
-for(int k=0;k<N;k++)
-{
-int d=abs(i-k);
-if(j>=d)now+=A[k][j-d];
-if(j+d<M)now+=A[k][j+d];
+void run_case(){
+    int n, m, mx = 0;
+    cin >> n >> m;
+    int a[n + 1][m + 1];
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < m; j++)
+            cin >> a[i][j];
+
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            int sum = a[i][j];
+            int x = i, u = j, z = j;
+            while(x >= 0){
+                if(u >= 0){
+                    sum += a[x][u];
+                    u--;
+                }
+                if(z <= m - 1){
+                    sum += a[x][z];
+                    z++;
+                }
+                x--;
+            }
+            x = i, u = j, z = j;
+            while(x <= n - 1){
+                if(u >= 0){
+                    sum += a[x][u];
+                    u--;
+                }
+                if(z <= m - 1){
+                    sum += a[x][z];
+                    z++;
+                }
+                x++;
+            }
+            sum -= (4 * a[i][j]);
+            mx = max(mx, sum);
+            sum = 0;
+        }
+    }
+    
+    cout << mx << '\n';
 }
-ans=max(ans,now);
-}
-cout<<ans<<"\n";
-}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t = 1;
+    cin >> t;
+    while (t-- > 0) run_case(); 
 }
