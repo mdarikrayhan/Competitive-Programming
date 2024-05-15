40d710cfbffca20d1187d12faaeff9b42bb6107b
mdarikrayhan <mdarikrayhan@gmail.com>
2024-05-15 17:21:00 +0600

    Add solution for problem `A - Chat room` Link: https://codeforces.com/contest/58/problem/A Tags: greedy, strings, *1000 Ref: https://codeforces.com/contest/58/submission/261044136

--- codeforces/58/A.cpp
+++ codeforces/58/A.cpp
@@ -1,11 +1,34 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
+#define ll long long
+const int N=1e5+10;
 using namespace std;
+void solve()
+{
+    string ans="hello";
+    string s;
+    int cnt=0;
+    cin>>s;
+    for(auto ss:s)
+    {
+        if(ss==ans[cnt]&&cnt<ans.size())
+            cnt++;
+    }
+    if(cnt==ans.size())
+        cout<<"YES";
+    else
+        cout<<"NO";
+}
 int main()
 {
-    char ch, a[6]="hello";
-    int i = 0;
-    while (cin >> ch)
-        if (a[i] == ch)
-            i++;
-    cout << (i == 5 ? "YES" : "NO");
-}
+	 ios::sync_with_stdio(false);
+cin.tie(0);
+    cout.tie(0);
+	int t=1;
+	//cin>>t;
+	while(t--)
+	{
+		solve();
+	}
+ }
+
+  			   	 	 	 	  	 	 			 		  	
