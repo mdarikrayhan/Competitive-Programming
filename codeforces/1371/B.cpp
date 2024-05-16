b13507b52357341286aa6071b2821ee7a54d9def
mdarikrayhan <mdarikrayhan@gmail.com>
2024-05-17 03:20:00 +0600

    Add solution for problem `B - Magical Calendar` Link: https://codeforces.com/contest/1371/problem/B Tags: math, *1200 Ref: https://codeforces.com/contest/1371/submission/261254243

--- codeforces/1371/B.cpp
+++ codeforces/1371/B.cpp
@@ -1,14 +1,31 @@
-#include<iostream>
+#include <bits/stdc++.h>
+
 using namespace std;
 
-int main()
-{
-   long long int t,n,r,c;
-cin>>t;
-while(t--){
-cin>>n>>r;
-c=min(n*(n-1)/2+1,r*(r+1)/2);
-cout<<c<<endl;
-}
-    return 0;
+using ll = long long;
+
+#define int ll
+#define all(a) a.begin(), a.end()
+#define rall(a) a.rbegin(), a.rend()
+
+void dbg_out() { cerr << endl; }
+template <typename H, typename... T>
+void dbg_out(H h, T... t) { cerr << ' ' << h; dbg_out(t...); }
+#define dbg(...) { cerr << #__VA_ARGS__ << ':'; dbg_out(__VA_ARGS__); }
+
+signed main() {
+	cin.tie(0)->sync_with_stdio(false);
+	cin.exceptions(cin.failbit);
+
+	int tt; cin>>tt;
+	while (tt--) {
+		int n, r; cin>>n>>r;
+
+		r = min(r, n);
+		int res = r*(r+1)/2;
+		if (r >= n) res -= (r-1);
+		cout<<res<<'\n';
+	}
+
+	return 0;
 }
