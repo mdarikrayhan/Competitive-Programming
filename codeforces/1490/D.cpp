96a1dbc2d42353003760dcf51819fef58f56ac21
mdarikrayhan <mdarikrayhan@gmail.com>
2024-05-17 03:17:00 +0600

    Add solution for problem `D - Permutation Transformation` Link: https://codeforces.com/contest/1490/problem/D Tags: dfs and similar, divide and conquer, implementation, *1200 Ref: https://codeforces.com/contest/1490/submission/261254098

--- codeforces/1490/D.cpp
+++ codeforces/1490/D.cpp
@@ -1,26 +1,74 @@
-#import<bits/stdc++.h>
+#include<bits/stdc++.h>
+
 using namespace std;
-int i,n,t,a[105],b[105];
-void f(int d,int x,int y)
+#define ll long long
+#define nl '\n'
+#define all(a) a.begin(),a.end()
+#define allr(a) a.rbegin(),a.rend()
+const ll MOD = 1e9 + 7,  N=105, X = 12e3;
+ll n, m;
+vector<int>adj[N];
+int build(vector<ll>v,int idx,int sz)
 {
-	int m=x;
-	if(x>y)return;
-	for(i=x;i<=y;i++)
-	{
-		if(a[i]>a[m])m=i;
-		b[m]=d;
-	}
-	f(d+1,x,m-1);
-	f(d+1,m+1,y);
+    if(idx>sz)
+        return 0;
+    if(idx==sz)
+        return v[idx];
+    int x;
+    x = max_element(v.begin() + idx, v.begin() + sz+1) - v.begin();
+
+    int z =build(v ,idx,x-1);
+    if(z)
+    adj[v[x]].push_back(z);
+
+    int a =build(v,x+1,sz);
+    if(a)
+      adj[v[x]].push_back(a);
+    return v[x];
 }
-main()
+vector<int>ans(N);
+void rec(int idx,int cnt)
 {
-	for(cin>>t;t--;)
-	{
-		cin>>n;
-		for(i=0;i++<n;)cin>>a[i];
-		f(0,1,n);
-		for(i=0;i++<n;)cout<<b[i]<<' ';
-		cout<<endl;
-	}
+    if(idx==0)
+        return;
+    ans[idx]= cnt;
+    for(auto&x:adj[idx])
+    {
+       rec(x,cnt+1);
+    }
+}
+void sol() {
+    cin>>n;
+    vector<ll>v(n);
+    for(int i=0;i<n;i++)
+    {
+        cin>>v[i];
+        adj[i].clear();
+        ans[v[i]]=0;
+    }
+    build(v,0,n-1);
+    rec(n,0);
+    for(int i=1;i<=n;i++)cout<<ans[v[i-1]]<<" ";
+}
+
+
+void file() {
+#ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+#endif
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    file();
+    int tests = 1;
+    cin >> tests;
+    while (tests--) {
+        sol();
+        cout << nl;
+    }
+    return 0;
 }
