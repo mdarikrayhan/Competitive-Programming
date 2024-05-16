bb7f8f06da5161e1fa7152225453c8a347f02872
mdarikrayhan <mdarikrayhan@gmail.com>
2024-05-17 05:27:00 +0600

    Add solution for problem `H1 - Maximum Crossings (Easy Version)` Link: https://codeforces.com/contest/1676/problem/H1 Tags: brute force, *1400 Ref: https://codeforces.com/contest/1676/submission/261259727

---
+++ codeforces/1676/H1.cpp
@@ -0,0 +1,34 @@
+//------ADIL SAYYED--------
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define pb push_back
+#define all(v) v.begin(),v.end()
+#define vin(v,n) for(ll i=0;i<n;i++) cin>>v[i]
+#define rep(i, n) for (ll i = 0; i < n; i++)
+#define forf(i, a, b) for (ll i = a; i < b; i++)
+#define forb(i, s, e) for (ll i = s; i >= e; i--)
+#define vt vector<ll>
+#define vp vector<pair<long long, long long>>
+#define srt(v) sort(v.begin(), v.end())
+#define rev(v) reverse(v.begin(), v.end())
+#define pn cout<<"NO"<<'\n'
+#define py cout<<"YES"<<'\n'
+
+void solve(){
+    ll n; cin>>n;
+    vector<ll> v(n);
+    for(ll i=0;i<n;i++) cin>>v[i];
+    ll c=0;
+    for(ll i=0;i<n;i++)
+        for(ll j=i+1;j<n;j++) if(v[j]<=v[i]) c++;
+    cout<<c<<'\n';   
+}
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    ll t; cin>>t;
+    while(t--)
+    solve();
+    return 0;
+}
