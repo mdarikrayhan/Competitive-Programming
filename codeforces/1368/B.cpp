5a99750e42bd0751f608fba029a891370c344a6d
mdarikrayhan <mdarikrayhan@gmail.com>
2024-05-17 03:18:00 +0600

    Add solution for problem `B - Codeforces Subsequences` Link: https://codeforces.com/contest/1368/problem/B Tags: brute force, constructive algorithms, greedy, math, strings, *1500 Ref: https://codeforces.com/contest/1368/submission/261254153

---
+++ codeforces/1368/B.cpp
@@ -0,0 +1,61 @@
+#define print_debug true
+
+#include "bits/stdc++.h"
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+const long long MOD = 1000000007;
+
+using namespace __gnu_pbds;
+using namespace std;
+
+typedef tree<int, null_type, less<int>, rb_tree_tag,
+             tree_order_statistics_node_update>
+    indexed_set;
+
+typedef long long ll;
+
+template<typename T> istream& operator>>(istream& in, vector<T>& a) {for(auto &x : a) in >> x; return in;};
+template<typename T1, typename T2> istream& operator>>(istream& in, pair<T1, T2>& x) {return in >> x.first >> x.second;}
+
+template<typename T1, typename T2> ostream& operator<<(ostream& out, const pair<T1, T2>& x) {return out << x.first << ' ' << x.second;}
+template<typename T> ostream& operator<<(ostream& out, vector<T>& a) {for(auto &x : a) out << x << ' '; return out;};
+template<typename T> ostream& operator<<(ostream& out, vector<vector<T>>& a) {for(auto &x : a) out << x << '\n'; return out;};
+template<typename T1, typename T2> ostream& operator<<(ostream& out, vector<pair<T1, T2>>& a) {for(auto &x : a) out << x << '\n'; return out;};
+
+void fileIO(string name) {
+    freopen((name + ".in").c_str(), "r", stdin);
+    freopen((name + ".out").c_str(), "w", stdout);
+}
+
+int main() {
+    iostream::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll n;
+    cin >> n;
+    string s = "codeforces";
+    vector<int> v(10, 1);
+    while (true) {
+        for (int i = 0; i < 10; ++i) {
+            ll a = 1;
+            for (int j = 0; j < 10; ++j) {
+                a *= v[j];
+            }
+            if (a >= n) {
+                for (int j = 0; j < 10; ++j) {
+                    for (int k = 0; k < v[j]; ++k) {
+                        cout << s[j];
+                    }
+                }
+                cout << "\n";
+                return 0;
+            }
+
+            v[i]++;
+        }
+    }
+
+    
+}
