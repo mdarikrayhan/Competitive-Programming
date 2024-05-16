6b91de42030c264b6381e2c4f9c7d732fd13d7a7
mdarikrayhan <mdarikrayhan@gmail.com>
2024-05-17 03:05:00 +0600

    Add solution for problem `B - Shuffle` Link: https://codeforces.com/contest/1366/problem/B Tags: math, two pointers, *1300 Ref: https://codeforces.com/contest/1366/submission/261253443

--- codeforces/1366/B.cpp
+++ codeforces/1366/B.cpp
@@ -1,8 +1,26 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-#define l1 long long
-int main(){ int t; cin>>t; while(t--){ l1 n,x,m; cin>>n>>x>>m; l1 p=x,y=x,l,r;
- while(m--){ cin>>l>>r; if(p>=l && p<=r)p=l; if(y>=l && y<=r)y=r;
- } cout<<y-p+1<<"\n";
+#define lli long long int
+void solve(){
+    lli n,x,m;
+    cin>>n>>x>>m;
+    lli l=x,r=x;
+    vector<pair<lli,lli>> v(m);
+    for(lli i=0;i<m;i++){
+        cin>>v[i].first;
+        cin>>v[i].second;
+        if((v[i].second>=l && v[i].first<=l) || (v[i].first<=r && v[i].second>=r)){
+            l=min(l,v[i].first);
+            r=max(r,v[i].second);
+        }
+    }
+    cout<<r-l+1<<endl;
 }
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
 }
