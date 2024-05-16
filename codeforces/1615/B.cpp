92494b3a0c66eeec53baa3a8af508a66cec9963a
mdarikrayhan <mdarikrayhan@gmail.com>
2024-05-17 04:36:00 +0600

    Add solution for problem `B - And It's Non-Zero` Link: https://codeforces.com/contest/1615/problem/B Tags: bitmasks, greedy, math, *1300 Ref: https://codeforces.com/contest/1615/submission/261257771

--- codeforces/1615/B.cpp
+++ codeforces/1615/B.cpp
@@ -1,18 +1,63 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-int t,l,r;
-int F(int x,int j){
-return (x>>j+1<<j)+min(x%(1<<j+1),1<<j);
+
+#include <ext/pb_ds/assoc_container.hpp> 
+#include <ext/pb_ds/tree_policy.hpp> 
+using namespace __gnu_pbds; 
+#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+
+#define int long long
+#define pb push_back
+#define endl '\n'
+const int mod = 1e9+7;
+const long long INF = 1e18;
+
+void init(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    //#ifndef ONLINE_JUDGE
+        //freopen("inputf.in","r",stdin);
+        //freopen("outputf.in","w",stdout);
+    //#endif
 }
-int main(){
-cin>>t;
-while(t--){
-cin>>l>>r;
-int z=r;
-for(int j=0;j<18;j++){
-z=min(z,F(r+1,j)-F(l,j));
+
+int noOfOneBits(int n,int i){
+    n++;
+    int fullBaskets = n/(1LL<<(i+1));
+    int ones = fullBaskets*(1LL<<i);
+    int left = n%(1LL<<(i+1));
+    ones += max(0LL,left-(1LL<<i));
+    return ones;
 }
-cout<<z<<endl;
+
+void solve(){
+    int l,r;
+    cin >> l >> r;
+
+    int ans = INT_MAX;
+    for(int i=0;i<62;i++){
+        int cntOne = noOfOneBits(r,i) - noOfOneBits(l-1,i);
+        int cntZero = (r-l+1)-cntOne;
+        ans = min(ans,cntZero);
+    }
+    cout << ans << endl;
 }
-return 0;
+
+int32_t main(){
+    
+    init();
+    //clock_t time_req;
+    //time_req = clock();
+
+    int _t = 1;
+    cin >> _t;
+    while(_t--)
+        solve();
+
+    //time_req = clock() - time_req;
+    //cout << endl << "Time Taken is ";
+    //cout << (float)time_req / CLOCKS_PER_SEC << " Seconds" << endl;
+
 }
