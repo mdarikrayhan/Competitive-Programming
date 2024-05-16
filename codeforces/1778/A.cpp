0819580d1037d3238c1424f7cfc4b38544a0fa11
mdarikrayhan <mdarikrayhan@gmail.com>
2024-05-16 10:22:00 +0600

    Add solution for problem `A - Flip Flop Sum` Link: https://codeforces.com/contest/1778/problem/A Tags: greedy, implementation, *800 Ref: https://codeforces.com/contest/1778/submission/261139478

--- codeforces/1778/A.cpp
+++ codeforces/1778/A.cpp
@@ -1,41 +1,85 @@
 #include <bits/stdc++.h>
+#include <string>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <list>
+#include <utility>
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define nl "\n"
+#define str string
+#define sp " "
+#define sz(x) x.size()
+#define all(x) x.begin(), x.end()
+#define allr(x) x.rbegin(), x.rend()
+#define fi first
+#define se second
+#define vi vector<int>
+#define vll vector<long long>
+#define vec vector
+#define pb push_back
+#define fixed(n) fixed << setprecision(n)
 using namespace std;
-const char nl = '\n';
+ 
+void Moageza()
+{
+    // #ifndef ONLINE_JUDGE
+    //     freopen("input.txt", "r", stdin),freopen("output.txt","w",stdout);
+    // #endif
+    ios_base::sync_with_stdio(0), cin.tie(nullptr), cout.tie(nullptr);
+}
+void solve()
+{
+   int a,d=0,c=0,s=0,f=0;cin >> a;
+   vec<int>v(a);
+   for(int i = 0; i < a; i++)cin >> v[i];
+   for(int i =0; i < a; i++){
+    if(v[i]==-1)c++;
+    else if(v[i]==1) {
+        d=max(d,c);
+        c=0;
+    }
+   }
+   d=max(d,c);
+   if(d==0){
+    for(int i = 0;i < a;i++){
+        if(i<=1)s-=v[i];
+        else s+=v[i];
+    }
+   }
+    if(d==1){
+    for(int i = 0;i < a;i++){
+        if(((v[i]==-1&&v[i+1]==1)||(v[i]==1&&v[i+1]==-1))&&f==0){
+            s-=v[i],s-=v[i+1];
+            i++,f++;
+        }
+        else s+=v[i];
+    }
+   }
+    if(d>=2){
+    for(int i = 0;i < a;i++){
+        if(v[i]==-1&&v[i+1]==-1&&f==0){
+            f++;
+            s+=abs(v[i]),s+=abs(v[i+1]);
+            i++;
+        }
+        else s+=v[i];
+    }
+   }
+   cout<<s;
+}
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    int T = 1;
-    cin >> T;
-    for (int i = 1; i <= T; i++)
+    Moageza();
+    int t;
+    cin >> t;
+    while (t--)
     {
-        int n, sum = 0;
-        cin >> n;
-        int a[n];
-        for (int i = 0; i < n; i++)
-        {
-            cin >> a[i];
-            sum += a[i];
-        }
-        vector<int> v;
-        for (int i = 0; i < n - 1; i++)
-        {
-            if (a[i] == a[i + 1] && a[i] == 1)
-            {
-                v.push_back(sum-4);
-            }
-            else if (a[i] == a[i + 1] && a[i] == -1)
-            {
-                v.push_back(sum+4);
-            }
-            else
-            {
-                v.push_back(sum);
-            }
-        }
-        sort(v.begin(), v.end());
-        cout << v[v.size() - 1] << nl;
+        solve();
+           cout << nl;
     }
+    //   solve();
     return 0;
 }
