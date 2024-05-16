c32d04e7bccd4842ef68302e1222d2f13bc68f93
mdarikrayhan <mdarikrayhan@gmail.com>
2024-05-16 16:49:00 +0600

    Add solution for problem `B - Avoid Local Maximums` Link: https://codeforces.com/contest/1635/problem/B Tags: greedy, *800 Ref: https://codeforces.com/contest/1635/submission/261181678

--- codeforces/1635/B.cpp
+++ codeforces/1635/B.cpp
@@ -1,83 +1,55 @@
-#include <bits/stdc++.h>
+#include<iostream>
 using namespace std;
-
-typedef long long ll;
-typedef long double ld;
-typedef complex<ld> cd;
-
-typedef pair<int, int> pi;
-typedef pair<ll, ll> pl;
-typedef pair<ld, ld> pd;
-
-typedef vector<int> vi;
-typedef vector<ld> vd;
-typedef vector<ll> vl;
-typedef vector<pi> vpi;
-typedef vector<pl> vpl;
-typedef vector<cd> vcd;
-
-template <class T>
-using pq = priority_queue<T>;
-template <class T>
-using pqg = priority_queue<T, vector<T>, greater<T>>;
-
-#define FOR(i, a, b) for (int i = a; i < (b); i++)
-#define F0R(i, a) for (int i = 0; i < (a); i++)
-#define FORd(i, a, b) for (int i = (b)-1; i >= a; i--)
-#define F0Rd(i, a) for (int i = (a)-1; i >= 0; i--)
-#define trav(a, x) for (auto &a : x)
-#define uid(a, b) uniform_int_distribution<int>(a, b)(rng)
-
-#define sz(x) (int)(x).size()
-#define mp make_pair
-#define pb push_back
-#define f first
-#define s second
-#define lb lower_bound
-#define ub upper_bound
-#define all(x) x.begin(), x.end()
-#define ins insert
-const char nl = '\n';
-void solve()
-{
-    int n;
-    cin >> n;
-    int arr[n + 10];
-    for (int i = 0; i < n; i++)
-    {
-        cin >> arr[i];
-    }
-    int cnt = 0;
-    for (int i = 1; i < n - 1; i++)
-    {
-        if (arr[i] > arr[i - 1] && arr[i] > arr[i + 1])
-        {
-            cnt++;
-            if (i == n - 2)
-            {
-                arr[i + 1] = arr[i];
-            }
-            else
-                arr[i + 1] = max(arr[i], arr[i + 2]);
-        }
-    }
-    cout << cnt << endl;
-    for (int i = 0; i < n; i++)
-    {
-        cout << arr[i] << " ";
-    }
-    cout << endl;
+long long a[1000000];
+int main() {
+	int t, m;
+	cin >> t;
+	while (t--) {
+		cin >> m;
+		int sum = 0, num = 0x3f3f3f3f;//sum为需要改的数字的个数，num用于记录极大值的位置
+		for (int i = 0; i < m; i++) {
+			cin >> a[i];
+		}
+		int aa, again = 0;
+		for (int i = 1; i < m - 1; i++) {
+			if (a[i] > a[i - 1] && a[i] > a[i + 1]) {//寻找极大值
+				int reduction = 0;
+				sum++;//需要改的数字加一
+ 
+				if (i - num == 2) {//判断两个极大值中间是否只差一个数
+					sum--;//两个极大值中间只差一个数，只改这两个极大值中间打那个数即可，减去多改的那一个
+					reduction = 1;
+				}
+ 
+				num = i;//记录极大值的位置
+ 
+				if (reduction == 1) {//两个极大值中间只差一个数，即只改这个数跟更大的那个一样的即可
+					num = 0x3f3f3f3f;//清空num值，准备记录下一个极大值的位置（三个相间隔为1个数字的极大值，必定要改两个数，所以此时，第三个极大值无需与前两个数相比较）
+					a[i - 3] = aa;//还原前一个极大值前面改变的数字
+					if (a[i] >= a[i - 2]) {//比较两极大值大小，使中间位置等于更大一方
+						a[i - 1] = a[i];
+					}
+					else {
+						a[i - 1] = a[i - 2];
+					}
+				}
+				else {
+					aa = a[i - 1];//发现一个极大值时，记录它前面一个数的值
+					a[i - 1] = a[i];//并把其改为与极大值相等的数字
+				}
+			}
+		}
+		printf("%d\n", sum);
+		for (int i = 0; i < m; i++) {
+			printf("%d", a[i]);
+			if (i != m - 1) {
+				printf(" ");
+			}
+		}
+		if (t != 0) {
+			printf("\n");
+		}
+	}
+	return 0;
 }
-
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    int T = 1;
-    cin >> T;
-    while (T--)
-    {
-        solve();
-    }
-    return 0;
-}
+		    	  		      			 			      	
