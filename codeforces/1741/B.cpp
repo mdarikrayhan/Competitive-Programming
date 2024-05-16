a573f3da1634c5de38d5458085d57f12f0f959a3
mdarikrayhan <mdarikrayhan@gmail.com>
2024-05-16 12:17:00 +0600

    Add solution for problem `B - Funny Permutation` Link: https://codeforces.com/contest/1741/problem/B Tags: constructive algorithms, math, *800 Ref: https://codeforces.com/contest/1741/submission/261149262

--- codeforces/1741/B.cpp
+++ codeforces/1741/B.cpp
@@ -1,146 +1,90 @@
-/***************************************************/
-/*            Author : Md. Arik Rayhan             */
-/*        Github : github.com/mdarikrayhan         */
-/***************************************************/
 #include <bits/stdc++.h>
+
+#define el '\n'
+#define fi first
+#define se second
+#define forn(i,n) for(int i=0; i< (int)n; ++i)
+#define for1(i,n) for(int i=1; i<= (int)n; ++i)
+#define rforn(i,n) for(int i =(int)(n) - 1; i>= 0; --i)
+#define fora(i,x,n) for(int i=(int)x; i< (int)n; ++i)
+#define rfora(i,x,n) for(int i=(int)x; i>= (int)n; --i)
+#define pb push_back
+#define pob pop_back
+#define all(v) v.begin(),v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define sz(v) ((int)v.size())
+#define CYN(x) cout << (x ? "YES" : "NO" ) <<endl; 
+#define cases(t) while(t--)
+#define mem(v, val) memset(v, (val), sizeof(v))
+#define D(x) cout << #x << ": " << x << endl;
+#define precision(x) cout<<setprecision(x)<<fixed;
+
 using namespace std;
-const char nl = '\n';
 
 typedef long long ll;
 typedef long double ld;
-typedef complex<ld> cd;
-
-typedef pair<int, int> pi;
-typedef pair<ll, ll> pl;
-typedef pair<ld, ld> pd;
-
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef pair<char,int> pci;
+// typedef tuple<int, int, int> tiii;
 typedef vector<int> vi;
-typedef vector<ld> vd;
-typedef vector<ll> vl;
-typedef vector<pi> vpi;
-typedef vector<pl> vpl;
-typedef vector<cd> vcd;
+typedef vector<ll> vll;
+typedef vector<bool> vb;
+typedef vector<pii> vii;
+typedef vector<vi> vvi;
+typedef vector<vll> vvll;
+typedef map<int,int> mpii;
+// typedef unordered_map<int,int> umpii;
+typedef set<int> seti;
+typedef priority_queue<int> pqi;
+typedef priority_queue<ll> pqll;
 
-#define rep(i, a, b) for (int i = a; i < (b); i++)
-#define per(i, a, b) for (int i = (b)-1; i >= a; i--)
-#define print_map(m)                                                     \
-    for (const auto &[key, value] : m)                                   \
-    {                                                                    \
-        cout << '[' << key << ']' << ' ' << '=' << ' ' << value << '\n'; \
-    }
-#define print_vector(v)                          \
-    int vadnfiv = 0;                             \
-    for (const auto &value : v)                  \
-    {                                            \
-        cout << vadnfiv << ' ' << value << '\n'; \
-        vadnfiv++;                               \
-    }
-#define gcd(a, b) __gcd(a, b)
-#define lcm(a, b) (a * (b / gcd(a, b)))
+const int inf = 1e9;
+const int nax = 1e5+200;
+const ld pi = acos(-1);
+const ld eps = 1e-9;
 
-#define start_time                            \
-    using std::chrono::duration;              \
-    using std::chrono::duration_cast;         \
-    using std::chrono::high_resolution_clock; \
-    using std::chrono::milliseconds;          \
-    auto t1111 = high_resolution_clock::now();
-#define end_time                                            \
-    auto t2222 = high_resolution_clock::now();              \
-    duration<double, std::milli> ms_double = t2222 - t1111; \
-    std::cout << ms_double.count() << 'm' << 's' << nl;
-#define sz(x) (int)(x).size()
-#define mp make_pair
-#define pb push_back
-#define ff first
-#define ss second
-#define lb lower_bound
-#define ub upper_bound
-#define all(x) x.begin(), x.end()
-#define ins insert
-#define fastio                        \
-    ios_base::sync_with_stdio(false); \
-    cin.tie(NULL);                    \
-    cout.tie(NULL);
-#define multicase         \
-    int NoOfTestCase = 1; \
-    cin >> NoOfTestCase;  \
-    for (int testcaseno = 1; testcaseno <= NoOfTestCase; testcaseno++)
-bool isPrime(int n)
-{
-    if (n == 2 || n == 3)
-    {
-        return true;
+int dr[] = {1,-1,0,0,1,-1,-1,1};
+int dc[] = {0,0,1,-1,1,1,-1,-1};
+
+ll gcd(ll a, ll b) { return !a ? b : gcd(b % a, a); }
+ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+ll mod(ll a, ll b) { return (((a % b) + b) % b); }
+  
+void solve(){
+    int n; cin>>n;
+
+    if(n == 3){
+        cout<<-1<<el;
+        return;
     }
-    if (n <= 1 || n % 2 == 0 || n % 3 == 0)
-    {
-        return false;
+
+    cout<<n<<" "<<n-1<<" ";
+    for1(i,n-2){
+        cout<<i<<" ";
     }
-    for (int i = 5; i * i <= n; i += 6)
-    {
-        if (n % i == 0 || n % (i + 2) == 0)
-        {
-            return false;
-        }
-    }
-    return true;
-}
-bool isPalindrome(string S)
-{
-    string P = S;
-    reverse(P.begin(), P.end());
-    if (S == P)
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
-}
-bool isPowerof(long long num, long long base)
-{
-    if (num <= 0)
-    {
-        return false;
-    }
-    if (num % base == 0)
-    {
-        return isPowerof(num / base, base);
-    }
-    if (num == 1)
-    {
-        return true;
-    }
-    return false;
+    cout<<el;
 }
 
-template <class T>
-bool ckmin(T &a, const T &b) { return b < a ? a = b, 1 : 0; }
-template <class T>
-bool ckmax(T &a, const T &b) { return a < b ? a = b, 1 : 0; }
+int main(){
+    ios_base::sync_with_stdio(false),
+    cin.tie(NULL);
+    precision(20);
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
 
-int main()
-{
-    fastio
-        multicase
-    {
-        int n;
-        cin >> n;
-        if (n == 2)
-        {
-            cout << "2 1" << nl;
-            continue;
-        }
-        if (n == 3)
-        {
-            cout << -1 << nl;
-            continue;
-        }
-        for(int i=3;i<=n;i++)
-        {
-            cout << i << ' ';
-        }
-        cout << 1 << ' ' << 2 << nl;
-    }
+    int t=1; 
+    cin>>t;
+    cases(t) solve();
     return 0;
 }
+
+//Compile: 
+//g++ --std=c++17 B.cpp -o a
+//How to run the file in any terminal
+//./D
+//How pass the input in the file
+// Input data
+//./D < input.txt 
+// Input data and generate output file
+//./D < input.txt > output.txt
