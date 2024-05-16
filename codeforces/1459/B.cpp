ea242565f494f0f53d100f6c4bf74d6fc52d9509
mdarikrayhan <mdarikrayhan@gmail.com>
2024-05-17 05:01:00 +0600

    Add solution for problem `B - Move and Turn` Link: https://codeforces.com/contest/1459/problem/B Tags: dp, math, *1300 Ref: https://codeforces.com/contest/1459/submission/261258791

--- codeforces/1459/B.cpp
+++ codeforces/1459/B.cpp
@@ -1,2 +1,42 @@
-#include<iostream>
-int main(){int n;std::cin>>n;std::cout<<1+(3*n*(n+4)+2-(n&1?-1:1)*(n*(n+4)+2))/8;}
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define len(v) (int)v.size() 
+#define all(v) v.begin(),v.end()
+#define rall(v) v.rbegin(),v.rend()
+#ifdef Raptor
+#include "debug.h"
+#else
+#define debug(...) 1
+#define imie(...) 1;
+#endif
+
+void solve(){
+	int n; cin>>n;
+	if(n%2==0){
+		ll sum = 0;
+		while(n > 0){
+			sum += 2*n;
+			n -= 4;
+		}
+		if(n == 0) sum++;
+		cout<<sum<<"\n";
+	}
+	else{
+		ll sum = 0;
+		while(n > 0){
+			sum += 4*n;
+			n -= 4;
+		}
+		cout<<sum<<"\n";
+	}
+	
+	
+}
+int main(){
+	ios::sync_with_stdio(false);
+	solve();
+	
+	return 0;
+}
