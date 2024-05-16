7b526579b9c471162ab9ddc7aec021b703b3bb25
mdarikrayhan <mdarikrayhan@gmail.com>
2024-05-16 16:54:00 +0600

    Add solution for problem `A - Changing Volume` Link: https://codeforces.com/contest/1255/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1255/submission/261182357

---
+++ codeforces/1255/A.cpp
@@ -0,0 +1,264 @@
+#include <iostream>
+#include "bits/stdc++.h"
+
+using namespace std;
+
+#define range(i, a, b) for(int (i) = a; (i) < (b); (i)++)
+#define int long long
+#define mod 1000000007
+//#define mod 998244353
+//#define infinity LONG_LONG_MAX
+#define infinity 9999999999999
+#define yes "yes"
+#define no "no"
+#define No "No"
+#define Yes "Yes"
+#define YES "YES"
+#define NO "NO"
+#define ten_6 1000000
+
+
+// TREE OF 11 edges 11 1 2 1 3 1 4 2 5 3 6 6 9 3 7 7 10 7 11 4 8
+void print() {
+    cout << "\n";
+}
+
+void print(string s) {
+    cout << s << '\n';
+}
+
+void print(double x) {
+    cout << x << '\n';
+}
+
+void print(int a) {
+    cout << a << '\n';
+}
+
+void print(pair<int, int> p) {
+    cout << "(" << p.first << ", " << p.second << "), ";
+}
+
+void print(int A[], int N) {
+    range(i, 0, N) cout << A[i] << ' ';
+    cout << '\n';
+}
+
+void print(bool A[], int N) {
+    range(i, 0, N) cout << A[i] << ' ';
+    cout << '\n';
+}
+
+void print(const vector<int> &v, int length) {
+    for (int x: v) {
+        cout << x << ' ';
+        length--;
+        if (length < 0) break;
+    }
+    cout << '\n';
+}
+
+void print(const vector<int> &v) {
+    for (int x: v) cout << x << ' ';
+    cout << '\n';
+}
+
+void print(const vector<string> &v) {
+    for (string x: v) cout << x << '\n';
+    cout << "\n\n\n";
+}
+
+void print(const vector<bool> &v) {
+    for (bool x: v) cout << x << ' ';
+    cout << '\n';
+}
+
+void print(const vector<vector<int>> &v) {
+    for (const auto &x: v) {
+        print(x);
+    }
+}
+
+void print(const vector<pair<int, int>> &v) {
+    for (auto it: v) {
+        print(it);
+        cout << ' ';
+    }
+    cout << "\n";
+}
+
+void print(const map<int, int> &mp) {
+    cout << "{";
+    for (auto it: mp) cout << it.first << ": " << it.second << ", ";
+    cout << "}\n";
+}
+
+void print(pair<int, int> A[], int N) {
+    range(i, 0, N) {
+        cout << "(" << A[i].first << ", " << A[i].second << ") ";
+    }
+    cout << '\n';
+}
+
+void print(const multiset<int> &st) {
+    for (int i: st) cout << i << ' ';
+    cout << '\n';
+}
+
+void print(const set<int> &st) {
+    for (int i: st) cout << i << ' ';
+    cout << '\n';
+}
+
+void print(const unordered_set<int> &st) {
+    for (int i: st) cout << i << ' ';
+    cout << '\n';
+}
+
+void print(queue<int> q) {
+    for (size_t i = 0; i < q.size(); i++) {
+        cout << q.front() << ' ';
+        q.push(q.front());
+        q.pop();
+    }
+    print();
+}
+
+
+int power(int x, int y, int p) {
+    int res = 1;
+    x = x % p;
+    if (x == 0) return 0;
+    while (y > 0) {
+        if (y & 1)
+            res = (res * x) % p;
+        y = y >> 1;
+        x = (x * x) % p;
+    }
+    return res;
+}
+
+int gcd(int a, int b) {
+    return b == 0 ? a : gcd(b, a % b);
+}
+
+long long multiply_mod(long long a, long long b) {
+    long long res = 0;
+    a %= mod;
+    while (b) {
+        if (b & 1)
+            res = (res + a) % mod;
+        a = (2 * a) % mod;
+
+        b >>= 1;
+    }
+    return res;
+}
+
+int get_digit_sum(int x) {
+    int ans = 0;
+    while (x) {
+        ans += (x % 10);
+        x /= 10;
+    }
+    return ans;
+}
+
+int input() {
+    int x;
+    cin >> x;
+    return x;
+}
+
+int chut() {
+    int x;
+    cin >> x;
+    return x;
+}
+
+string read() {
+    string s;
+    cin >> s;
+    return s;
+}
+
+vector<int> read_vector(int len) {
+    vector<int> temp(len);
+    range(i, 0, len) cin >> temp[i];
+    return temp;
+}
+
+vector<int> spf;
+
+void sieve(int MAXN) {
+    spf.resize(MAXN);
+    spf[1] = 1;
+    for (int i = 2; i < MAXN; i++)
+        spf[i] = i;
+    for (int i = 4; i < MAXN; i += 2)
+        spf[i] = 2;
+
+    for (int i = 3; i * i < MAXN; i++) {
+        if (spf[i] == i) {
+            for (int j = i * i; j < MAXN; j += i)
+                if (spf[j] == j)
+                    spf[j] = i;
+        }
+    }
+}
+
+long long inverse(long long x, long long p) {
+    return power(x, p - 2, p);
+}
+
+
+int n, m, k;
+vector<int> arr, perm;
+vector<int> *adj;
+
+
+void solve() {
+
+    int a = chut(), b = chut();
+    n = abs(a - b);
+
+    int ans = n / 5;
+    int rem = n % 5;
+    if (rem == 4 || rem == 3){
+        print(ans + 2);
+        return;
+    }
+    if (rem == 1 || rem == 2) {
+        print(ans + 1);
+        return;
+    }
+    print(ans);
+
+
+}
+
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+
+#ifndef ONLINE_JUDGE
+    freopen("../input.txt", "r", stdin);
+    freopen("../output.txt", "w", stdout);
+#endif
+
+//    sieve(1000001);
+
+    int T = 1;
+
+    cin >> T;
+
+
+    while (T--) {
+        solve();
+    }
+
+
+    return 0;
+}
