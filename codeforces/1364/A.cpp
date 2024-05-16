68d9824ab3ace0c36a43f1beef898e254bb37f81
mdarikrayhan <mdarikrayhan@gmail.com>
2024-05-16 13:21:00 +0600

    Add solution for problem `A - XXXXX` Link: https://codeforces.com/contest/1364/problem/A Tags: brute force, data structures, number theory, two pointers, *1200 Ref: https://codeforces.com/contest/1364/submission/261157346

--- codeforces/1364/A.cpp
+++ codeforces/1364/A.cpp
@@ -1,17 +1,66 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+#define fastio()                      \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+#define endl '\n'
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
+#define INF 1e18
+#define int long long
+#define print(a)      \
+    for (auto it : a) \
+        cout << it << " ";
+#define loopi(a, b) for (int i = a; i < b; i++)
+#define loopj(a, b) for (int j = a; j < b; j++)
 using namespace std;
-
-int main(){
-int t,x,n,a; cin>>t;
-while(t--){
-cin>>n>>x;
-int sum = 0, l = -1;
-for(int i = 0;i<n;i++){
-cin>>a;
-sum=sum+a;
-if(sum%x) l = max(l,max(i+1,n-(i+1)));
+int M = 998244353;
+const int N = 2e18;
+//-------------------------------------------------------------------------------------------------------------
+void solve()
+{
+    int n, x;
+    cin >> n >> x;
+    vector<int> v(n);
+    int sum=0;
+    for (int i = 0; i < n; i++)
+    {
+       cin >> v[i];
+       sum+=v[i];
+    }
+    if(sum%x!=0){
+        cout<<n<<endl;
+        return;
+    }
+    int l = -1, r = -1;
+    loopi(0,n){
+        if(v[i] % x != 0){
+            l = i + 1;
+            break;
+        }
+    }
+    for(int i = n - 1; i >= 0; i--){
+        if(v[i] % x != 0){
+            r = i;
+            break;
+        }
+    }
+    if(l == -1)
+        cout << r << endl;
+    else 
+        cout << max(n - l, r) << endl;
 }
-cout<<l<<endl;
-}
-
+//--------------------------------------------------------------------------------------------------------------
+signed main()
+{
+    fastio();
+    int T = 1;
+    cin >> T;
+    while (T--)
+    {
+        solve();
+    }
+    return 0;
 }
