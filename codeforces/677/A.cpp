cf5c707140e1debd15359ca4b74ab4b665abcfae
mdarikrayhan <mdarikrayhan@gmail.com>
2024-05-16 09:44:00 +0600

    Add solution for problem `A - Vanya and Fence` Link: https://codeforces.com/contest/677/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/677/submission/261136569

--- codeforces/677/A.cpp
+++ codeforces/677/A.cpp
@@ -1,2 +1,25 @@
-#include<iostream>
-main(){int n,h,x;std::cin>>n>>h;while(std::cin>>x)if(x>h)n++;std::cout<<n;}
+// A. Vanya and Fence.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+    short numberOfFriends{ 0 }, theHightOfFence{ 0 };
+    cin >> numberOfFriends >> theHightOfFence;
+    short theFriendHight{ 0 };
+    unsigned short MinWidth{ 0 };
+    while (numberOfFriends--) {
+        cin >> theFriendHight;
+        if (theFriendHight > theHightOfFence) {
+            MinWidth += 2;
+
+        }
+        else {
+            MinWidth++;
+        }
+    }
+    cout << MinWidth;
+}
