eacbb05cec54467876d7f377f30df84ae654b1ff
mdarikrayhan <mdarikrayhan@gmail.com>
2024-05-16 16:25:00 +0600

    Add solution for problem `A - Game` Link: https://codeforces.com/contest/513/problem/A Tags: constructive algorithms, math, *800 Ref: https://codeforces.com/contest/513/submission/261178796

---
+++ codeforces/513/A.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main(){
+    long long a,b,c,d;
+    cin>>a>>b>>c>>d;
+    if(a>b) cout<<"First"<<endl;
+    else cout<<"Second"<<endl;
+    return 0;
+}
