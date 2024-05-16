2f99cc605751c6d6771d7ca1d60ef384a65bc4a2
mdarikrayhan <mdarikrayhan@gmail.com>
2024-05-16 12:57:00 +0600

    Add solution for problem `A - Strange Table` Link: https://codeforces.com/contest/1506/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1506/submission/261154713

---
+++ codeforces/1506/A.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define ld long double
+#define pl pair<ll, ll>
+#define vi vector<ll>
+#define sortx(X) sort(X.begin(),X.end());
+#define el "\n"
+#define yes cout<<"YES"<<el;
+#define no cout<<"NO"<<el;
+#define MW_HY ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+using namespace std;
+// ----------------------------Functions---------------------------- //
+long long string_to_num(char c, string word){
+  ll val = 0;
+  if (c == 'x')
+  {
+    for (ll i = 0; i < word.size(); i++)
+    {
+      val *= 10;
+      val += word[i]-48;
+    }
+  }
+  else
+  {
+    val += c-48;
+  }
+  return val;
+}
+bool isPrime(ll k)
+{
+  if (k == 1)
+  {
+    return false;
+  }
+  for (ll i = 2; i * i <= k; i++)
+  {
+    if (k % i == 0)
+    {
+      return false;
+    }
+  }
+  return true;
+}
+// ----------------------------------------------------------------- //
+void solution(){
+  ll n, m, x;
+  cin >> n >> m >> x;
+  ll column = x/n, row = (x%n);
+  if (x % n)
+  {
+    column++;
+  }
+  if (x % n == 0)
+  {
+    row = n;
+  }
+  row--;
+  cout << (m*row)+column << el;  
+}
+
+int main(){ MW_HY
+  int ntimes  = 1;
+  cin >> ntimes;
+  while (ntimes--)
+  {
+    solution();
+  }
+}
