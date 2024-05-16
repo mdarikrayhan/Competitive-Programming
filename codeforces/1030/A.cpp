a5b43ccfc91702b1909bd1f11629de863ee971d3
mdarikrayhan <mdarikrayhan@gmail.com>
2024-05-16 09:39:00 +0600

    Add solution for problem `A - In Search of an Easy Problem` Link: https://codeforces.com/contest/1030/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/1030/submission/261136174

--- codeforces/1030/A.cpp
+++ codeforces/1030/A.cpp
@@ -1,2 +1,25 @@
-#import<cstdio>
-int k,n,p;main(){for(scanf("%d",&n);n--;p+=k)scanf("%d",&k);puts(p?"Hard":"Easy");}
+#include<iostream>
+#include <stack>
+#include<string>
+#include<algorithm>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	int one = 0;
+	for (int i = 0; i < n; i++) {
+		int x;
+		cin >> x;
+		if (x == 1)
+			one++;
+	}
+
+	if (one)
+		cout << "HARD" << endl;
+	else
+		cout << "EASY" << endl;
+}
