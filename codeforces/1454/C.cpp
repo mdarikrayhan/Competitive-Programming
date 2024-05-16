eff38fda4ba61aa28a705fcd56a3653a107c0de0
mdarikrayhan <mdarikrayhan@gmail.com>
2024-05-16 14:33:00 +0600

    Add solution for problem `C - Sequence Transformation` Link: https://codeforces.com/contest/1454/problem/C Tags: greedy, implementation, *1200 Ref: https://codeforces.com/contest/1454/submission/261165859

--- codeforces/1454/C.cpp
+++ codeforces/1454/C.cpp
@@ -1,20 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
-int t;cin>>t;
-while(t--){
-int n;cin>>n;
-int a[n+1]={},cnt[n+1]={};
-for(int i=1;i<=n;i++){
-int x;cin>>x;
-if(i-a[x]!=1)cnt[x]++;
-a[x]=i;
+using ll = long long;
+#define endl '\n'
+#define vi vector<int>
+#define vll vector<ll>
+#define f(i,a,b) for(int i = (a);i <= (b);++i)
+#define all(c) c.begin(),c.end()
+const int p = 1e9 + 7;
+const int N = 1e5 + 8;
+void solve()
+{
+    int n;cin >> n;
+    map<int,vi > mp;
+    f(i,1,n){
+        int u;cin >> u;
+        mp[u].push_back(i);
+    }
+    int ans = 2e9;
+    for(auto & [i,v] : mp){
+        int res = v.size() + 1;
+        if(v.size() == n){
+            ans = min(ans,0);continue;
+        }
+        if(v.size() == 1){
+            if(*v.begin() == 1 || *v.rbegin() == n)ans = min(ans,1);
+            else ans = min(ans,2);continue;
+        }
+        if(v[0] == 1)res --;
+        if(v[v.size() - 1] == n)res --;
+        for(int j = 0;j < v.size() - 1;j++){
+            if(v[j] + 1 == v[j + 1])res--;
+        }
+        ans = min(ans,res);
+    }cout << ans << endl;
 }
-int mn= INT_MAX;
-for(int i=1;i<=n;i++)if(a[i]){
-mn = min(mn , cnt[i]+(a[i]!=n));
+int main() 
+{
+    ios::sync_with_stdio,cin.tie(0),cout.tie(0);
+    int _ = 1;
+    cin >> _;
+    while(_--)solve();
+    return 0;
 }
-cout<<mn<<endl;
-}
-}
- 	   		   			  				   	 	 			
