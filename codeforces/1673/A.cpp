e0c10599788746c6edac8460680ac8554fee01cc
mdarikrayhan <mdarikrayhan@gmail.com>
2024-05-16 16:44:00 +0600

    Add solution for problem `A - Subtle Substring Subtraction` Link: https://codeforces.com/contest/1673/problem/A Tags: games, greedy, strings, *800 Ref: https://codeforces.com/contest/1673/submission/261181107

--- codeforces/1673/A.cpp
+++ codeforces/1673/A.cpp
@@ -1,33 +1,69 @@
-#include <bits/stdc++.h>
+#define _USE_MATH_DEFINES
+#include  <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include<math.h>
+#include<iomanip>
+#include<stdio.h>
+#include <stdlib.h>
+#include<stdio.h>
+#include <queue>
+#include<map>
+#include <sstream>
+#include<set>
+#include<stack>
+#include<time.h>
+#include <sys/types.h>
+
+
+typedef long long int ll;
+//#include<bits/stdc++.h>
+
+
 using namespace std;
+
+
 
 int main()
 {
-    int tc;
-    cin >> tc;
-    while(tc--)
-    {
-        string s;
-        cin >> s;
-        int n=s.length(),alice=0;
-        for(int i=0;i<n;i++)
-            alice += s[i]-'a'+1;
-        if(n%2==0)
-            cout << "Alice " << alice << '\n';
-        else
-        {
-            int bob;
-            if(s[0]<=s[n-1])
-                bob = s[0]-'a'+1;
-            else
-                bob = s[n-1]-'a'+1;
-            alice -= bob;
-            if(alice > bob)
-                cout << "Alice " << alice-bob << '\n';
-            else if(alice < bob)
-                cout << "Bob " << bob-alice << '\n';
-            else
-                cout << "Draw " << 0 << '\n';
-        }
-    }
+
+    int Q;
+    Q = 1;
+   cin >> Q;
+    
+    
+   while (Q--) {
+       string p;
+       cin >> p;
+       int  n = p.size();
+       int r = 0;
+       for (int i = 0; i < n; i++) {
+           r += p[i] - 'a'+1;
+         //  cout << r;
+       }
+       
+       if (n % 2) {
+           
+           if (n>1) {
+               if (p[0] >= p[n - 1])r -= (p[n - 1]-'a'+1)*2;
+               else r -= (p[0]-'a'+1)*2;
+               cout << "Alice" << " ";
+               cout << r << endl;
+           }
+           else {
+               cout << "Bob" << " ";
+               cout << r << endl;
+           }
+       }
+       else {
+           cout << "Alice" << " ";
+           cout << r << endl;
+       }
+
+   }
+    return 0
+; 
 }
