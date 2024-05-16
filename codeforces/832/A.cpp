b0dd2c4c4bb8a7a3fa67dfb186e2ff82e9ea7cdd
mdarikrayhan <mdarikrayhan@gmail.com>
2024-05-16 16:20:00 +0600

    Add solution for problem `A - Sasha and Sticks` Link: https://codeforces.com/contest/832/problem/A Tags: games, math, *800 Ref: https://codeforces.com/contest/832/submission/261178215

--- codeforces/832/A.cpp
+++ codeforces/832/A.cpp
@@ -1,11 +1,14 @@
-#include<bits/stdc++.h>
+// LUOGU_RID: 159011329
+#include <iostream>
+
 using namespace std;
 int main(){
-    long long n , k;
-    cin >>n >>k;
-    n = n / k;
-    if (n % 2 == 1)
-        cout<<"YES";
-    else
-        cout<<"NO";
+	long long n,k;
+	cin >> n;
+	cin >> k;
+	if((n/k)&1){
+	cout << "YES";
+	} else {
+	cout << "NO";
+	}
 }
