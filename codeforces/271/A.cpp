1fb9337982a99f10e089c94356072c8659bad331
mdarikrayhan <mdarikrayhan@gmail.com>
2024-05-16 09:42:00 +0600

    Add solution for problem `A - Beautiful Year` Link: https://codeforces.com/contest/271/problem/A Tags: brute force, *800 Ref: https://codeforces.com/contest/271/submission/261136428

--- codeforces/271/A.cpp
+++ codeforces/271/A.cpp
@@ -1,23 +1,35 @@
-#include <iostream>
+#include<iostream>
+#include<vector>
 
-using namespace std;
+using namespace std ;
 
-int main()
-{
-    int y;
-    cin >> y;
-    while (true)
-    {
-        y += 1;
-        int a = y / 1000;
-        int b = y / 100 % 10;
-        int c = y / 10 % 10;
-        int d = y % 10;
-        if (a != b && a != c && a != d && b != c && b != d && c != d)
-        {
-            break;
-        }
+bool check(int y, int nums[]){
+
+    int a = y;
+
+    while(a > 0){
+        int n = a % 10;
+        a = a / 10;
+
+        nums[n]++;
+        if(nums[n] > 1) return false;
     }
-    cout << y << endl;
+
+    return true;
+}
+
+int main(){
+    int y;
+    cin>>y;
+    int a = 1;
+
+    while(true){
+        y++;
+        int nums[10] = {0};
+        if(check(y, nums)) break;
+    }
+
+    cout<<y<<endl;
     return 0;
+
 }
