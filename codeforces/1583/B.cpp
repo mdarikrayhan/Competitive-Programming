7f7b1758a1bf25feb03540fc974025a38880c042
mdarikrayhan <mdarikrayhan@gmail.com>
2024-05-17 07:13:00 +0600

    Add solution for problem `B - Omkar and Heavenly Tree` Link: https://codeforces.com/contest/1583/problem/B Tags: constructive algorithms, trees, *1200 Ref: https://codeforces.com/contest/1583/submission/261263314

--- codeforces/1583/B.cpp
+++ codeforces/1583/B.cpp
@@ -1,12 +1,51 @@
-#include<cstdio>
-int n,m,_,f[100001],x,i,j;
-int main(){
-	scanf("%d",&_);
-	while(_--){
-		scanf("%d%d",&n,&m);
-		for(i=1;i<=n;i++)f[i]=0;
-		for(i=1;i<=m;i++)scanf("%*d%d%*d",&x),f[x]=1;
-		for(i=1;i<=n;i++)if(!f[i])break;
-		for(j=1;j<=n;j++)if(i!=j)printf("%d %d\n",i,j);
-	}
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define append push_back
+#define pii pair<int,int>
+#define ld long double
+
+void quick_read(){ios::sync_with_stdio(false); cin.tie(0);}
+
+int multi_test=1;
+void solve()
+{
+    int n,m;
+    cin>>n>>m;
+    vector<int> vi(n+1);
+    for(int i=0;i<m;i++)
+    {
+        int a,b,c;
+        cin>>a>>b>>c;
+        vi[b]++;
+    }
+    int dex=-1;
+    for(int i=1;i<=n;i++)
+    {
+        if(vi[i]==0)
+        {
+            dex=i;
+            break;
+        }
+    }
+    for(int i=1;i<=n;i++)
+    {
+        if(i!=dex)
+        {
+            cout<<dex<<" "<<i<<endl;
+        }
+    }
+}
+
+signed main()
+{
+	int T=1;
+	if(multi_test)cin>>T;
+	while(T--)solve();
+
+
+
+    return 0;
 }
