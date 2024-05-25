c78e6827ffaed18d3a9a2d96a1381ac1ec5e72d5
mdarikrayhan <mdarikrayhan@gmail.com>
2024-05-25 12:16:00 +0600

    Add solution for problem `C - Li Hua and Chess` Link: https://codeforces.com/contest/1797/problem/C Tags: constructive algorithms, greedy, interactive, *1600 Ref: https://codeforces.com/contest/1797/submission/262466574

--- codeforces/1797/C.cpp
+++ codeforces/1797/C.cpp
@@ -1,14 +1,59 @@
 #include <bits/stdc++.h>
+
+#define int LL
 using namespace std;
- 
-int main(){
-    int tc; cin>>tc;
-    while(tc--){
-        int n, m, a, b, c, x, y; cin>>n>>m;
-        cout<<"? "<<1<<" "<<1<<endl; cin>>a;
-        cout<<"? "<<1<<" "<<min(a+1, m)<<endl; cin>>b;
-        cout<<"? "<<min(a+1, n)<<" "<<1<<endl; cin>>c;
-        x = b+1; y = c+1;
+typedef long long LL;
+
+// void IOS()
+// {
+//     ios::sync_with_stdio(false);
+//     cin.tie(nullptr);
+//     cout.tie(nullptr);
+// }
+void f(int x,int y){
+    int d1,d2;
+    cout<<"? "<<x<<" "<<y<<endl;
+    cin>>d1;
+    if(d1==0){
         cout<<"! "<<x<<" "<<y<<endl;
     }
+    else{
+        cout<<"? "<<x-d1<<" "<<y<<endl;
+        cin>>d2;
+        if(d2==0){
+            cout<<"! "<<x-d1<<" "<<y<<endl;
+        }
+        else cout<<"! "<<x<<" "<<y-d1<<endl;
+    }
+}
+void solve(){
+    int n,m,d0,d1,d2;
+    cin>>n>>m;
+    cout<<"? 1 1"<<endl;
+    cin>>d0;
+    if(d0==0){
+        cout<<"! 1 1"<<endl;
+    }
+    else{
+        if(n<=d0){
+            f(n,1+d0);
+        }
+        else if(m<=d0){
+            f(1+d0,m);
+        }
+        else{
+            f(1+d0,1+d0);
+        }
+        
+    }
+}
+signed main()
+{
+    // IOS();
+    int T;
+    cin>>T;
+    while(T--){
+        solve();
+    }
+    return 0;
 }
