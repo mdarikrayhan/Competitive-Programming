5c33f23184210f48718e091d3a91f5342384ef8b
mdarikrayhan <mdarikrayhan@gmail.com>
2024-05-26 02:02:00 +0600

    Add solution for problem `D - Bit Guessing Game` Link: https://codeforces.com/contest/1780/problem/D Tags: binary search, bitmasks, constructive algorithms, interactive, *1800 Ref: https://codeforces.com/contest/1780/submission/262621395

--- codeforces/1780/D.cpp
+++ codeforces/1780/D.cpp
@@ -1,12 +1,31 @@
-#include<iostream>
-int main() {
-	int t, c, d, r, s;
-	std::cin >> t;
-	while (t--) {
-		std::cin >> c;
-		s = d = 0;
-		for (int b = 0; c > d; b++)
-			std::cout << "- " << (1 << b) << std::endl, std::cin >> r, d += (r >= c), s += ((r >= c) + 1) * (1 << b), c = r;
-		std::cout << "! " << s << std::endl;
+#include <bits/stdc++.h>
+using namespace std;
+
+void run() {
+	int cnt; cin >> cnt;
+
+	int ans = 0;
+	for (int i = 0; i < 30 and cnt; i++) {
+		cout << "- " << (1 << i) << endl;
+		int x; cin >> x; assert(x != -1);
+
+		if (x < cnt) {
+			ans |= 1 << i;
+			cnt--;
+		} else {
+			ans |= 1 << (i + x - cnt + 1);
+			int orig = i;
+			i += x - cnt + 1;
+
+			cout << "- " << (1 << i) - 1 - ((1 << orig) - 1) << endl;
+			cin >> x; assert(x != -1); cnt = x;
+		}
 	}
+
+	cout << "! " << ans << endl;
+}
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+	int t; cin >> t; while (t--) run();
 }
