28561978d3a20e3ba448bf1c2be72b1cc38478f9
mdarikrayhan <mdarikrayhan@gmail.com>
2024-05-25 16:34:00 +0600

    Add solution for problem `B - Was it Rated?` Link: https://codeforces.com/contest/1812/problem/B Tags: *special problem, *special problem, *special problem, brute force, implementation Ref: https://codeforces.com/contest/1812/submission/262497003

---
+++ codeforces/1812/B.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    if(n==15||n==20||n==21)
+    {
+        cout<< "No"<<endl;
+    }
+    else
+    cout<< "Yes"<<endl ;
+}
