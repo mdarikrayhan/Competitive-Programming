d69e6a084466a548d33da3bc9e4a4fa2343febc2
mdarikrayhan <mdarikrayhan@gmail.com>
2024-05-25 17:34:00 +0600

    Add solution for problem `C - They Have Fooled` Link: https://codeforces.com/contest/1952/problem/C Tags: brute force, schedules Ref: https://codeforces.com/contest/1952/submission/262503595

---
+++ codeforces/1952/C.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+#define int long long
+#define mod 1000000007
+#define endl '\n'
+#define pii pair<int,int>
+#define I_am_weak ios::sync_with_stdio(0),cin.tie(0);
+using namespace std;
+signed main(){
+    I_am_weak
+    int n;
+    cin>>n;
+    vector<int>a;
+    a.push_back(10);
+    a.push_back(10);
+    a.push_back(8);
+    a.push_back(9);
+    a.push_back(8);
+    a.push_back(7);
+    a.push_back(7);
+    a.push_back(7);
+    a.push_back(7);
+    a.push_back(0);
+    a.push_back(9);
+    a.push_back(6);
+    a.push_back(8);
+    cout<<a[n]<<endl;;
+
+
+}
