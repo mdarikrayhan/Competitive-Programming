e0099700177d61e446ab2354e47c341676ceb96e
mdarikrayhan <mdarikrayhan@gmail.com>
2024-05-26 04:17:00 +0600

    Add solution for problem `B - Om Nom and Spiders` Link: https://codeforces.com/contest/436/problem/B Tags: implementation, math, *1400 Ref: https://codeforces.com/contest/436/submission/262629035

---
+++ codeforces/436/B.cpp
@@ -0,0 +1,87 @@
+/* 
+
+    ॐ हौं जूं सः ॐ भूर्भुवः स्वः 
+    ॐ त्र्यम्बकं यजामहे सुगन्धिं पुष्टिवर्धनम् उर्वारुकमिव बन्धनान्मृत्योर्मुक्षीय मामृतात् 
+    ॐ स्वः भुवः भूः ॐ सः जूं हौं ॐ
+
+   @Author  : Akshat Madhavan
+   @Country : INDIA 
+   
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define all(x) x.begin(), x.end()
+
+#ifndef ONLINE_JUDGE
+#include "debug.h"
+#define deb(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define deb(x...)
+#endif
+
+
+template<class T>void output (vector<T> &a) {for (auto &x : a) {cout << x << ' ';}cout << '\n';}
+template<typename T> void chmin (T &a, T b) {if (a > b) swap(a, b);}
+template<typename T> void chmax (T &a, T b) {if (a < b) swap(a, b);}
+
+
+int n, m, k;
+char ch;
+
+void solve() {
+    cin >> n >> m >> k;
+
+    vector<int> answer(m);
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            cin >> ch;
+            if (ch == 'R') {
+                if (j + i < m) {
+                    answer[j + i] += 1;
+                }
+            } else if (ch == 'L') {
+                if (j >= i) {
+                    answer[j - i] += 1;
+                }
+            } else if (ch == 'U') {
+                if (i % 2 == 0) {
+                    answer[j] += 1;
+                }
+            }
+        }
+    }
+
+    output(answer);
+}
+
+int32_t main() {
+    
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+    #ifndef ONLINE_JUDGE   
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+        freopen("error.txt", "w", stderr);  
+    #endif  
+
+    int tc = 1;
+    // cin >> tc;
+    for (int cases = 1; cases <= tc; ++cases) {
+        solve();
+        // cout << (solve() ? "YES\n" : "NO\n");
+        // cout << "Case # " << cases;
+    } 
+
+    #ifndef ONLINE_JUDGE
+        double timeTaken = 1000.0 * clock() / CLOCKS_PER_SEC;
+        cout << "\n[Finished in " << timeTaken << "ms]";
+        cerr << "\n[Finished in " << timeTaken << "ms]";
+    #endif
+
+    return 0;
+}
+
+//JAI SHREE RAM !!!!
