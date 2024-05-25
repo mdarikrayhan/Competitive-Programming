0eb8754432b0c88de1e2a3da1f971f0ba681be00
mdarikrayhan <mdarikrayhan@gmail.com>
2024-05-25 16:18:00 +0600

    Add solution for problem `C - Buns` Link: https://codeforces.com/contest/106/problem/C Tags: dp, *1700 Ref: https://codeforces.com/contest/106/submission/262495175

---
+++ codeforces/106/C.cpp
@@ -0,0 +1,146 @@
+// राधाकृष्ण राधाकृष्ण राधाकृष्णराधाकृष्ण राधाकृष्ण राधाकृष्ण राधाकृष्ण राधाकृष्ण राधाकृष्ण राधाकृष्ण राधाकृष्ण राधाकृष्ण राधाकृष्ण राधाकृष्ण राधाकृष्ण राधाकृष्ण राधाकृष्णराधाकृष्ण राधाकृष्ण राधाकृष्ण
+// राधाकृष्ण राधाकृष्णराधाकृष्ण राधाकृष्णराधाकृष्णराधाकृष्ण राधाकृष्ण राधाकृष्णराधाकृष्ण राधाकृष्ण राधाकृष्णराधाकृष्ण राधाकृष्ण राधाकृष्ण राधाकृष्ण राधाकृष्णराधाकृष्ण राधाकृष्णराधाकृष्णराधाकृष्ण राधाकृष्ण
+// राधाकृष्ण राधाकृष्णराधाकृष्णराधाकृष्ण राधाकृष्ण राधाकृष्ण राधाकृष्ण राधाकृष्ण राधाकृष्ण राधाकृष्णराधाकृष्ण राधाकृष्ण राधाकृष्णराधाकृष्ण राधाकृष्ण राधाकृष्णराधाकृष्ण राधाकृष्णराधाकृष्णराधाकृष्ण राधाकृष्ण
+//  राधाकृष्णराधाकृष्णराधाकृष्णराधाकृष्ण राधाकृष्ण राधाकृष्ण राधाकृष्ण राधाकृष्ण राधाकृष्ण राधाकृष्णराधाकृष्ण राधाकृष्ण राधाकृष्णराधाकृष्ण राधाकृष्ण राधाकृष्णराधाकृष्ण राधाकृष्णराधाकृष्णराधाकृष्ण राधाकृष्ण
+//  राधाकृष्णराधाकृष्णराधाकृष्णराधाकृष्ण राधाकृष्ण राधाकृष्ण राधाकृष्ण राधाकृष्ण राधाकृष्ण राधाकृष्णराधाकृष्ण राधाकृष्ण राधाकृष्णराधाकृष्ण राधाकृष्ण राधाकृष्णराधाकृष्ण राधाकृष्णराधाकृष्णराधाकृष्ण राधाकृष्ण
+// राधाकृष्ण राधाकृष्णराधाकृष्णराधाकृष्णराधाकृष्ण राधाकृष्ण राधाकृष्ण राधाकृष्ण राधाकृष्ण राधाकृष्ण राधाकृष्णराधाकृष्ण राधाकृष्ण राधाकृष्णराधाकृष्ण राधाकृष्ण राधाकृष्णराधाकृष्ण राधाकृष्णराधाकृष्णराधाकृष्ण राधाकृष्ण
+//  राधाकृष्णराधाकृष्णराधाकृष्णराधाकृष्ण राधाकृष्ण राधाकृष्ण राधाकृष्ण राधाकृष्ण राधाकृष्ण राधाकृष्णराधाकृष्ण राधाकृष्ण राधाकृष्णराधाकृष्ण राधाकृष्ण राधाकृष्णराधाकृष्ण राधाकृष्णराधाकृष्णराधाकृष्ण राधाकृष्ण
+// ॐ सह नाववतुॐ सह नाववतु मंत्र । ॐ सहना ववतु | सह नौ भुनक्तु  सह वीर्यम करवावाहै तेजस्वी अवधीतमस्तु मां विदविशावाहै ॐ ॐ सह नाववतु । सह नौ भुनक्तु । सह वीर्य करवाहै । तेजस्वि नावधीतमस्तु मा विद्विषावहै । ॐ शान्तिः शान्तिः शान्तिः ॥
+//  श्रीकृष्णाय वासुदेवाय हरये परमात्मने । प्रणतः क्लेशनाशाय गोविंदाय नमो नमः ॥
+#include <math.h>
+#include <algorithm>
+#include <climits>
+#include <iomanip>
+#include <iostream>
+#include <cstring>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <utility>
+#include <unordered_map>
+#include <vector>
+#include <unordered_set>
+
+using namespace std;
+
+/* clang-format off */
+
+
+#define ll long long int 
+#define pii pair<int, int>
+#define pll pair<long long int , long long int >
+#define vi vector<int>
+#define vc vector<char>
+#define vll vector<long long int >
+#define mii map<int, int>
+#define mll map<ll,ll>
+#define si set<int>
+#define sll set<long long int>
+#define sc set<char>
+#define f first
+#define s second
+#define endl "\n"
+#define sp <<" "
+#define pb push_back
+#define MOD 1000000007
+#define fora(a) for(auto u:a)
+//#define gcd(a,b) __gcd(a,b)
+//#define lcm(a,b) (a*(b/gcd(a,b)))
+#define all(a) (a).begin(),(a).end()
+#define rall(a) (a).rbegin(),(a).rend()
+#define test int tc; cin>>tc; while(tc--)
+#define forn(i,n) for(auto i=0; i<n; i++)
+#define printv(a) {for(auto u:a) cout<<u<<" "; cout<<endl;}
+#define printm(a) {for(auto u:a) cout<<u.f sp u.s<<endl;}
+#define op() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fori(a,b,c) {for(a = c.begin(); a!=b; a++) cout<< *a<< " "; cout<<endl;}
+#define fraction(a) cout.unsetf(ios::floatfield); cout.precision(a); cout.setf(ios::fixed,ios::floatfield);
+#define ip(v) for(auto &x:v) cin>>x;
+//typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<ll> vl;
+//typedef vector<int> vi;
+//typedef vector<char> vc;
+typedef vector<string> vs;
+typedef vector<int>::iterator vit;
+//typedef set<int> si;
+//typedef map<int, int> mii;
+const double eps = 1e-9;
+/* FUNCTIONS */
+#define ff(i,s,e) for(long long int i=s;i<e;i++)
+#define cf(i,s,e) for(long long int i=s;i<=e;i++)
+#define rf(i,e,s) for(long long int i=e-1;i>=s;i--)
+#define pb push_back
+#define eb emplace_back
+
+/* PRINTS */
+template <class T>
+void print_v(vector<T> &v) { cout << "{"; for (auto x : v) cout << x << ","; cout << "\b}"; }
+
+/* UTILS */
+#define MOD 1000000007
+#define PI 3.1415926535897932384626433832795
+#define read(type) readInt<type>()
+//ll min(ll a,ll b) { if (a<b) return a; return b; }
+ll min(ll a,ll b) { if (a<b) return a; return b; }
+ll max(ll a,ll b) { if (a>b) return a; return b; }
+//ll max(ll a,ll b) { if (a>b) return a; return b; }
+ll gcd(ll a,ll b) { if (b==0) return a; return gcd(b, a%b); }
+ll lcm(ll a,ll b) { return a/gcd(a,b)*b; }
+string to_upper(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='a' && a[i]<='z') a[i]-='a'-'A'; return a; }
+string to_lower(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='A' && a[i]<='Z') a[i]+='a'-'A'; return a; }
+bool prime(ll a) { if (a==1) return 0; for (int i=2;i<=round(sqrt(a));++i) if (a%i==0) return 0; return 1; }
+void yes() { cout<<"YES\n"; }
+void no() { cout<<"NO\n"; }
+
+/*  All Required define Pre-Processors and typedef Constants */
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int  uint64;
+
+/* clang-format on */
+
+/* Main()  function */
+// ll dp[55][50005];
+ll n, m, c, d;
+vector<vector<ll>>dp;
+ll help(vector<vector<ll>>&grid,ll idx, ll valc,ll vald){
+
+if(idx==m) return (valc/c)*d;
+
+
+if(dp[idx][valc]!=-1) return dp[idx][valc];
+ll ans1=0;
+ll ans = INT_MIN;
+ll k =grid[idx][0]/grid[idx][1];
+for(int i=0;i<=k;i++){
+    if(i*grid[idx][2]<=valc)
+ans=max(ans,i*grid[idx][3]+help(grid,idx+1,valc-i*grid[idx][2],vald));
+}
+  ans=max(ans,(valc/c)*d);
+return dp[idx][valc]=ans;
+}
+
+
+
+int main()
+{
+    op();
+ 
+  cin>>n>>m>>c>>d;
+  vector<vector<ll>>grid(m,vector<ll>(4));
+  for(auto &x:grid){
+    for(auto &y:x) cin>>y;
+  }
+dp=vector<vector<ll>>(m+1000,vector<ll>(c+1000,-1));
+
+ll ans=help(grid,0,n,0);
+cout<<ans<<endl;
+
+
+    return 0;
+}
