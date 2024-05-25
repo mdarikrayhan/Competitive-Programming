e1fefde6e891f8f86aad3bdeb84369d14fe64ce5
mdarikrayhan <mdarikrayhan@gmail.com>
2024-05-25 17:36:00 +0600

    Add solution for problem `G - Mathematician Takeover` Link: https://codeforces.com/contest/1952/problem/G Tags: binary search, dfs and similar, math Ref: https://codeforces.com/contest/1952/submission/262503780

---
+++ codeforces/1952/G.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+#define int int64_t
+using namespace std;
+
+void solve() {
+    double a;
+    cin >> a;
+    cout << log2(a);
+}
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t{1};
+//    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
