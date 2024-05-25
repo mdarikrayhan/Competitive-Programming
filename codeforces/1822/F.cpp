e95ddef0c6473f9d7299f573cd7c63469311bde1
mdarikrayhan <mdarikrayhan@gmail.com>
2024-05-25 16:29:00 +0600

    Add solution for problem `F - Gardening Friends` Link: https://codeforces.com/contest/1822/problem/F Tags: brute force, dfs and similar, dp, graphs, trees, *1700 Ref: https://codeforces.com/contest/1822/submission/262496403

--- codeforces/1822/F.cpp
+++ codeforces/1822/F.cpp
@@ -1,37 +1,60 @@
-#include<bits/stdc++.h>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-void runcase(){
-    int n,k,c;
-    cin >> n >> k >> c;
-    vector<vector<int>> adj(n+1);
-    for(int i=1;i<n;i++){
-        int u,v;
-        cin >> u >> v;
-        adj[u].emplace_back(v);
-        adj[v].emplace_back(u);
+#define int long long
+#define pi pair<int, int>
+#define all(x) x.begin(), x.end()
+#define endl '\n'
+template<class T> bool ckmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+// go down to child -> maximum dist from other subtrees increases by one
+// maximum dist in child subtree decreases by one
+// cost = max(those) * k + c * dep
+void solve() {
+  int n,k,c;cin>>n>>k>>c;
+  vector<vector<int>> adj(n);
+  for(int i=0;i<n-1;i++){
+    int u,v;cin>>u>>v;
+    u--,v--;
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+  }
+  vector<int> dep(n),hit(n);
+  auto dfs1=[&](int u,int p,auto&& dfs1)->void{
+    for(auto v:adj[u]){
+      if(v==p)continue;
+      dep[v]=dep[u]+1;
+      dfs1(v,u,dfs1);
+      ckmax(hit[u],hit[v]+1);
     }
-    vector<int> dp(n+1);
-    long long ans=0;
-    function<void(int,int,int)> dfs=[&](int u,int p,int l){
-        int x=0,y=0;
-        for(auto v:adj[u]){
-            if(v==p)continue;
-            dfs(v,u,l+1);
-            dp[u]=max(dp[u],dp[v]+1);
-            y=max(y,dp[v]+1);
-            if(x<y)swap(x,y);
-        }
-        ans=max({ans,1ll*(x+y)*k-1ll*(y+l)*c,1ll*x*k-1ll*l*c});
-    };
-    dfs(1,0,0);
-    cout << ans << '\n';
+  };
+  dfs1(0,0,dfs1);
+  int ans=-1e18;
+  auto dfs2=[&](int u,int p,int above,auto&& dfs2)->void{
+    int h=above;
+    vector<pi> hs{{above+1,1e18}};
+    for(auto v:adj[u]){
+      if(v==p)continue;
+      ckmax(h,hit[v]+1);
+      hs.push_back({hit[v]+2,v});
+    }
+    sort(all(hs),greater<pi>());
+    ckmax(ans,h*k-dep[u]*c);
+    for(auto v:adj[u]){
+      if(v==p)continue;
+      int nxt=hs[0].first;
+      if(hs[0].second==v)nxt=hs[1].first;
+      dfs2(v,u,nxt,dfs2);
+    }
+  };
+  dfs2(0,0,0,dfs2);
+  cout<<ans<<endl;
 }
 
-int main(){
-    cin.tie(nullptr)->sync_with_stdio(false);
-    int t(1);
-    cin >> t;
-    while(t--)runcase();
+signed main() {
+  ios_base::sync_with_stdio(false); cin.tie(NULL);
+  int t = 1;
+  cin >> t;
+  while(t--) solve();
 }
