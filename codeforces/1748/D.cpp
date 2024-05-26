b716ca6f5f71fcf06b2124e6b83b4116cdba6f67
mdarikrayhan <mdarikrayhan@gmail.com>
2024-05-27 03:39:00 +0600

    Add solution for problem `D - ConstructOR ` Link: https://codeforces.com/contest/1748/problem/D Tags: bitmasks, chinese remainder theorem, combinatorics, constructive algorithms, math, number theory, *2100 Ref: https://codeforces.com/contest/1748/submission/262804019

--- codeforces/1748/D.cpp
+++ codeforces/1748/D.cpp
@@ -1,20 +1,54 @@
-#include<stdio.h>
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
 
-int main()
-{
-	int t;
-	long long a,b,d,x;
-	scanf("%d",&t);
-	while(t--) {
-		scanf("%lld%lld%lld",&a,&b,&d);
-		x=0;
-		if((a&-a)<(d&-d)||(b&-b)<(d&-d)) {
-			puts("-1");
-			continue;
-		}
-		for(int i=0; (x|a|b)>x; i++)
-			if(((a|b)&(d&-d)<<i)&&!(x&(d&-d)<<i))
-				x+=d<<i;
-		printf("%lld\n",x);
-	}
+ll binpow(ll b, ll p, ll w) {
+    ll c = 1;
+    while (p > 0) {
+        if (p & 1) c = 1LL * c * b % w;
+        p >>= 1;
+        b = 1LL * b * b % w;
+    }
+    return c;
 }
+
+void solve() {
+    ll a, b, d;
+    cin >> a >> b >> d;
+    a |= b;
+
+    if ((d & -d) > (a & -a)) {
+        cout << "-1\n";
+        return;
+    }
+
+    if (a % d == 0) {
+        cout << a << '\n';
+        return;
+    }
+
+    int k = 0;
+    while (1 ^ d & 1) {
+        d >>= 1;
+        k++;
+    }
+
+    ll inv = (d + 1) >> 1;
+    ll p = (binpow(inv, 30 - k, d) + d - 1) % d;
+    ll x = (1LL << k) * (p * (1LL << 30 - k) + (1LL << 30 - k) - 1);
+    cout << x << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        solve();
+    }
+
+    return 0;
+}
+
