aabcd056e2b7eb917b4f0ce589e4402401c80a68
mdarikrayhan <mdarikrayhan@gmail.com>
2024-05-27 05:52:00 +0600

    Add solution for problem `G2 - Magic Triples (Hard Version)` Link: https://codeforces.com/contest/1822/problem/G2 Tags: brute force, data structures, math, number theory, *2200 Ref: https://codeforces.com/contest/1822/submission/262808912

--- codeforces/1822/G2.cpp
+++ codeforces/1822/G2.cpp
@@ -1,45 +1,49 @@
 #include<bits/stdc++.h>
+#define ll long long
+#define pii pair<long long,long long>
 using namespace std;
-#define int long long
-int a[200005],n,b[200005];
-map<int,int> mp;
-int ask(int x)
+const int B = (int)1e6;
+vector dd(B,vector<ll>());
+void solve()
 {
-	if(mp.count(x))return mp[x];
-	return 0;
-} 
-signed main()
-{
-	ios::sync_with_stdio(0);
-	cin.tie(0);cout.tie(0);
-	int T;
-	cin>>T;
-	while(T--)
-	{
-		mp.clear();
-		cin>>n;
-		for(int i=1;i<=n;i++)
-		{
-			cin>>a[i];
-			b[i]=a[i];
-			mp[a[i]]++;
-		}
-		sort(b+1,b+n+1);
-		int len=unique(b+1,b+n+1)-b-1,ans=0;
-		for(int i=1;i<=len;i++)
-		{
-			ans+=mp[b[i]]*(mp[b[i]]-1)*(mp[b[i]]-2);
-			for(int j=1;j*j<=b[i]&&b[i]*j<=b[len];j++)
-			{
-				if(b[i]%j==0)
-				{
-					if(j!=1)
-						ans+=ask(b[i])*ask(b[i]/j)*ask(b[i]*j);
-					if(j*j!=b[i])
-						ans+=ask(b[i])*ask(j)*ask(b[i]*b[i]/j);
-				}
-			}
-		}
-		cout<<ans<<endl;
+	int n;cin>>n;
+	map<ll,ll>cnt;
+	for(int i=1;i<=n;i++) {
+		int x;cin>>x;
+		cnt[x]++;
 	}
-} 
+	ll ans=0;
+	for(auto [x,num]:cnt) {
+		if(num>2) ans+=num*(num-1)*(num-2);
+		if(x<B) {
+			for(auto d:dd[x])
+				if(cnt.count(x/d) and cnt.count(x*d))
+				ans+=num*cnt[x/d]*cnt[x*d];
+		} else {
+			for(int j=2;x*j<=(int)1e9;j++)
+				if(x%j==0 and cnt.count(x/j) and cnt.count(x*j)) {
+					ans+=num*cnt[x/j]*cnt[x*j];
+				}
+		}
+	}
+	cout<<ans<<'\n';
+ } 
+ main()
+ {
+ 	ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+ 	int _  = 1;cin>>_;
+ 	for(int i=2;i<(int)1e6;i++) {
+ 		dd[i].push_back(i);
+ 		for(int j=2;1LL*j*j<=i;j++)
+ 			if(i%j==0) {
+ 				dd[i].push_back(j);
+ 				if(1LL*j*j!=i) dd[i].push_back(i/j); 
+			 }
+	 }
+	for(int i=2;i<=(int)100;i++) {
+		for(auto d:dd[i])
+			cerr<<d<<' ';
+		cerr<<'\n';
+	}
+ 	while(_--) solve();
+ }
