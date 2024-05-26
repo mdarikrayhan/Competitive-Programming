bac335cc2e478aea8c661c5a1ce91a2524796fbe
mdarikrayhan <mdarikrayhan@gmail.com>
2024-05-27 04:01:00 +0600

    Add solution for problem `D - Flexible String Revisit` Link: https://codeforces.com/contest/1778/problem/D Tags: combinatorics, dp, math, probabilities, *2100 Ref: https://codeforces.com/contest/1778/submission/262804989

--- codeforces/1778/D.cpp
+++ codeforces/1778/D.cpp
@@ -1,20 +1,56 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define int long long
-int T,n,c,A,f[1<<20],r[1<<20],D=998244353;
-string s,t; 
-main(){
-	cin>>T,r[1]=1;
-	for(int i=2;i<=1e6;i++)
-		r[i]=(D-D/i)*r[D%i]%D;
-	while(T--&&cin>>n>>s>>t){
-		c=A=0,f[n]=1;
-		for(int i=0;i<n;i++)
-			c+=s[i]^t[i];
-		for(int i=n-1;i;i--)
-			f[i]=(n+(n-i)*f[i+1])%D*r[i]%D;
-		for(int i=1;i<=c;i++)
-			(A+=f[i])%=D;
-		cout<<A<<'\n';
-	}
+#define mod 998244353
+#define IOS ios::sync_with_stdio(0),cin.tie(0)
+typedef long long ll; 
+typedef pair<ll,ll> p;
+const int N=1e6+5;
+ll dp[N];
+ll ksm(ll x,ll y)
+{
+    ll ans=1;
+    for(;y;)
+    {
+        if(y&1) ans=ans*x%mod;
+        x=x*x%mod;
+        y>>=1;
+    }
+    return ans;
+}
+ll get_inv(ll x){return ksm(x,mod-2);}
+int main()
+{
+    IOS;
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        ll n;
+        cin>>n;
+        string a,b;
+        cin>>a>>b;
+        ll ct1=0,ct2=0;
+        
+        for(int i=0;i<n;i++)
+        {
+            if(a[i]!=b[i])
+            ct1++;
+            else
+            ct2++;
+        }
+        if(ct1==0)
+        {
+            cout<<0<<'\n';
+            continue;   
+        }
+        dp[n]=1;
+        for(int i=n-1;i>=1;i--)
+        {
+            dp[i]=(n+(n-i)*dp[i+1])%mod*get_inv(i)%mod;
+        }
+        ll sum=0;
+        for(int i=1;i<=ct1;i++)
+        sum=(sum+dp[i])%mod;
+        cout<<sum<<'\n';
+    }
 }
