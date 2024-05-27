9e50c4d36d62a5183c68c4b8620d32a9c9d100be
mdarikrayhan <mdarikrayhan@gmail.com>
2024-05-28 04:13:00 +0600

    Add solution for problem `E - Cactus Wall` Link: https://codeforces.com/contest/1749/problem/E Tags: constructive algorithms, dfs and similar, graphs, shortest paths, *2400 Ref: https://codeforces.com/contest/1749/submission/262937383

--- codeforces/1749/E.cpp
+++ codeforces/1749/E.cpp
@@ -1,50 +1,94 @@
+// LUOGU_RID: 160444257
 #include<bits/stdc++.h>
-#define id(x,y) ((x)*m-m+(y))
 using namespace std;
-const int N=2e5+5,M=4e5+5,T=2e6+5;
-int dx[4]={1,-1,1,-1},dy[4]={1,1,-1,-1};
-int n,m;char*s[N];
-int L,R,stk[T],dis[M],from[M];
-void ins(int x,int y,int k1){
-if(x<1||x>n||y<1||y>m||(y>1&&s[x][y-1])||(y<m&&s[x][y+1])||
-(x>1&&s[x-1][y])||(x<n&&s[x+1][y])||dis[id(x,y)]!=1e9)return;
-int t=s[x][y]?--L:++R,k=id(x,y);
-dis[k]=dis[from[stk[t]=k]=k1]+(!s[x][y]);
+typedef pair<int,int> PII;
+typedef long long ll;
+const int N = 2e5 + 10;
+const int INF = 0x3f3f3f3f;
+vector<int> dist[N],pre[N];
+int n,m;
+int dx[8] = {0,1,0,-1,-1,-1,1,1};
+int dy[8] = {1,0,-1,0,-1,1,-1,1};
+vector<char> ch[N];
+deque<PII>q;
+bool check(int x,int y){
+	if(x < 1 || y < 1 || x > n || y > m) return false;
+	for(int i = 0;i < 4;i ++){
+		int fx = x + dx[i],fy = y + dy[i];
+		if(fx >= 1 && fy >= 1 && fx <= n && fy <= m && ch[fx][fy] == '#') return false;
+	}
+	return true;
 }
-void xxx(){
-scanf("%d%d",&n,&m);
-for(int i=1;i<=n;i++){
-s[i]=new char[m+5];
-scanf("%s",s[i]+1);
-for(int j=1;j<=m;j++){
-s[i][j]=s[i][j]=='#';
-int t=id(i,j);
-dis[t]=1e9,from[t]=0;
-}
-}
-int p=1;R=1e6;L=R+1;
-for(int i=1;i<=n;i++)ins(i,1,0);
-while(L<=R){
-int k=stk[L++],x=(k-1)/m+1,y=k-x*m+m;
-for(int o=0;o<4;o++)
-ins(x+dx[o],y+dy[o],k);
-}
-for(int i=1;i<=n;i++)
-if(dis[id(p,m)]>dis[id(i,m)])p=i;
-if(dis[id(p,m)]==1e9){puts("NO");return;}
-for(int k=id(p,m);k;){
-int x=(k-1)/m+1,y=k-x*m+m;
-s[x][y]=1;k=from[k];
-}
-puts("YES");
-for(int i=1;i<=n;i++){
-for(int j=1;j<=m;j++)
-s[i][j]=s[i][j]?'#':'.';
-printf("%s\n",s[i]+1);
-delete s[i];
-}
+void init(){
+	for(int i = 1;i <= n;i ++){
+		dist[i].clear();
+		pre[i].clear();
+		ch[i].clear();
+		for(int j = 0;j <= m;j ++){
+			dist[i].push_back(INF);
+			pre[i].push_back(0);
+			ch[i].push_back('\0');
+		}
+	}
+	q.clear();
 }
 int main(){
-int T;scanf("%d",&T);for(;T--;)
-xxx();return 0;
+	int T; 
+	cin >> T;
+	while(T --){
+		cin >> n >> m;
+		init();
+		for(int i = 1;i <= n;i ++){
+			for(int j = 1;j <= m;j ++) cin >> ch[i][j];	
+		} 	
+		for(int i = 1;i <= n;i ++){
+			if(ch[i][1] == '#'){
+				q.push_front({i,1});
+				dist[i][1] = 0;
+			}else if(check(i,1)){
+				q.push_back({i,1});
+				dist[i][1] = 1;
+			}
+		}
+		while(!q.empty()){
+			int x = q.front().first,y = q.front().second;
+			q.pop_front();
+			for(int i = 4;i < 8;i ++){
+				int fx = x + dx[i],fy = y + dy[i];
+				if(!check(fx,fy)) continue;
+				int w = (ch[fx][fy] == '.');
+				if(dist[x][y] + w < dist[fx][fy]){
+					dist[fx][fy] = dist[x][y] + w;
+					pre[fx][fy] = i;
+					if(w) q.push_back({fx,fy});
+					else q.push_front({fx,fy});
+				}
+			}
+		}	
+		int minn = INF,pos = -1;
+		for(int i = 1;i <= n;i ++){
+			if(dist[i][m] < minn){
+				minn = dist[i][m];
+				pos = i;
+			} 
+		} 
+		if(minn >= INF){
+			cout << "NO\n";
+			continue;
+		}
+		cout << "YES\n";
+		int x = pos,y = m;
+		while(1){
+			ch[x][y] = '#';
+			if(!pre[x][y]) break;
+			int pres = pre[x][y];
+			x -= dx[pres];
+			y -= dy[pres];
+		}
+		for(int i = 1;i <= n;i ++){
+			for(int j = 1;j <= m;j ++) cout << ch[i][j];
+			cout << "\n";
+		}
+	}
+	return 0;
 }
