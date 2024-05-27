c2e3bc05d75f46c77edab99ea536955d275ceaa2
mdarikrayhan <mdarikrayhan@gmail.com>
2024-05-27 06:21:00 +0600

    Add solution for problem `D - Olympiad` Link: https://codeforces.com/contest/222/problem/D Tags: binary search, greedy, sortings, two pointers, *1900 Ref: https://codeforces.com/contest/222/submission/262809824

---
+++ codeforces/222/D.cpp
@@ -0,0 +1,113 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+#define IOS ios_base::sync_with_stdio(0);  cin.tie(0); cout.tie(0);
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>order_set;
+typedef pair<int, int>pr;
+#define all(i)     i.begin() , i.end()
+#define ft     first
+#define sn     second
+#define pb     push_back
+#define totalone(mask)  __builtin_popcount(mask)
+#define chkbit(mask,bit)  (mask&(1LL << bit))
+
+// debug section start
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i : x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+// debug section closed
+
+#define en "\n"
+#define sum(n) ((1LL*(n)*(n+1))/ 2LL)
+#define sqr(n) (1LL*(n)*(n))
+#define vag(a,b) ((a + b - 1)/b)
+
+#define MAXN 100010
+#define inf 1e6+5
+const int mod = 1e9 + 7;
+
+int n, x, a[MAXN], b[MAXN];
+
+bool chk(int md)
+{
+    int j = md - 1;
+    for (int i = 0; i < md; i++, j--) {
+        if (a[i] + b[j] < x) {
+            return false;
+        }
+    }
+    return true;
+}
+void solve()
+{
+    // int n, x;
+    cin >> n >> x;
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    for (int i = 0; i < n; i++) {
+        cin >> b[i];
+    }
+
+    sort(a, a + n); sort(b, b + n);
+    reverse(a, a + n); reverse(b, b + n);
+
+    int l = 1, r = n, an = n;
+    while ( l <= r)
+    {
+        int md = l + (r - l) / 2;
+        bool ok = chk(md);
+        debug(md, ok);
+        if (ok) {
+            an = md;
+            l = md + 1;
+        }
+        else r = md - 1;
+    }
+    cout << 1 << " " << an << en;
+}
+int main()
+{
+    IOS;
+    ll t;
+    t = 1;
+    // cin >> t;
+
+    int c = 0;
+    while ( t-- )
+    {
+        // cout<<"Case "<<++c<<": ";
+        solve();
+    }
+    return 0;
+}
