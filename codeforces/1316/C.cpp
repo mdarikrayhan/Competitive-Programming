3bca6c001e982b30762d2f11be5b716480f2da8f
mdarikrayhan <mdarikrayhan@gmail.com>
2024-05-28 02:48:00 +0600

    Add solution for problem `C - Primitive Primes` Link: https://codeforces.com/contest/1316/problem/C Tags: constructive algorithms, math, ternary search, *1800 Ref: https://codeforces.com/contest/1316/submission/262933009

---
+++ codeforces/1316/C.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+//#pragma GCC optimize ("O3")
+#define nl '\n'
+#define ls k << 1
+#define rs k << 1 | 1
+#define pb push_back
+#define sz(x) (int)x.size()
+#define mk std :: make_pair
+#define PII std :: pair <int, int>
+#define PIL std :: pair <int, i64>
+#define PLL std :: pair <i64, i64>
+#define DEB std :: cout << "ok" << '\n';
+#define deb(x) std :: cout << #x << " = " << x << '\n';
+using i64 = long long;
+typedef unsigned long long ull;
+template <class T>
+T max(T a, T b) {
+	return a > b ? a : b;
+}
+template <class T>
+T min(T a, T b) {
+	return a < b ? a : b;
+}
+template <class T>
+T abs(T a) {
+	return a < 0 ? -a : a;
+}
+std :: mt19937 rd(114514);
+const int INF = 0x3f3f3f3f;
+const i64 LLINF = 0x3f3f3f3f3f3f3f3f;
+const int Range = 1e9;
+const double eps = 1e-6;
+const int Mod = 1e9 + 7;
+// const int N = ;
+void solve() {
+	int n, m, p;
+	std :: cin >> n >> m >> p;
+	std :: vector <int> a(n), b(m);
+	int x = -1, y = -1;
+	for(int i = 0; i < n; ++ i) {
+		std :: cin >> a[i];
+		if(a[i] % p && x == -1) {
+			x = i;
+		} 
+	}
+	for(int j = 0; j < m; ++ j) {
+		std :: cin >> b[j];
+		if(b[j] % p && y == -1) {
+			y = j;
+		}
+	}
+	std :: cout << x + y;
+}
+int main() {
+	//freopen("1.in", "r", stdin);
+	//freopen("1.out", "w", stdout);
+	std :: ios :: sync_with_stdio(false);
+	std :: cin.tie(0);
+	std :: cout.tie(0);
+	int _ = 1;
+	//std :: cin >> _; 
+	while(_ --) solve();
+	return 0;
+}
