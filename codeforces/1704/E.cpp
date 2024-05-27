6b5a6f9f126d7518bb50f2f548aa13267ecf12f4
mdarikrayhan <mdarikrayhan@gmail.com>
2024-05-27 06:04:00 +0600

    Add solution for problem `E - Count Seconds` Link: https://codeforces.com/contest/1704/problem/E Tags: brute force, constructive algorithms, dp, graphs, implementation, math, *2200 Ref: https://codeforces.com/contest/1704/submission/262809302

--- codeforces/1704/E.cpp
+++ codeforces/1704/E.cpp
@@ -1,45 +1,65 @@
-#include<cstdio>
-#include<vector>
-#include<queue>
+// LUOGU_RID: 160133527
+//A tree without skin will surely die.
+//A man without face is invincible.
+#include<bits/stdc++.h>
 using namespace std;
-const int N=1002,mod=998244353;
-int T,n,m,a[N],r[N],num,d[N],cnt;
-vector<int>e[N];
-queue<int>q;
-int main(){
-	scanf("%d",&T);
-	while(T--){
-		scanf("%d%d",&n,&m),num=cnt=0;
-		for(int i=1;i<=n;++i){
-			scanf("%d",a+i);
-			e[i].clear(),r[i]=0;
-		}
-		for(int u,v;m--;){
-			scanf("%d%d",&u,&v);
-			e[u].push_back(v),++r[v];
-		}
-		for(int i=1;i<=n;++i)
-			if(!r[i]) q.push(i);
-		for(int u;q.size();){
-			u=q.front(),q.pop();
-			d[++num]=u;
-			for(int v:e[u])
-				if(!--r[v]) q.push(v);
-		}
-		for(int i=1;i<n;++i){
-			bool flag=0;
-			for(int j=n;j;--j)
-				if(a[d[j]]>0){
-					--a[d[j]],flag=1;
-					for(int v:e[d[j]]) ++a[v];
-				}
-			if(flag) ++cnt;
-			else break;
-		}
-		for(int i=1;i<=n;++i)
-			for(int v:e[d[i]])
-				(a[v]+=a[d[i]])%=mod;
-		printf("%d\n",(a[d[n]]+cnt)%mod);
+#define int long long
+#define mid ((l+r)>>1)
+#define lowbit(x) (x&-x)
+#define all(x) (x).begin(),(x).end()
+#define rep(i,j,k) for (int i=j;i<=k;++i)
+#define per(i,j,k) for (int i=j;i>=k;--i)
+#define Tim ((double)clock()/CLOCKS_PER_SEC)
+int const N=1e3+10;
+int const mod=998244353;
+int const inf=1e18;
+int n,m,rd[N],cd[N],val[N];
+vector<int>a[N];
+struct node{int lf,rg,len;};
+vector<node>Add[N];
+void solve(){
+	cin>>n>>m;
+	rep(i,1,n) a[i].clear(),Add[i].clear(),rd[i]=cd[i]=0,cin>>val[i];
+	while (m--){
+		int x,y;cin>>x>>y;
+		a[x].push_back(y);
+		++rd[y],++cd[x];
 	}
+	queue<int>q;
+	rep(i,1,n)
+		if (!rd[i]) q.push(i);
+	int ans=0;
+	while (!q.empty()){
+		int x=q.front();q.pop();
+		int lf=1,rg=val[x],len=val[x];
+		sort(all(Add[x]),[](node a,node b){
+			return (a.lf^b.lf)?(a.lf<b.lf):(a.rg<b.rg);
+		});
+		vector<node>V;
+		for (auto it:Add[x])
+			if (it.lf>rg){
+				if (lf<=rg) V.push_back((node){lf,rg,len});
+				lf=it.lf,rg=it.rg,len=it.len;
+			}else{
+				len+=it.len,len%=mod;
+				rg=min(inf,rg+it.rg-it.lf+1);
+			}
+		if (lf<=rg) V.push_back((node){lf,rg,len});
+		Add[x]=V;
+		if (Add[x].size() && !cd[x])
+			ans=(Add[x].back().lf+Add[x].back().len+mod-1)%mod;
+		for (auto v:a[x]){
+			for (auto it:Add[x]) Add[v].push_back((node){it.lf+1,min(inf,it.rg+1),it.len});
+			if (!--rd[v]) q.push(v);
+		}
+	}
+	cout<<ans<<'\n';
+}
+signed main(){
+	ios::sync_with_stdio(false);
+	cin.tie(0),cout.tie(0);
+	int t=1;
+	cin>>t;
+	while (t--) solve();
 	return 0;
 }
