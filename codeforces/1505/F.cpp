664d485eeeb388b1bafa3e270314a16f39d161a3
mdarikrayhan <mdarikrayhan@gmail.com>
2024-05-28 09:18:00 +0600

    Add solution for problem `F - Math` Link: https://codeforces.com/contest/1505/problem/F Tags: *special problem, math, *2200 Ref: https://codeforces.com/contest/1505/submission/262950349

---
+++ codeforces/1505/F.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define f first
+#define s second
+#define ll long long
+#define pb push_back
+#define IOS ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define mk make_pair
+#define yes cout << "YES" << '\n'
+#define no cout << "NO" << '\n'
+#define all(x) x.begin(),x.end()
+#define sz(x) (int) x.size()
+#pragma GCC target("avx2")
+#pragma GCC optimize("Ofast")
+#pragma comment(linker, "/stack:200000000")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#pragma GCC optimize("unroll-loops")
+//#define int long long
+//stoi(s)
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int x;
+  cin >> x;
+  cout << 2 - x * x << '\n';
+  return 0;
+}
