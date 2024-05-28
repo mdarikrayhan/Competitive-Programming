0cd7c6916d52ae6082d23a86d4eb08fb55e817a4
mdarikrayhan <mdarikrayhan@gmail.com>
2024-05-29 01:58:00 +0600

    Add solution for problem `G - Delicious Dessert` Link: https://codeforces.com/contest/1780/problem/G Tags: binary search, dsu, hashing, math, number theory, string suffix structures, *2400 Ref: https://codeforces.com/contest/1780/submission/263060789

--- codeforces/1780/G.cpp
+++ codeforces/1780/G.cpp
@@ -1,125 +1,97 @@
-#pragma GCC optimize("O3,unroll-loops")
-#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
-#include <iostream>
-#include <vector>
-#include <map>
-#include <set>
-#include <stack>
-#include <queue>
-#include <limits>
-#include <string>
-#include <array>
-#include <sstream>
-#include <numeric>
-#include <list>
-#include <deque>
-#include <cstring>
-#include <cmath>
-#include <cstdlib>
-#include <cstdio>
-#include <random>
-#include <algorithm>
-#include <climits>
-#include <complex>
-#include <cassert>
-#include <bitset>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-// Super Idol的笑容
-//    都没你的甜
-//  八月正午的阳光
-//    都没你耀眼
-//  热爱105°C的你
-// 滴滴清纯的蒸馏水
+typedef long long ll; 
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<vector<int> > vvi;
+typedef vector<pii> vii;
 
-#define itr(...) begin(__VA_ARGS__), end(__VA_ARGS__)
-#define fastIO() ios::sync_with_stdio(false),cin.tie(nullptr),cout.tie(nullptr)
-#define debug(x...) do { cout << "\033[32;1m" << #x <<" -> "; rd_debug(x); } while (0)
-void rd_debug() { cout << "\033[39;0m" << endl; }
-template<class T, class... Ts> void rd_debug(const T& arg,const Ts&... args) { cout << arg << " "; rd_debug(args...); }
-#define pb push_back
-#define PF(x) ((x) * (x))
-#define LF(x) ((x) * PF(x))
+template <typename T>
+inline T gcd(T a, T b) { while (b != 0) swap(b, a %= b); return a; }
 
-typedef long long ll;
-typedef unsigned long long ull;
-typedef pair<int, int> PII;
-typedef pair<ll, ll> PLL;
-
-const double eps = 1e-6;
-const int MOD = 1e9 + 7;
-const int inf = 0x3f3f3f3f;
-const ll infl = 0x3f3f3f3f3f3f3f3fll;
-mt19937_64 mrand(random_device{}());
-ll rnd(ll x) { return mrand() % x;}
-template<int N>
-struct Suffix_Automation{
-	int len[N], fa[N], minr[N], ch[N][26], rt[N], tot, last, cnt[N];
-	void clear(){
-		tot = last = 1;
-		memset(ch[1], 0, sizeof ch[1]);
-	}
-	Suffix_Automation(){
-		clear();
-	}
-	void ins(const int& c, const int& pos = 0){
-		int p = last, np = last = ++tot;
-		minr[np] = len[np] = len[p] + 1;
-		cnt[np]++;
-		memset(ch[np], 0, sizeof ch[np]);
-		for(; p && !ch[p][c]; p = fa[p]){
-			ch[p][c] = np;
-		}
-		if(!p){
-			return void(fa[np] = 1);
-		}
-		int q = ch[p][c];
-		if(len[p] + 1 == len[q]){
-			return void(fa[np] = q);
-		}
-		int nq = ++tot;
-		len[nq] = len[p] + 1;
-		minr[nq] = minr[q];
-		fa[nq] = fa[q];
-		memcpy(ch[nq], ch[q], sizeof ch[q]);
-		fa[np] = fa[q] = nq;
-		for(; p && ch[p][c] == q; p = fa[p]){
-			ch[p][c] = nq; 
-		}
-	}
+const int MAX_LEN = 1000000 + 10;
+struct state{
+	int len = 0; 
+	int cnt = 1; 
+	int link = 0; 
+	map<char, int> nxt; 
 };
-Suffix_Automation<2000005> sam;
-int c[2000005], rk[2000005];
-char S[1000005];
-int n;
-void solve(){
-	cin >> n;
-	cin >> (S + 1);
-//	Shai();
-	sam.clear();
-	for(int i = 1; i <= n; i++){
-		sam.ins(S[i]-'a');
+
+state st[MAX_LEN * 2];
+int sz = 0, last = 0;
+
+void SA_init() {
+	st[0].len = 0; 
+	st[0].link = -1; 
+	sz++; 
+	last = 0;
+}
+
+void SA_extend(char c) {  
+	int cur = sz++; 
+	st[cur].len = st[last].len + 1; 
+	int p = last; 
+	while(p != -1 && !st[p].nxt.count(c)) {
+		st[p].nxt[c] = cur; 
+		p = st[p].link; 
 	}
-	for(int i = 1; i <= sam.tot; i++) ++c[sam.len[i]];
-	for(int i = 1; i <= n; i++) c[i] += c[i-1];
-	for(int i = 1; i <= sam.tot; i++) rk[--c[sam.len[i]]] = i;
-	for(int i = sam.tot; --i; ){
-		sam.cnt[sam.fa[rk[i]]] += sam.cnt[rk[i]];
-	}
-	ll ans = 0;
-	for(int i = 2; i <= sam.tot; i++){
-		for(int j = sam.len[sam.fa[i]] + 1; j <= sam.len[i] && j <= sam.cnt[i]; j++){
-			if(sam.cnt[i] % j == 0){
-				ans += sam.cnt[i];
+
+	if(p == -1) {
+		st[cur].link = 0; 
+	}else{
+		int q = st[p].nxt[c]; 
+		if(st[p].len + 1 == st[q].len) { 
+			st[cur].link = q; 
+		}else{
+			int clone = sz++; 
+			st[clone].len = st[p].len + 1; 
+			st[clone].nxt = st[q].nxt; 
+			st[clone].link = st[q].link; 
+			st[clone].cnt = 0; 
+			while(p != -1 && st[p].nxt[c] == q) {
+				st[p].nxt[c] = clone; 
+				p = st[p].link; 
 			}
+			st[q].link = st[cur].link = clone; 
 		}
 	}
-	cout << ans << "\n";
+	last = cur; 
 }
-int main() {
-  fastIO();
-  solve();
 
-  return 0;
+vi sorted_by_lens[MAX_LEN]; 
+
+signed main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	
+	int n; cin >> n; 
+	string t; cin >> t; 
+	SA_init();
+	for(char c : t) SA_extend(c);
+
+	for(int i = 0; i <= sz; i++) sorted_by_lens[st[i].len].push_back(i);
+	for(int i = MAX_LEN - 1; i > 0; i--) {
+		for(int cur : sorted_by_lens[i]) {
+			st[st[cur].link].cnt += st[cur].cnt; 
+		}
+	}
+
+	vector<vii> events(sz); 
+	for(int i = 1; i < sz; i++) {
+		int min_len = st[st[i].link].len; 
+		int max_len = st[i].len; 
+		if(min_len >= 0) events[min_len].emplace_back(st[i].cnt, -1);
+		if(max_len >= 0) events[max_len].emplace_back(st[i].cnt, 1);
+	}
+
+	ll ans = 0;
+	vi counter(sz);
+	for(int i = 1; i < sz; i++) {
+		for(int j = i; j < sz; j += i) counter[j]++; 
+		for(auto j : events[i]) {
+			ans += 1LL * j.first * counter[j.first] * j.second; 
+		}
+	}
+	cout << ans << "\n"; 
 }
