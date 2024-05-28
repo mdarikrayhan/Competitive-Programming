87d3deb24a01e3c43e169bd0899802b12fd60d28
mdarikrayhan <mdarikrayhan@gmail.com>
2024-05-29 04:20:00 +0600

    Add solution for problem `F - Strange Instructions` Link: https://codeforces.com/contest/1621/problem/F Tags: data structures, greedy, implementation, *2700 Ref: https://codeforces.com/contest/1621/submission/263068654

---
+++ codeforces/1621/F.cpp
@@ -0,0 +1,236 @@
+//Never compromise. Not even in the face of Armageddon.
+
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+
+//pragmas
+#pragma GCC optimize("03")
+#pragma GCC target("avx,avx2,fma")
+//#pragma GCC optimization("01, 02, 03, 0fast, unroll-loops")              //Retard mode
+//#pragma GCC optimize("Ofast,unroll-loops,omit-frame-pointer,inline")     //Optimization flags
+//#pragma GCC option("arch=native,tune=native,no-zero-upper")              //Enable AVX
+
+//types
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define ll long long int
+#define TYPE long long int
+#define ppair pair<TYPE, TYPE>
+#define vecvec(a, i, j, x) vector<vector<TYPE>> a (i, vector<TYPE> (j, x))
+#define vecvecvec(a, i, j, k, x) vector<vector<vector<TYPE>>> dp (i + 1, vector<vector<TYPE>>(j + 1, vector<TYPE>(k + 1, x)))
+
+using namespace std;
+using namespace __gnu_pbds;
+
+//random stuff
+#define all(a) a.begin(),a.end()
+#define endl "\n"
+#define pb push_back
+#define mp make_pair
+#define sz(x) (long long int)x.size()
+#define F first
+#define S second
+#define sp " " 
+const ll inf = 0x3f3f3f3f, INF = 1e18;
+const int MOD = 1e9 + 7;
+
+typedef tree<long long int, null_type, less<long long int>, rb_tree_tag, tree_order_statistics_node_update> indexed_set;             //for multiset, use less_equal and erase x in multiset a -> a.erase(a.find_by_order(a.order_of_key(x)))
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+#define safe_map unordered_map<long long, int, custom_hash>
+#define hashmap gp_hash_table<int, int, custom_hash>
+
+
+//debug
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#define reach cerr<<"reached"<<endl
+#else
+#define debug(x...)
+#define reach 
+#endif
+
+
+/*---------------------------------------------------------------------------------------------------------------------------*/
+ll gcd(ll a, ll b) {if (b > a) {return gcd(b, a);} if (b == 0) {return a;} return gcd(b, a % b);}
+ll expo(ll a, ll b, ll mod) {ll res = 1; while (b > 0) {if (b & 1)res = (res * a) % mod; a = (a * a) % mod; b = b >> 1;} return res;}
+void extendgcd(ll a, ll b, ll*v) {if (b == 0) {v[0] = 1; v[1] = 0; v[2] = a; return ;} extendgcd(b, a % b, v); ll x = v[1]; v[1] = v[0] - v[1] * (a / b); v[0] = x; return;} //pass an arry of size1 3
+ll mminv(ll a, ll b) {ll arr[3]; extendgcd(a, b, arr); return arr[0];} //for non prime b
+ll mminvprime(ll a, ll b) {return expo(a, b - 2, b);}
+bool revsort(ll a, ll b) {return a > b;}
+void swap(int &x, int &y) {int temp = x; x = y; y = temp;}
+ll combination(ll n, ll r, ll m, ll *fact, ll *ifact) {ll val1 = fact[n]; ll val2 = ifact[n - r]; ll val3 = ifact[r]; return (((val1 * val2) % m) * val3) % m;}
+void google(int t) {cout << "Case #" << t << ": ";}
+vector<ll> sieve(int n) {int*arr = new int[n + 1](); vector<ll> vect; for (ll i = 2; i <= n; i++)if (arr[i] == 0) {vect.push_back(i); for (ll j = 2 * i; j <= n; j += i)arr[j] = 1;} return vect;}
+ll mod_add(ll a, ll b, ll m = MOD) {a = a % m; b = b % m; return (((a + b) % m) + m) % m;}
+ll mod_mul(ll a, ll b, ll m = MOD) {a = a % m; b = b % m; return (((a * b) % m) + m) % m;}
+ll mod_sub(ll a, ll b, ll m = MOD) {a = a % m; b = b % m; return (((a - b) % m) + m) % m;}
+ll mod_div(ll a, ll b, ll m = MOD) {a = a % m; b = b % m; return (mod_mul(a, mminvprime(b, m), m) + m) % m;}  //only for prime m
+ll phin(ll n) {ll number = n; if (n % 2 == 0) {number /= 2; while (n % 2 == 0) n /= 2;} for (ll i = 3; i <= sqrt(n); i += 2) {if (n % i == 0) {while (n % i == 0)n /= i; number = (number / i * (i - 1));}} if (n > 1)number = (number / n * (n - 1)) ; return number;} //O(sqrt(N))
+void precision(int a) {cout << setprecision(a) << fixed;}
+ll ceil_div(ll x, ll y){return (x + y - 1) / y;}
+unsigned long long power(unsigned long long x,ll y, ll p){unsigned long long res = 1;x = x % p; while (y > 0){if (y & 1)res = (res * x) % p;y = y >> 1;x = (x * x) % p;}return res;}
+unsigned long long modInverse(unsigned long long n,int p){return power(n, p - 2, p);}
+ll nCr(ll n,ll r, ll p){if (n < r)return 0;if (r == 0)return 1;unsigned long long fac[n + 1];fac[0] = 1;for (int i = 1; i <= n; i++)fac[i] = (fac[i - 1] * i) % p;return (fac[n] * modInverse(fac[r], p) % p* modInverse(fac[n - r], p) % p)% p;}
+ll accumulate(const vector<long long int> &nums){ll sum = 0; for(auto x : nums) sum += x; return sum;}
+ll tmax(ll a, ll b, ll c = 0, ll d = -INF, ll e = -INF, ll f = -INF){return max(a, max(b, max(c, max(d, max(e, f)))));}
+int log2_floor(unsigned long long i) {return i ? __builtin_clzll(1) - __builtin_clzll(i) : -1;}
+string bin(ll n){return bitset<32>(n).to_string();}
+/*--------------------------------------------------------------------------------------------------------------------------*/
+
+const int MAXN = 2e5+5;
+
+int n, a, b, c;
+string s;
+
+
+//code starts
+int main()
+{
+    fastio();
+    ll tt;
+    cin >> tt;
+    while(tt--)
+    {
+        cin >> n >> a >> b >> c >> s;
+        
+        int edge = 0;
+        vector<int> doable(4, 0);
+
+        priority_queue<int, vector<int>, greater<int>> window;
+        for(int i = 0; i < n; i ++)
+        {
+            int j = i;
+            while(j < n and s[i] == s[j])   ++ j;
+
+            if(s[i] == '0')
+            {
+                doable[1] += j - i - 1;
+                if(i != 0 and j != n)
+                {
+                    if(j - i > 1)   window.push(j - i - 1);
+                    else    ++ doable[3];
+                }
+                else    ++ edge;
+            }
+            else    doable[2] += j - i - 1;
+            i = j - 1;
+        }
+
+        ll score = 0;
+
+        if(doable[1] >= doable[2])
+        {
+            while(doable[2] > 0)
+            {
+                score += a + b;
+                -- doable[1], -- doable[2];
+                
+                if(!window.empty())
+                {
+                    int opt = window.top() - 1;
+                    window.pop();
+
+                    if(opt > 0) window.push(opt);
+                    else    ++ doable[3];
+                }
+            }
+
+            while(doable[3] --)
+                if(b > c)
+                    score += b - c;
+            
+            if(doable[1])   score += a;
+        }
+        else if(doable[1] < doable[2])
+        {
+            while(doable[1] > 0)
+            {
+                score += a + b;
+                -- doable[1], -- doable[2];
+
+                if(!window.empty())
+                {
+                    int opt = window.top() - 1;
+                    window.pop();
+                    
+                    if(opt > 0) window.push(opt);
+                    else    ++ doable[3];
+                }
+            }
+
+            -- doable[2], score += b;
+
+            while(doable[3] --)
+                if(b > c)
+                    score += b - c;
+            
+            while((edge --) and (doable[2] --))
+                if(b > c)
+                    score +=  b - c;
+        }
+
+        cout << score << endl;
+    }
+}
+
+/*
+obs: 
+1) number of 1-moves constant
+2) never optimal to do 3-move if 2-move available, as performing a 2-move may increase number of 3-moves but never vice versa
+3) performing a 3-move increases doable 2-moves
+
+cases:
+1) (1-moves = 2-moves)
+    here first we can do like 1 2 1 2 1 2 and then if
+        a) b > c do 3 2 3 2 3 2 3 2
+        b) b <= c do nothing
+2) (1-moves < 2-moves)
+    here first we can do like 2 1 2 1 2 1 2 by greedily picking from smallest window such that generated 3-moves are maximised
+        a) b > c do 3 2 3 2 3 2 3 2, if there are any at edges, and also some remaining 2-moves then perform these as well
+        b) b <= c do nothing
+3) (1-moves > 2-moves)
+    here first we can do like 1 2 1 2 1 2 1 2 by greedily picking from smallest window such that generated 3-moves are maximised and all 2-moves are exhausted
+    now we can only perform some 3-2 combos and then end this shit with a final 1 move
+        a) b > c perform 3 2 3 2 3 2 3 2 and end with a 1 move
+        b) b <= c do nothing
+*/
+
+
+
+// There is an idea of a Patrick Bateman. Some kind of abstraction.
+// But there is no real me. Only an entity. Something illusory. 
+// And though I can hide my cold gaze, and you can shake my hand and
+// feel flesh gripping yours, and maybe you can even sense our lifestyles
+// are probably comparable, I simply am not there.
