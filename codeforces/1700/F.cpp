173370b0c4c5cd091ec4d88d4f4f516d1104aacf
mdarikrayhan <mdarikrayhan@gmail.com>
2024-05-28 16:59:00 +0600

    Add solution for problem `F - Puzzle` Link: https://codeforces.com/contest/1700/problem/F Tags: constructive algorithms, dp, greedy, *2600 Ref: https://codeforces.com/contest/1700/submission/262998901

--- codeforces/1700/F.cpp
+++ codeforces/1700/F.cpp
@@ -1,19 +1,107 @@
-#include<iostream>
+#include <bits/stdc++.h>
+ 
 using namespace std;
-int const N=233333;
-long long n,ans,u,d,a[5][N];
-int main(){
-ios::sync_with_stdio(0);
-cin>>n;
-for(int i=1;i<=4;i++)
-for(int j=1;j<=n;j++)
-cin>>a[i][j];
-for(int i=1,t;i<=n;i++){
-u+=a[1][i]-a[3][i];
-d+=a[2][i]-a[4][i];
-if(u>0&&0>d) {t=min(u,-d);ans+=t;u-=t;d+=t;}
-if(u<0&&0<d) {t=min(-u,d);ans+=t;u+=t;d-=t;}
-ans+=abs(u)+abs(d);
+
+template<class F, class S>
+ostream &operator<<(ostream &s, const pair<F, S> &v) {
+    s << "(" << v.first << ", " << v.second << ")";
+    return s;
 }
-cout<<(u||d?-1:ans)<<"\n";
+ 
+template<ranges::range T> requires (!is_convertible_v<T, string_view>)
+istream &operator>>(istream &s, T &&v) { 
+    for (auto &&x : v) s >> x; 
+    return s; 
 }
+template<ranges::range T> requires (!is_convertible_v<T, string_view>)
+ostream &operator<<(ostream &s, T &&v) { 
+    for (auto &&x : v) s << x << ' '; 
+    return s; 
+}
+ 
+#ifdef LOCAL
+template<class... T> void dbg(T... x) {
+    char e{};
+    ((cerr << e << x, e = ' '), ...);
+}
+#define debug(x...) dbg(#x, '=', x, '\n')
+#else
+#define debug(...) ((void)0)
+#endif
+ 
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define ff first
+#define ss second
+ 
+template <class T> inline constexpr T inf = numeric_limits<T>::max() / 2;
+template <class T> bool chmin(T &a, T b) { return (b < a and (a = b, true)); }
+template <class T> bool chmax(T &a, T b) { return (a < b and (a = b, true)); }
+ 
+using u32 = unsigned int;
+using i64 = long long;
+using u64 = unsigned long long;
+using i128 = __int128;
+
+constexpr i64 mod = 998244353;
+
+void solve() {
+    int n;
+    cin >> n;
+
+    array A{vector<int>(n), vector<int>(n)};
+    cin >> A;
+
+    for (int i : {0, 1})
+        for (int j = 0; j < n; j++) {
+            int x;
+            cin >> x;
+            A[i][j] -= x;
+        }
+
+    auto sgn = [&](i64 x) {
+        return (x > 0) - (x < 0);
+    };
+
+    i64 ans = 0;
+    for (int i = 0; i < n; i++) {
+        if (sgn(A[0][i]) * sgn(A[1][i]) < 0) {
+            ans++;
+            if (A[0][i] > 0) {
+                A[0][i]--;
+                A[1][i]++;
+            } else {
+                A[0][i]++;
+                A[1][i]--;
+            }
+        }
+        ans += abs(A[0][i]) + abs(A[1][i]);
+        if (i + 1 < n) {
+            A[0][i + 1] += A[0][i];
+            A[1][i + 1] += A[1][i];
+        }
+    }
+
+    if (A[0][n - 1] + A[1][n - 1] != 0) {
+        cout << -1 << '\n';
+    } else {
+        cout << ans << '\n';
+    }
+    
+}
+
+signed main() {
+    cin.tie(0)->sync_with_stdio(false);
+    cin.exceptions(cin.failbit);
+
+    int t = 1;
+    // cin >> t;
+    
+    while (t--) {
+        solve();
+    }
+    
+    return 0;
+}
+
+
