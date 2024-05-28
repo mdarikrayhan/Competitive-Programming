21e3a64bd60f97d0f5ef5dd4200a4b52639890fc
mdarikrayhan <mdarikrayhan@gmail.com>
2024-05-29 03:57:00 +0600

    Add solution for problem `C - Two permutations` Link: https://codeforces.com/contest/323/problem/C Tags: data structures, *2400 Ref: https://codeforces.com/contest/323/submission/263067604

--- codeforces/323/C.cpp
+++ codeforces/323/C.cpp
@@ -1,31 +1,92 @@
-#include<iostream>
-const int mxn=1e6+1;
-int idx[mxn],l[mxn*21],r[mxn*21],c[mxn*21],rt[mxn],s,j,n,m;
-int f(int i){return (i-1+j)%n+1;}
-void add(int &x,int y,int i,int ll,int rr){
-    x=++s,c[x]=c[y]+1,l[x]=l[y],r[x]=r[y];
-    if(ll^rr){
-        int mm=(ll+rr)>>1;
-        if(i<=mm)add(l[x],l[y],i,ll,mm);
-        else add(r[x],r[y],i,mm+1,rr);
-    }
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define debug1 cout<<"test1"<<endl;
+#define debug2 cout<<"test2"<<endl;
+ll read(){
+    ll x = 0;char ch = getchar();int f = 1;
+    while(ch<'0'||ch>'9'){if(ch=='-') f = -1;ch = getchar();}
+    while(ch>='0' && ch<='9'){x = x*10+ch-'0';ch = getchar();}
+    return x*f;
 }
-int ask(int x,int ql,int qr,int ll,int rr){
-    if(!x||ll>qr||rr<ql)return 0;
-    if(ll>=ql&&rr<=qr)return c[x];
-    int mm=(ll+rr)>>1;
-    return ask(l[x],ql,qr,ll,mm)+ask(r[x],ql,qr,mm+1,rr);
+//std::chrono::system_clock::time_point nowtime(){return std::chrono::high_resolution_clock::now();}
+//void printtime(std::chrono::system_clock::time_point a , std::chrono::system_clock::time_point b){
+    //auto c = std::chrono::duration_cast<std::chrono::nanoseconds>(b - a);printf(\"%.3f\n\" , c.count() * 1e-9);return;
+//}
+const int N = 40000007;
+
+int temp[2000007];
+int ls[N] , rs[N] , sum[N] , tot = 0 , rt[2000007];
+int n = read();
+void update(int& u,int pre ,int l,int r ,int x){
+    if(pre == 0) return;
+    u = ++ tot;
+    sum[u] = sum[pre] + 1 , ls[u] = ls[pre] , rs[u] = rs[pre]; 
+
+    int mid = (l + r) >> 1;
+    if(x <= mid) update(ls[u] , ls[pre] , l , mid , x);
+    else update(rs[u] , rs[pre] , mid + 1 , r , x);
+    return;
 }
-int main(){
-    scanf("%d",&n);
-    for(int i=1,a;i<=n;++i)scanf("%d",&a),idx[a]=i;
-    for(int i=1,b;i<=n;++i)scanf("%d",&b),add(rt[i],rt[i-1],idx[b],1,n);
-    scanf("%d",&m);
-    for(int i=1,x,y,z,w;i<=m;++i){
-        scanf("%d%d%d%d",&x,&y,&z,&w);
-        x=f(x),y=f(y),z=f(z),w=f(w);
-        if(x>y)std::swap(x,y);
-        if(z>w)std::swap(z,w);
-        printf("%d\n",j=(ask(rt[w],x,y,1,n)-ask(rt[z-1],x,y,1,n))),++j;
+int query(int u,int l,int r,int x,int y){
+    if(r < x || y < l) return 0;
+    if(u == 0) return 0;
+    if(x <= l && r <= y) return sum[u];
+    int mid = (l + r) >> 1;
+    return query(ls[u] , l , mid , x , y) + query(rs[u] , mid + 1 , r , x , y);
+}
+
+void build(int& u,int l,int r){
+    u = ++ tot;
+    if(l == r) return;
+    int mid = (l + r) >> 1;
+    build(ls[u] , l , mid) , build(rs[u] , mid + 1 , r);
+    return;
+}
+
+
+
+int f(int z , int x){
+    return ((z - 1 + x) % n) + 1;
+}
+int main()
+{
+    //freopen(".in","r",stdin);
+    //freopen(".out","w",stdout);
+
+    for(int i = 1;i <= n;i ++){
+        temp[read()] = i;
     }
+
+    build(rt[0] , 1 , n);
+
+    for(int i = 1;i <= n;i ++){
+        // num[i] = temp[read()];
+        update(rt[i] , rt[i - 1] , 1 , n , temp[read()]);
+    }
+
+
+
+    int x = 0;
+
+    int m = read();
+    for(int i = 1;i <= m;i ++){
+
+        int a = read() , b = read() , c = read() , d = read();
+
+        a = f(a , x) , b = f(b , x) , c = f(c , x) , d = f(d , x);
+
+        int l1 = min(a , b) , r1 = max(a , b) , l2 = min(c , d) , r2 = max(c , d);
+
+        int ans1 = query(rt[r2] , 1 , n , l1 , r1);
+        int ans2 = query(rt[l2 - 1] , 1 , n , l1 , r1);
+
+        printf("%d\n" , ans1 - ans2);
+
+        x = ans1 - ans2 + 1;
+    }
+
+
+
+    return 0;
 }
