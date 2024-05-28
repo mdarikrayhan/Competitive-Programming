aacf51b718f78aa27d2a9abcc877b4d6dffa11b4
mdarikrayhan <mdarikrayhan@gmail.com>
2024-05-28 12:15:00 +0600

    Add solution for problem `D - GCD Table` Link: https://codeforces.com/contest/338/problem/D Tags: chinese remainder theorem, math, number theory, *2900 Ref: https://codeforces.com/contest/338/submission/262966047

--- codeforces/338/D.cpp
+++ codeforces/338/D.cpp
@@ -1,22 +1,70 @@
-#include<bits/stdc++.h>
+// LUOGU_RID: 160377973
+#include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-ll a[10005];
-void exgcd(ll a,ll b,ll &x,ll &y){if(!b)return x=1,y=0,void();exgcd(b,a%b,y,x);y-=(a/b)*x;}
+int k;
+long long n, m, z[10010];
+long long gcd(long long a, long long b)
+{
+    if (!b)
+        return a;
+    else
+        return gcd(b, a % b);
+}
 int main()
 {
-	ll n,m,x,l=0,r=0;int k;
-	cin>>n>>m>>k>>a[0];l=a[0];r=0;
-	for(int i=1;i<k;i++)
-	{
-		cin>>a[i];ll t=__gcd(l,a[i]);
-		if(1.0*l/t*a[i]>1e13||l/t*a[i]>n||(-i-r)%t){puts("NO");return 0;}
-		ll p,q,m=l/t*a[i];exgcd(l/t,a[i]/t,p,q);
-		r=(r+(ll)((__int128)p*(-i-r)/t%m*l%m)+m)%m;l=m;
-	}
-	if(!r)r=l;
-	if(l>n||r+k-1>m){puts("NO");return 0;}
-	for(int i=0;i<k;i++)if(__gcd(l,r+i)!=a[i]){puts("NO");return 0;}
-	puts("YES");
-	return 0;
+    cin >> n >> m >> k;
+    for (int a = 1; a <= k; a++)
+        cin >> z[a];
+    long long v = z[1];
+    for (int a = 2; a <= k; a++)
+        if (v > n)
+            break;
+        else
+            v = v / gcd(v, z[a]) * z[a];
+    if (v > n)
+    {
+        printf("NO\n");
+        return 0;
+    }
+    long long v1 = z[1];
+    long long mo1 = z[1];
+    if (v1 + k - 1 > m)
+    {
+        printf("NO\n");
+        return 0;
+    }
+    for (int a = 2; a <= k; a++)
+    {
+        v1 %= mo1;
+        long long v2 = ((-(a - 1)) % z[a] + z[a]) % z[a];
+        long long mo2 = z[a];
+        long long cnt = 0;
+        if (mo1 > mo2)
+        {
+            while (cnt <= mo2 && v1 + k - 1 <= m && v1 % mo2 != v2)
+                v1 += mo1, cnt++;
+        }
+        else
+        {
+            while (cnt <= mo1 && v2 + k - 1 <= m && v2 % mo1 != v1)
+                v2 += mo2, cnt++;
+            v1 = v2;
+        }
+        v2 %= mo2;
+        if (v1 % mo2 != v2 || v1 + k - 1 > m)
+        {
+            printf("NO\n");
+            return 0;
+        }
+        mo1 = mo1 / gcd(mo1, mo2) * mo2;
+    }
+    if (!v1)
+        v1 = mo1;
+    for (long long a = 1; a <= k; a++)
+        if (gcd(v1 + a - 1, v) != z[a])
+        {
+            printf("NO\n");
+            return 0;
+        }
+    printf("YES\n");
 }
