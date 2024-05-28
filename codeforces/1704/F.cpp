c74f9b8417e836b77afec778021336cdc40854c8
mdarikrayhan <mdarikrayhan@gmail.com>
2024-05-28 16:21:00 +0600

    Add solution for problem `F - Colouring Game` Link: https://codeforces.com/contest/1704/problem/F Tags: constructive algorithms, dp, games, *2600 Ref: https://codeforces.com/contest/1704/submission/262994304

--- codeforces/1704/F.cpp
+++ codeforces/1704/F.cpp
@@ -1,20 +1,44 @@
-#include<bits/stdc++.h> 
+// LUOGU_RID: 160133317
+//A tree without skin will surely die.
+//A man without face is invincible.
+#include<bits/stdc++.h>
 using namespace std;
-int const M=500500,sg[]={4,8,1,1,2,0,3,1,1,0,3,3,2,2,4,4,5,5,9,3,3,0,1,1,3,0,2,1,1,0,4,5,3,7};
-int i,j,n,T,R,Ans;char s[M];
-int SG(int x){return (x==0||x==1||x==15||x==35)?0:(x==17||x==18||x==32||x==52)?2:sg[x%34];}
-int main(){
-	scanf("%d",&T);
-	while (T--){
-		scanf("%d%s",&n,s+1);Ans=R=0;
-		for (i=1;i<=n;i++) R+=s[i]=='R';
-		if ((R<<1)>n) {puts("Alice");continue;}
-		if ((R<<1)<n) {puts("Bob");continue;}
-		for (i=1;i<=n;i=j){ j=i+1;
-			while (j<=n&&s[j]!=s[j-1]) j++;
-			Ans^=SG(j-i);
-		} 
-		puts(Ans?"Alice":"Bob");
-	}
-	return 0;
+#define int long long
+#define mid ((l+r)>>1)
+#define lowbit(x) (x&-x)
+#define all(x) (x).begin(),(x).end()
+#define rep(i,j,k) for (int i=j;i<=k;++i)
+#define per(i,j,k) for (int i=j;i>=k;--i)
+#define Tim ((double)clock()/CLOCKS_PER_SEC)
+int const N=5e5+10;
+int sg[N];
+void solve(){
+    int n;string s;
+    cin>>n>>s,s=" "+s;
+    int cntR=0,cntB=0;
+    rep(i,1,n) cntR+=(s[i]=='R'),cntB+=(s[i]=='B');
+    if (cntR>cntB) return cout<<"Alice\n",void();
+    if (cntB>cntR) return cout<<"Bob\n",void();
+    int smsg=0,len=0;
+    rep(i,1,n){
+        if (s[i]==s[i-1]) smsg^=sg[len],len=0;
+        ++len;
+    }
+    smsg^=sg[len];
+    if (smsg) cout<<"Alice\n";
+    else cout<<"Bob\n";
+}
+signed main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0),cout.tie(0);
+    rep(i,2,100){
+        map<int,int>vis;
+        rep(j,1,i-1) vis[sg[j-1]^sg[i-j-1]]=1;
+        while (vis[sg[i]]) ++sg[i];
+    }
+    rep(i,101,N-1) sg[i]=sg[i-34];
+    int t=1;
+    cin>>t;
+    while (t--) solve();
+    return 0;
 }
