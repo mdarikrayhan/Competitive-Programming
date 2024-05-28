b839e20faa2c59f6a956276e18b94c2f98f53466
mdarikrayhan <mdarikrayhan@gmail.com>
2024-05-29 02:09:00 +0600

    Add solution for problem `F - Bracket Insertion` Link: https://codeforces.com/contest/1781/problem/F Tags: combinatorics, dp, math, trees, *2700 Ref: https://codeforces.com/contest/1781/submission/263061592

--- codeforces/1781/F.cpp
+++ codeforces/1781/F.cpp
@@ -1,124 +1,37 @@
-#include <bits/stdc++.h>
-
+#include<bits/stdc++.h>
 using namespace std;
-
-template <typename T>
-T inverse(T a, T m) {
-  T u = 0, v = 1;
-  while (a != 0) {
-    T t = m / a;
-    m -= t * a; swap(a, m);
-    u -= t * v; swap(u, v);
-  }
-  assert(m == 1);
-  return u;
+#define ll long long
+int n;
+ll p,dp[505][505],g[505][505],ans,f[505] = {1},inv[10005] = {1,1},invf[505] = {1};
+const ll mod = 998244353;
+inline ll C(ll n,ll m)
+{
+	if(n < 0||m < 0||n < m)return 0;
+	return f[n] * invf[m] % mod * invf[n - m] % mod;
 }
-
-template <typename T>
-class Modular {
- public:
-  using Type = typename decay<decltype(T::value)>::type;
-
-  constexpr Modular() : value() {}
-  template <typename U>
-  Modular(const U& x) {
-    value = normalize(x);
-  }
-
-  template <typename U>
-  static Type normalize(const U& x) {
-    Type v;
-    if (-mod() <= x && x < mod()) v = static_cast<Type>(x);
-    else v = static_cast<Type>(x % mod());
-    if (v < 0) v += mod();
-    return v;
-  }
-
-  const Type& operator()() const { return value; }
-  template <typename U>
-  explicit operator U() const { return static_cast<U>(value); }
-  constexpr static Type mod() { return T::value; }
-
-  Modular& operator+=(const Modular& other) { if ((value += other.value) >= mod()) value -= mod(); return *this; }
-  Modular& operator-=(const Modular& other) { if ((value -= other.value) < 0) value += mod(); return *this; }
-  template <typename U> Modular& operator+=(const U& other) { return *this += Modular(other); }
-  template <typename U> Modular& operator-=(const U& other) { return *this -= Modular(other); }
-  Modular operator-() const { return Modular(-value); }
-
-  template <typename U = T>
-  typename enable_if<is_same<typename Modular<U>::Type, int>::value, Modular>::type& operator*=(const Modular& rhs) {
-    value = normalize(static_cast<int64_t>(value) * static_cast<int64_t>(rhs.value));
-    return *this;
-  }
-
-  Modular& operator/=(const Modular& other) { return *this *= Modular(inverse(other.value, mod())); }
-
-  template <typename V, typename U>
-  friend V& operator>>(V& stream, Modular<U>& number);
-
- private:
-  Type value;
-};
-
-template <typename T> Modular<T> operator+(const Modular<T>& lhs, const Modular<T>& rhs) { return Modular<T>(lhs) += rhs; }
-template <typename T, typename U> Modular<T> operator+(const Modular<T>& lhs, U rhs) { return Modular<T>(lhs) += rhs; }
-template <typename T, typename U> Modular<T> operator+(U lhs, const Modular<T>& rhs) { return Modular<T>(lhs) += rhs; }
-
-template <typename T> Modular<T> operator-(const Modular<T>& lhs, const Modular<T>& rhs) { return Modular<T>(lhs) -= rhs; }
-template <typename T, typename U> Modular<T> operator-(const Modular<T>& lhs, U rhs) { return Modular<T>(lhs) -= rhs; }
-template <typename T, typename U> Modular<T> operator-(U lhs, const Modular<T>& rhs) { return Modular<T>(lhs) -= rhs; }
-
-template <typename T> Modular<T> operator*(const Modular<T>& lhs, const Modular<T>& rhs) { return Modular<T>(lhs) *= rhs; }
-
-template <typename U, typename T>
-U& operator<<(U& stream, const Modular<T>& number) {
-  return stream << number();
+int main()
+{
+	for(int i = 2;i <= 10000;i = -~i)
+		inv[i] = inv[mod % i] * (mod - mod / i) % mod;
+	for(int i = 1;i <= 505;i = -~i)
+		f[i] = f[i - 1] * i % mod,
+		invf[i] = invf[i - 1] * inv[i] % mod;
+	scanf("%d %lld",&n,&p);
+	(p *= inv[10000]) %= mod;
+	for(int i = 0;i <= n;i = -~i)dp[0][i] = g[0][i] = 1;
+	for(int i = 1;i <= n;i = -~i)
+	{
+		for(int k = 0;k < i;k = -~k)
+			(dp[i][0] += p * dp[k][1] % mod * C(i - 1,k) % mod * g[i - 1 - k][0]) %= mod;
+		for(int j = 1;j <= n;j = -~j)
+			for(int k = 0;k < i;k = -~k)
+				(dp[i][j] += (p * dp[k][j + 1] + (998244354 - p) * dp[k][j - 1]) % mod * C(i - 1,k) % mod * g[i - 1 - k][j]) %= mod;
+		for(int j = 0;j <= n;j = -~j)
+			for(int k = 0;k <= i;k = -~k)
+				(g[i][j] += C(i,k) * dp[k][j] % mod * dp[i - k][j]) %= mod;
+	}
+	ans = dp[n][0];
+	for(int i = 1;i <= 2 * n;i += 2)(ans *= inv[i]) %= mod;
+	printf("%lld",ans);
 }
-
-template <typename U, typename T>
-U& operator>>(U& stream, Modular<T>& number) {
-  typename common_type<typename Modular<T>::Type, long long>::type x;
-  stream >> x;
-  number.value = Modular<T>::normalize(x);
-  return stream;
-}
-
-constexpr int md = 998244353;
-using Mint = Modular<std::integral_constant<decay<decltype(md)>::type, md>>;
-
-int main() {
-  ios::sync_with_stdio(false);
-  cin.tie(0);
-  int n;
-  Mint p;
-  cin >> n >> p;
-  p /= 10000;
-  vector<vector<Mint>> C(n + 1, vector<Mint>(n + 1));
-  for (int i = 0; i <= n; i++) {
-    C[i][0] = 1;
-    for (int j = 1; j <= i; j++) {
-      C[i][j] = C[i - 1][j] + C[i - 1][j - 1];
-    }
-  }
-  vector<vector<Mint>> dp(n + 1, vector<Mint>(n + 1));
-  vector<vector<Mint>> aux(n + 1, vector<Mint>(n + 1));
-  for (int b = 0; b <= n; b++) {
-    dp[0][b] = aux[0][b] = 1;
-  }
-  for (int i = 1; i <= n; i++) {
-    for (int b = 0; b <= n - i; b++) {
-      for (int y = 0; y <= i - 1; y++) {
-        dp[i][b] += C[i - 1][y] * aux[i - 1 - y][b] * (dp[y][b + 1] * p + (b == 0 ? 0 : dp[y][b - 1] * (1 - p)));
-      }
-      for (int j = 0; j <= i; j++) {
-        aux[i][b] += dp[j][b] * dp[i - j][b] * C[i][j];
-      }
-    }
-  }
-  auto ans = dp[n][0];
-  for (int i = 1; i <= 2 * n; i += 2) {
-    ans /= i;
-  }
-  cout << ans << '\n';
-  return 0;
-}
+ 	 	 		  		  			 		 			 				 	
