4cb85e02db4ab308f14b8ba2eb74a41b001be2a3
mdarikrayhan <mdarikrayhan@gmail.com>
2024-05-29 03:32:00 +0600

    Add solution for problem `F - Equal Reversal` Link: https://codeforces.com/contest/1698/problem/F Tags: constructive algorithms, graphs, implementation, math, *2800 Ref: https://codeforces.com/contest/1698/submission/263066444

--- codeforces/1698/F.cpp
+++ codeforces/1698/F.cpp
@@ -1,27 +1,84 @@
-#include<bits/stdc++.h>
-#define end return void(puts("NO"));
-using namespace std;const int N=505;
-int a[N],b[N],n,i,j,k,x,y,f,it,T_T;vector<pair<int,int>>ans;
-void D(int l,int r){ans.push_back(make_pair(l,r));reverse(a+l,a+r+1);}
-void go(){
-	for(scanf("%d",&n),i=1;i<=n;++i)scanf("%d",a+i);
-	for(i=1;i<=n;++i)scanf("%d",b+i);
-	if(a[1]!=b[1])end;
-	for(i=2;i<=n;++i)if(a[i]!=b[i]){
-		x=b[i-1],y=b[i];f=0;it=-1;
-		for(j=i;j<n;++j)if(a[j]==y&&a[j+1]==x){f=1;D(i-1,j+1);break;}else if(a[j]==x&&a[j+1]==y)it=j;
-		if(f)continue;
-		if(it==-1)end;
-		for(j=i-1;j<=it;++j)for(k=it+1;k<=n;++k)if(a[j]==a[k]&&!f){f=1;D(j,k);break;}
-		if(!f)end;
-		for(j=i;j<n;++j)if(a[j]==y&&a[j+1]==x){
-			D(i-1,j+1);break;
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+
+const int N = 505;
+
+int n,a[N],b[N];
+vector<pair<int,int> > ans;
+
+void rev(int l,int r){
+	ans.push_back({l,r});
+	reverse(a+l,a+r+1);
+}
+
+int sol(int i){
+	for (int j=i; j<n; j++){
+		if (a[j]==b[i] && a[j+1]==b[i-1]){
+			rev(i-1,j+1);
+			return 1;
 		}
 	}
-	puts("YES");
-	printf("%d\n",ans.size());
-	for(auto&x:ans)printf("%d %d\n",x.first,x.second);
+	for (int p=i; p<n; p++){
+		if (a[p]==b[i-1] && a[p+1]==b[i]){
+			bool f=0;
+			for (int j=i-1; j<=p && !f; j++){
+				for (int k=p+1; k<=n && !f; k++){
+					if (a[j]==a[k]){
+						rev(j,k),f=1;
+					}
+				}
+			}
+			if (!f){
+				return 0;
+			}
+			for (int j=i; j<n; j++){
+				if (a[j]==b[i] && a[j+1]==b[i-1]){
+					rev(i-1,j+1);
+					return 1;
+				}
+			}
+		}
+	}
+	return 0;
 }
+
+void solve(){
+	cin>>n;
+	ans.clear();
+	for (int i=1; i<=n; i++){
+		cin>>a[i];
+	}
+	for (int i=1; i<=n; i++){
+		cin>>b[i];
+	}
+	if (a[1]!=b[1] || a[n]!=b[n]){
+		cout<<"NO\n";
+		return;
+	}
+	for (int i=1; i<=n; i++){
+		if (a[i]!=b[i] && !sol(i)){
+			cout<<"NO\n";
+			return;
+		}
+	}
+	cout<<"YES\n";
+	cout<<ans.size()<<"\n";
+	for (auto u : ans){
+		cout<<u.first<<" "<<u.second<<"\n";
+	}
+}
+
 int main(){
-	for(scanf("%d",&T_T);T_T--;ans.clear())go();
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int t;
+	cin>>t;
+	while (t--){
+		solve();
+	}
+	return 0;
 }
