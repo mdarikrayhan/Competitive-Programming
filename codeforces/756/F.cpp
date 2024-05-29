b772517903ea8e259d37e3c5f53b52771f9fbd26
mdarikrayhan <mdarikrayhan@gmail.com>
2024-05-29 19:16:00 +0600

    Add solution for problem `F - Long number` Link: https://codeforces.com/contest/756/problem/F Tags: expression parsing, math, number theory, *3400 Ref: https://codeforces.com/contest/756/submission/263146495

--- codeforces/756/F.cpp
+++ codeforces/756/F.cpp
@@ -1,110 +1,84 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-const int Maxn = 100005, p = 1e9 + 7;
-string str;
-int top;
-struct num
-{
-	long long val1, val2, len1, len2;
-	num()
-	{
-		val1 = val2 = 0, len1 = len2 = 1;
-	}
-	num(long long v1, long long v2, long long l1, long long l2)
-	{
-		val1 = v1, val2 = v2, len1 = l1, len2 = l2;
-	}
-	bool operator != (const num &tmp) const
-	{
-		return val1 != tmp.val1 || val2 != tmp.val2 || len1 != tmp.len1 || len2 != tmp.len2;
-	}
-	void clear(void)
-	{
-		val1 = val2 = 0, len1 = len2 = 1;
-	}
-} tmp, now, d[Maxn], val[Maxn];
-long long fast_pow(long long x, long long y)
-{
-	long long ans = 1, now = x;
-	while (y)
-	{
-		if (y & 1) ans = ans * now % p;
-		now = now * now % p;
-		y >>= 1;
-	}
-	return ans;
+#define mp make_pair
+typedef pair<int,int> pii;
+typedef long long ll;
+const int mod=1e9+7,N=100105;
+#define x first
+#define y second
+int p[N],q[N],nxt[N];
+char s[N];
+int fpow(int x,int y,int mod) {
+	int res=1;
+	while (y) { if (y&1) res=1ll*res*x%mod; x=1ll*x*x%mod,y>>=1; } 
+	return res;
 }
-num add(num a, num b)
-{
-	num result;
-	result.len1 = a.len1 * b.len1 % p, result.len2 = a.len2 * b.len2 % (p - 1);
-	result.val1 = (a.val1 * b.len1 + b.val1) % p;
-	result.val2 = (a.val2 * b.len2 + b.val2) % (p - 1);
-	return result;
+int inv(int a) { return a<=1?a:(int)(mod-(ll)mod/a*inv(mod%a)%mod); }
+pii operator + (const pii &a,const pii &b) {
+	int x=((ll)a.x*fpow(10,b.y,mod)%mod+b.x)%mod,y=(a.y+b.y)%(mod-1);
+	return mp(x,y);
 }
-num mul(num a, num b)
-{
-	num result;
-	result.len1 = fast_pow(b.len1, a.val2);
-	result.val1 = b.val1 * (b.len1 == 1 ? a.val2 : (fast_pow(b.len1, a.val2) - 1 + p) % p * fast_pow(b.len1 - 1 + p, p - 2) % p);
-	return result;
+pii calc2(int a,int b,int alen,int blen,int n) {
+	int A=p[n],INV=inv(A-1);
+	int B=fpow(A,(mod+blen-alen)%(mod-1),mod);
+	int Ans=1ll*INV*INV%mod*(B-A)%mod;
+	Ans=(Ans+1ll*B*a%mod*INV)%mod;
+	Ans=((Ans-1ll*b*INV)%mod+mod)%mod;
+	B=1ll*(blen-alen+1)*n%(mod-1)+mod-1;
+	return mp(Ans,B%(mod-1));
 }
-num cal(num a, num b)
-{
-	long long tmp_val1 = ((fast_pow(a.len1, b.val2 - a.val2 + 2 + p - 1) - 1) * fast_pow(a.len1 - 1, p - 2) - 1 - (b.val1 - a.val1 + 1) + p) % p;
-	long long tmp_val2 = (a.val1 - 1) * (fast_pow(a.len1, b.val2 - a.val2 + 1 + p - 1) - 1) % p, sum;
-	if (a.len1 == 1)
-	{
-		tmp_val1 = (b.val1 - a.val1 + 2 + p) * (b.val1 - a.val1 + 1 + p) / 2 % p;
-		tmp_val2 = a.val1 * (b.val1 - a.val1 + 1 + p) % p;
-		sum = (tmp_val1 + tmp_val2) % p;
+pii calc(int S,int E) {
+	for (int i=S; i<E; i++) {
+		if (s[i]=='(') {i=nxt[i];continue;}
+		if (s[i]=='+') return calc(S,i)+calc(i+1,E);
 	}
-	else sum = (tmp_val1 + tmp_val2) * fast_pow(a.len1 - 1, p - 2) % p;
-	return num(sum, 0, fast_pow(a.len1, b.val2 - a.val2 + p), 0);
-}
-num Copy(num a, num b)
-{
-	num result;
-	if (a.len1 == b.len1)
-		return cal(a, b);
-	result = add(result, cal(a, num(a.len1 - 1, (a.len2 - 1 + p - 1) % (p - 1), a.len1, a.len2)));
-	num t = (num){a.len1, a.len2, a.len1 * 10 % p, a.len2 * 10 % (p - 1)};
-	while (t.len1 != b.len1)
-	{
-		result = add(result, cal(t, num((t.len1 - 1) % p, (t.len2 - 1 + p - 1) % (p - 1), t.len1, t.len2)));
-		(t.len1 *= 10) %= p, (t.val1 *= 10) %= p;
-		(t.len2 *= 10) %= (p - 1), (t.val2 *= 10) %= (p - 1);
-	}
-	result = add(result, cal(t, b));
-	return result;
-}
-int main()
-{
-	cin >> str;
-	str = '(' + str + ')';
-	int siz = str.size();
-	for (int i = 0; i < siz; i++)
-	{
-		if (tmp != (num){0, 0, 1, 1} && !isdigit(str[i]))
-			now = Copy(tmp, now), tmp.clear();
-		if (str[i] == '(')
-		{
-			top++;
-			d[top] = now, now.clear();
+	if (s[E-1]==')') {
+		int mid=0,val=0;
+		for (int i=S; i<E; i++) {
+			if (s[i]=='(') {
+				pii a=calc(i+1,E-1); int b=fpow(10,a.y,mod),ansx;
+				if (b==1) ansx=1ll*a.x*val%mod;
+				else {
+					int c=1ll*(fpow(b,mid,mod)-1)*inv(b-1)%mod;
+					ansx=1ll*a.x*c%mod;
+				}
+				int ansy=1ll*mid*a.y%(mod-1);
+				return pii((mod+ansx)%mod,ansy);
+			}
+			mid=(mid*10ll+s[i]-'0')%(mod-1);
+			val=(val*10ll+s[i]-'0')%mod;
 		}
-		if (str[i] == ')')
-		{
-			val[top] = add(val[top], now);
-			if (d[top] != (num){0, 0, 1, 1}) now = mul(d[top], val[top]);
-			else now = val[top];
-			val[top].clear(), d[top].clear();
-			top--;
-		}
-		if (isdigit(str[i])) now = add(now, (num){str[i] - '0', str[i] - '0', 10, 10});
-		if (str[i] == '+') val[top] = add(val[top], now), now.clear();
-		if (str[i] == '-') tmp = now, now.clear();
 	}
-	printf("%lld", now.val1);
+	int Ans=0,Anslen=0;
+	for (int i=S; i<E; i++) {
+		if (s[i]=='-') {
+			int L=i-S,LL=E-i-1,b=0,blen=0;
+			for (i++; i<E; i++) {
+				b=(b*10ll+s[i]-'0')%mod;
+				blen=(blen*10ll+s[i]-'0')%(mod-1);
+			}
+			if (L==LL) return calc2(Ans,b,Anslen,blen,L);
+			pii res=calc2(Ans,(mod-1+p[L])%mod,Anslen,(mod-2+q[L])%(mod-1),L);
+			for (i=L+1; i<LL; i++)
+				res=res+calc2(p[i-1],(p[i]+mod-1)%mod,q[i-1],(q[i]+mod-2)%(mod-1),i);
+			res=res+calc2(p[LL-1],b,q[LL-1],blen,LL);
+			return res;
+		}
+		Ans=(Ans*10ll+s[i]-'0')%mod;
+		Anslen=(Anslen*10ll+s[i]-'0')%(mod-1);
+	}
+	return mp(Ans,E-S);
+}
+int main() {
+	scanf("%s",s);
+	int n=strlen(s);
+	for (int i=p[0]=1; i<N; i++) p[i]=p[i-1]*10ll%mod;
+	for (int i=q[0]=1; i<N; i++) q[i]=q[i-1]*10ll%(mod-1);
+	stack<int> sta;
+	for (int i=0; i<n; i++) {
+		if (s[i]=='(') sta.push(i);
+		if (s[i]==')') nxt[sta.top()]=i,sta.pop();
+	}
+	printf("%d\n",calc(0,n).x);
 	return 0;
 }
