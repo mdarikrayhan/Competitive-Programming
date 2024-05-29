15d7a72d956b2370fce7a076317fbd4fc491622b
mdarikrayhan <mdarikrayhan@gmail.com>
2024-05-29 14:22:00 +0600

    Add solution for problem `E - Fibonacci Number` Link: https://codeforces.com/contest/193/problem/E Tags: brute force, math, matrices, *2900 Ref: https://codeforces.com/contest/193/submission/263110493

--- codeforces/193/E.cpp
+++ codeforces/193/E.cpp
@@ -1,27 +1,104 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+#define int __int128
 using namespace std;
-#define ll long long
-#define rep(i,j,k)for(ll i=(j);i<=(k);i++)
-#define pii pair<ll,ll>
-#define fi first
-#define se second
-ll n,l,md;
-vector<ll> z[20];
-ll mul(ll a,ll b){ll d=((__int128)a*b)%md;
-	if(d<0)d+=md;return d;
+
+#ifndef ONLINE_JUDGE
+#include <debug-template.hpp>
+#define debug(...) cerr << "[ " << #__VA_ARGS__ << " ]: ", debug_out(__VA_ARGS__)
+#else
+#define debug(...) 42
+#endif
+
+const int mod = 1e13;
+
+struct matrix {
+	int n, m;
+	vector <vector <int>> rem;
+	matrix (int nn, int mm) {
+		n = nn, m = mm;
+		rem.assign(n, vector <int> (m, 0));
+	}
+};
+
+matrix operator * (matrix x, matrix y) {
+	matrix c(x.n, y.m);
+	for (int i = 0; i < x.n; i ++) for (int j = 0; j < y.m; j ++) {
+		int now = 0;
+		for (int k = 0; k < x.m; k ++) now += x.rem[i][k] * y.rem[k][j], now %= mod;
+		c.rem[i][j] = now;
+	}
+	return c;
 }
-pii sol(ll x){pii t;
-	if(x==0)return {0,1};
-	if(x&1){t=sol(x-1);return {t.se,(t.fi+t.se)%md};}
-	t=sol(x/2);return {mul(t.fi,2*t.se-t.fi),(mul(t.fi,t.fi)+mul(t.se,t.se))%md};
+
+matrix con (matrix a, int k) {
+	matrix res (a.n, a.m);
+	for (int i = 0; i < a.n; i ++) res.rem[i][i] = 1;
+	while (k) {
+		if (k & 1) res = res * a;
+		a = a * a;
+		k >>= 1;
+	}
+	return res;
 }
-int main(){scanf("%lld",&n);
-	l=60;md=10;
-	rep(i,0,l-1)if(sol(i).fi==n%md)z[0].push_back(i);
-	rep(i,0,11){md*=10;
-		for(ll j:z[i])rep(k,0,9)
-			if(sol(j+k*l).fi==n%md)z[i+1].push_back(j+k*l);l*=10;
-	}sort(z[12].begin(),z[12].end());
-	if(!z[12].size())puts("-1");
-	else printf("%lld\n",z[12][0]);
+
+int f (int n) {
+	matrix phi(2, 2);
+	phi.rem = {{1, 1}, {1, 0}};
+	return con(phi, n).rem[1][0];
+}
+
+int lcm (int x, int y) {
+	return x * y / __gcd(x, y);
+}
+
+int con(int n, int x) {
+	int ans = 1;
+	while (x) {
+		if (x & 1) ans *= n;
+		n *= n;
+		x >>= 1;
+	}
+	return ans;
+}
+
+void pf (int x) {
+	long long y = x;
+	cout << y << "\n";
+}
+
+void in (__int128 &x) {
+	long long y; cin >> y;
+	x = y;
+}
+
+signed main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0), cout.tie(0);
+	
+	int n; in(n);
+	int t, len = 10, tt = 1;
+	vector <int> rem;
+	for (int i = 0; i < 60; i ++) if (f(i) % len == n % len) rem.push_back(i);
+	
+	tt = 60;
+	for (int now = 2; now <= 13; now ++) {
+		len *= 10;
+		t = lcm(con(2, now - 1) * 3, con(5, now - 1) * 20);
+		
+		vector <int> use;
+		for (auto pos : rem) {
+			for (int i = pos; i < pos + t; i += tt) {
+				if (f(i) % len == n % len) use.push_back(i);
+			}
+		}
+		rem.resize(use.size());
+		for (int i = 0; i < rem.size(); i ++) rem[i] = use[i];
+		tt = t;
+	}
+	
+	sort(rem.begin(), rem.end());
+	if (rem.size()) pf (rem[0]);
+	else cout << "-1\n";
+	
+	return 0;
 }
