7ccaa42c246eeadcd62e63b4bf48582d44a39b1a
mdarikrayhan <mdarikrayhan@gmail.com>
2024-05-29 17:42:00 +0600

    Add solution for problem `F - Pairwise Sums` Link: https://codeforces.com/contest/470/problem/F Tags: *special problem, *2300 Ref: https://codeforces.com/contest/470/submission/263134633

---
+++ codeforces/470/F.f
@@ -0,0 +1,18 @@
+[0[^ $$ 47> \58\> &]['0- \ 10*+]#%] r:
+
+r;! n:   {read the number of elements in array}
+
+0i:
+[n;i;>]
+[r;! i;1+i:]
+#        {read the elements of the array}
+
+0i:
+[n;i;>]
+[$ n;O + . " " n;1-O i;1+i:]
+#
+
+0i:
+[n;2*i;>]
+[% i;1+i:]
+#
