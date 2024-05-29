5611f3326d1fa70716c12540b362235c14000495
mdarikrayhan <mdarikrayhan@gmail.com>
2024-05-29 08:07:00 +0600

    Add solution for problem `C - Heidi and Library (hard)` Link: https://codeforces.com/contest/802/problem/C Tags: flows, *2600 Ref: https://codeforces.com/contest/802/submission/263077042

---
+++ codeforces/802/C.cpp
@@ -0,0 +1,229 @@
+/*******************************************************************************************************************************
+ * 
+ *   @Author: ManInM00N
+ *   @Filename: ttre.cpp
+ *   @Date: 2024-05-07 21:13:31
+ * 
+                                                            &   `
+                                                              !h
+                                                               IL
+                                                               `L;
+                                                                |@L                             :.
+                                                                 TLL             |L                `'
+                                                                 '|L*          .*||L          `     |
+                                                                b |L*          ||@|h        .      |`
+                                               .,;h@@@@phn,      Tp&L*      .  |*|&L,      |   ** .L
+                                   .;nwmt@@@&P**|L|LLLLLLL**LIKKKbINL*       ; |L|L L     |***  **|`
+                                 ,|*` ,vpKP&L.,,``****|#pw+,, |KEKKKNL       |&pL`|*|    m  '****|L
+                              '-*`  aKP0PH T@L '***   IEEEbp,`'*KEEEKKp     |LIEL*|L*+ ,|`  | '**|
+                          ,|*`    ,KP*IP*L 'EL*+  `  :IEEEEEEPh,'IEEK#Kp,***LLIEL*|@|E;|L   * **|L  ' ' **|;.
+                        :'       |P` |&*'L  !E `*    !KEEEEEEEH|H@IKEK#KNL*||&IKLL||IEHL*  *,|*`|*    '**|L|*
+                      '`      ' |`   |L  |   `Y, `. '  'KEEEEEEHLL|||KKKBN@|@&#EL|LL|KP*``'*`  |* *  ||@t*|P
+                    '       '  '     !`  'L    `:  ':  ..`KKEEEE@|L|||LIKKKKKKK&L|L|&L    '  '|L+,  ,*'`;P*
+                  '         '        : :  *L     '+,'IKKbKppKKEEE@|L|||||IKKKKE&LL**|L` |L  :L*.,,''`,|**,`
+                 *                   `     *h,    '|L|KKEEEEE0KKEEE@L|||||LIKBEIEL  |L  *||,`  |L` :*``*|`
+                             |        * '   '0@+    '|LL5KKKKEEEKKKEHL||&P&LIKbE|&m*IN,;,|@W@*;*      ,|n          ,,;;n
+               ;            :&      ' '  : ';'IE@L,   '||LLLL*5KKKKKb@L&&@LIK#KKKp#NKPP|KEKKKbL*   `;mPL+:;'```**||||LL,
+          .|;m&*            ||h      ',|  | `|L||&@L|+, `**|L**`'  ``TKKN#p#K#KKKKKKE``L*KL,|KL+ '*tPL|L,,,p@#KbLL#EKM**
+       ,|Lw***`             *'LL ** , `|E, |, `|L&@L|LL|,  ``*|+,   `  IKKKKKKKNp##KbppppmhKP*    '**  '|`'**``*||||hn
+   ''````                  |L `|EL||L|,'|EL *|  `***LL`*|c,     ``**+,,  *KKK**IKKKK#BKKKpp            , *L    |@@&W@L&h
+            ,L             &L,  |LHLLLLL*IEL '|L;    `**:||L**|w++++++ *** `   !KPKKKKKKKKN++||*''*'|||`|L`|,  *|&E@&&E&
+          |L|             |K@L| '||||||L|L*&h  `*L        `'**                  `IEKKpEPKKKKKpp, |* ,||L**  *L *||||&KE&
+          ||`    :  :*    IKELLL,'||LLLL**|+'L,  '|               :              `IEKKPKp|mL||L`|* |*  '*+   `|||&&PPP*`
+         ||L     '**||L ||KE@LL|&L``*``` `   `` '||&,             '|               IEKK@KE@LL|@&L,|*      *YH'````
+         |&L  |L:*|*||L*|IEE&EL`*&   `           '&@L|,            |H              'IKKK@IKp@||@@HL**.|H|+  `|,,;,
+         |&|L|&@|*L|||L||IEL&PHL |b    '     '  *,|IE***|,  ', `   '&              ' IKKKbEKKbKb@&@@@L|&L|&L||PKK#p@np
+         |EL||E|&@LL|&LLLIHL|L*IEm|EL       *,'*|L||L*   `*|,`|,':  'L              *'IKKKKE&ELL*L**TPWH@&@@|H    `*KKKK
+         !@L||H|||LL||LL`|L|E  ,+`*TEH,      `hm||&@|@, ;|&####KNppp,*         :L|  :*`IKKKK@TL`t.':    ```*``         ``
+          |L|&@L||@`'*** |L|L;**`    ***+   ,, |LL&|LTH@#KKKKKMKKK`KNm~        |L|L  ** IKPKNL`.   `'*u.
+         ||||IE|LL&L ` ' '|L*`  ,++a,,,, '|,||@L||L`` !P`:p#ESN#bEh:KL         |*|L  ||L|IEPL`  '
+         {H&&KP|HL|L  |L` |*  :@##KKKKKKNp  '*|||&@L`    'KKP5KK|5h |*   `    |**|L .{E@|&Kh    * +,`
+        pH|IP '**|IP  'L,  `;t#KMPKP5KE`IE`    ``*H5bL,   '|L, ,m*  *       :+*|*|L  |KEL|IE .,    ||
+      ,KP&|@    |&IEb  |&L :L#B` :KKKKKEKP*           `      ```   '  |`   .|* * |@  'IEL|&&h,LL   .'*|n,
+    :*`  {PP  LL{KE@Kp 'LIp'| `N  *LL***`|                         .|`  ` ;#P |  |E   IE@&KEEm|`*  |m
+         !HL ||LIKPEKEL |`Tb |,     '*:*`                       ;+|P   ` |KPL:*  IEL++IE0KEEEP|  : |&
+          &  |*|I`!PKPIh|b `*L'u                                 .*     aPP*  |L'5E@|&&EIEEEKb'L ',|P
+          *L|` *| !L|LIKLIb;,,`'|u                              ,*  ,,aKKE* ,&KELIP@|LK|K5KEM     *'P
+                |, *`|,|*|Kb`*KNp,`;                  ,n      .*`,p@@#P&#P,pKEEKEKbE@KLP #KP      |p
+                 *:* |&#h TKbL'KKL'Kp           ***  ``      :.**|#KKKEKEpKKKEKEKKKEEP*;P`        |`
+                   | IK`|L'IEKp'TKpKKKp,                   ''` ,#KKPIKKKP`  IEKKKKKbKp
+                ,pKKL`;#KK |*|KKpIKKKKKKbp,                 .a#KKKKb#KK     !KN  TKKKEbn
+              ;pHP` LIKM`   |L  TK#KEb |KKEIH:.          ,pKKEKKKKEKM`     ; IKN |KKKb@|bn.
+         .;||HP`  /KP` :*,|P*    |KKP`IKKKKM    '<:.,,apKE0KKEEKKKKN       IN|IKKp `KKE*&@|@p,
+  ,::**|L@pP`  ,p@m*` ,|L*``    L#KK,#KIEKP         &KKKKKKKKKKKKKKKn  L  |IKbL`KKNp`KKN TELTKKhn
+'` ,pKKKKP,,p#KEL`   |H*       |IKbbP &KKP          #KKKKKKKKKKKKKKEH  L  |IEKKp *KKKpIKN `Kp
+这是Philia,她真的很可爱！！！
+********************************************************************************************************************************/
+
+#pragma GCC optimize(2)
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const int maxn = 2e6 + 7;
+const int N = 2e5+7;
+const ll INF = 1e18;
+ll n, m, k, x, y, z;
+#define ls p << 1
+#define rs p << 1 | 1
+#define Pa pair<ll, ll>
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+#define rep(i,n) for(int i = 0; i < (int)n; i++)
+#define FOR(n) for(int i = 0; i < (int)n; i++)
+#define repi(i,a,b) for(int i = (int)a; i < (int)b; i++)
+ll mx[4]={0,0,1,-1},my[4] = {1,-1,0,0};
+
+struct node
+{
+    ll to, next, w,flow;
+    node() {}
+    node(ll w, ll to) : to(to), next(0), w(w) {}
+    friend bool operator<(node a, node b)
+    {
+        return a.w > b.w;
+    }
+};
+ll nxt, rnxt;
+node e[maxn * 2];
+ll h[N];
+bool vis[N];
+void add(ll u,ll v,ll w,ll flow){
+    e[++nxt].next=h[u];
+    e[nxt].w=w;
+    e[nxt].to=v;
+    e[nxt].flow=flow;
+    h[u] = nxt;
+}
+void init(){
+    nxt=1;
+    for (int i = 0; i <= n*2+7; i++)//网络流使用2*n+7
+    {
+        h[i] = -1;
+    }
+}
+ll S, T; // 源点(起点) ,汇点(终点)
+ll mf[N];  // 记录单边最大流量
+ll cur[N]; //当前弧优化
+ll  d[N];
+ll maxflow, mincost;
+bool spfa(){
+    // memset(d,0x3f,sizeof(d));
+    repi(i,0,2*n+8){
+        d[i] = INF;
+    }
+    queue<ll> q;
+    q.push(S);
+    vis[S] = 1;
+    d[S] = 0;
+    while (!q.empty())
+    {
+        ll tt =q.front();
+        q.pop();
+        vis[tt] = 0;
+        for(int i = h[tt];~i;i=e[i].next){
+            ll v= e[i].to;
+            ll w = e[i].w;
+            ll flow = e[i].flow;
+            if (flow&&d[v]>d[tt]+w){//有流量且是最短路;
+                d[v] = d[tt]+w;
+                if (!vis[v]){
+                    vis[v] =1;
+                    q.push(v);
+                }
+            }
+        }
+    }
+    return d[T]!=INF;//检验是否有剩余流量
+}
+ll dfs(ll now, ll mf)
+{
+    if (now == T)
+        return mf;
+    ll sum = 0;
+    vis[now] = 1;
+    for (int i = cur[now]; ~i; i = e[i].next)
+    {
+        ll v = e[i].to, w = e[i].w;
+        ll f = e[i].flow;
+        cur[now] = i; // 当前弧优化
+        if (!vis[v] && f && d[v] == d[now] + w) // 按层推流，费用流是按距离推流
+        {                                       // 相邻节点并且还有流量
+            ll flow = dfs(v, min(mf, f));       // 求得从此路后可能的flow
+            if (flow == 0)
+                continue;
+            e[i].flow -= flow;
+            e[i ^ 1].flow += flow; // 更新残留网
+            sum += flow;           // 累加now节点此时可流出的流量
+            mf -= flow;            // 相对的now节点剩下的流量减少
+            mincost += flow * w;
+            if (mf == 0)
+                break; // 余量优化，没东西流出就跳出循环
+        }
+    }
+    vis[now] = 0;
+    return sum;     // 返回总流量
+}
+void MCMF()
+{
+    maxflow = 0;
+    mincost = 0 ;
+    memset(vis, 0, sizeof(vis));
+    while (spfa())
+    {
+        memcpy(cur, h, sizeof(h)); // cur弧初始化
+        maxflow+=dfs(S,INF); 
+   }
+}
+
+
+void solve()
+{
+    string ss;
+    cin >>n >>k;
+    init();
+    S =0 ,T =2*n+7;
+    vector<ll> a(n+1);
+    vector<ll> pre(n+1);
+    vector<ll> b(n+1);
+    repi(i,1,n+1){
+        cin >>a[i];
+        
+        add(i,i+n,0,1);
+        add(i+n,i,0,0);
+        add(i+n,T,0,1);
+        add(T,i+n,0,0);
+    }   
+    repi(i,1,n+1){
+        cin >>b[i];
+    }
+    repi(i,1,1+n){
+        if (pre[a[i]]!=0){
+            add(i-1,pre[a[i]]+n,-b[a[i]],1);
+            add(pre[a[i]]+n,i-1,b[a[i]],0);
+        }
+        pre[a[i]] = i;
+        
+        add(S,i,b[a[i]],1);
+        add(i,S,-b[a[i]],0);
+        if (i>1){
+            add(i-1, i, 0, k - 1);
+            add(i,i-1,0,0);
+        }
+    }
+    MCMF();
+    cout << mincost<<'\n';
+
+    
+    
+    // cout << endl;
+}
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+    ll t = 1;
+    // cin >>t;
+    while (t--)
+        solve();
+    return 0;
+}
