81367e02d7b32ccda4a37358bc886418883d5a0e
mdarikrayhan <mdarikrayhan@gmail.com>
2024-05-29 07:25:00 +0600

    Add solution for problem `H - L BREAK into program` Link: https://codeforces.com/contest/1505/problem/H Tags: *special problem, *2500 Ref: https://codeforces.com/contest/1505/submission/263075197

---
+++ codeforces/1505/H.cpp
@@ -0,0 +1,16 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+int main()
+{
+    std::cout<<"contest/171";
+
+    return 0;
+}
