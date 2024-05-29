bb0ceaf4fb7b5d7e9c1eb0b87ad90f35f554744c
mdarikrayhan <mdarikrayhan@gmail.com>
2024-05-29 12:57:00 +0600

    Add solution for problem `G - Omkar and Time Travel` Link: https://codeforces.com/contest/1583/problem/G Tags: data structures, math, *3000 Ref: https://codeforces.com/contest/1583/submission/263099163

---
+++ codeforces/1583/G.cpp
@@ -0,0 +1,310 @@
+//Never compromise. Not even in the face of Armageddon.
+
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+
+//pragmas
+#pragma GCC optimize("03")
+#pragma GCC target("avx,avx2,fma")
+//#pragma GCC optimization("01, 02, 03, 0fast, unroll-loops")              //Retard mode
+//#pragma GCC optimize("Ofast,unroll-loops,omit-frame-pointer,inline")     //Optimization flags
+//#pragma GCC option("arch=native,tune=native,no-zero-upper")              //Enable AVX
+
+//types
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define ll long long
+#define TYPE long long
+#define ppair pair<TYPE, TYPE>
+#define vecvec(a, i, j, x) vector<vector<TYPE>> a (i, vector<TYPE> (j, x))
+#define vecvecvec(a, i, j, k, x) vector<vector<vector<TYPE>>> a (i + 1, vector<vector<TYPE>>(j + 1, vector<TYPE>(k + 1, x)))
+
+using namespace std;
+using namespace __gnu_pbds;
+
+//random stuff
+#define all(a) a.begin(),a.end()
+#define endl "\n"
+#define pb push_back
+#define mp make_pair
+#define sz(x) (long long int)x.size()
+#define F first
+#define S second
+#define sp " " 
+const ll inf = 0x3f3f3f3f, INF = 1e18;
+const int MOD = 1e9 + 7;
+const ll BIG_MOD = 489133282872437279; //use to decrease hash collision probability
+
+typedef tree<long long int, null_type, less<long long int>, rb_tree_tag, tree_order_statistics_node_update> indexed_set;             //for multiset, use less_equal and erase x in multiset a -> a.erase(a.find_by_order(a.order_of_key(x)))
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+#define safe_map unordered_map<long long, int, custom_hash>
+#define hashmap gp_hash_table<int, int, custom_hash>
+
+
+//debug
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) {cerr << "[" << #x << "] = ["; _print(x);}
+#define reach cerr<<"reached"<<endl
+#else
+#define debug(x...)
+#define reach 
+#endif
+
+
+/*---------------------------------------------------------------------------------------------------------------------------*/
+ll gcd(ll a, ll b) {if (b > a) {return gcd(b, a);} if (b == 0) {return a;} return gcd(b, a % b);}
+ll expo(ll a, ll b, ll mod) {ll res = 1; while (b > 0) {if (b & 1)res = (res * a) % mod; a = (a * a) % mod; b = b >> 1;} return res;}
+void extendgcd(ll a, ll b, ll*v) {if (b == 0) {v[0] = 1; v[1] = 0; v[2] = a; return ;} extendgcd(b, a % b, v); ll x = v[1]; v[1] = v[0] - v[1] * (a / b); v[0] = x; return;} //pass an arry of size1 3
+ll mminv(ll a, ll b) {ll arr[3]; extendgcd(a, b, arr); return arr[0];} //for non prime b
+ll mminvprime(ll a, ll b) {return expo(a, b - 2, b);}
+bool revsort(ll a, ll b) {return a > b;}
+void swap(int &x, int &y) {int temp = x; x = y; y = temp;}
+ll combination(ll n, ll r, ll m, ll *fact, ll *ifact) {ll val1 = fact[n]; ll val2 = ifact[n - r]; ll val3 = ifact[r]; return (((val1 * val2) % m) * val3) % m;}
+void google(int t) {cout << "Case #" << t << ": ";}
+vector<ll> sieve(int n) {int*arr = new int[n + 1](); vector<ll> vect; for (ll i = 2; i <= n; i++)if (arr[i] == 0) {vect.push_back(i); for (ll j = 2 * i; j <= n; j += i)arr[j] = 1;} return vect;}
+ll mod_add(ll a, ll b, ll m = MOD) {a = a % m; b = b % m; return (((a + b) % m) + m) % m;}
+ll mod_mul(ll a, ll b, ll m = MOD) {a = a % m; b = b % m; return (((a * b) % m) + m) % m;}
+ll mod_sub(ll a, ll b, ll m = MOD) {a = a % m; b = b % m; return (((a - b) % m) + m) % m;}
+ll mod_div(ll a, ll b, ll m = MOD) {a = a % m; b = b % m; return (mod_mul(a, mminvprime(b, m), m) + m) % m;}  //only for prime m
+ll phin(ll n) {ll number = n; if (n % 2 == 0) {number /= 2; while (n % 2 == 0) n /= 2;} for (ll i = 3; i <= sqrt(n); i += 2) {if (n % i == 0) {while (n % i == 0)n /= i; number = (number / i * (i - 1));}} if (n > 1)number = (number / n * (n - 1)) ; return number;} //O(sqrt(N))
+void precision(int a) {cout << setprecision(a) << fixed;}
+ll ceil_div(ll x, ll y){return (x + y - 1) / y;}
+unsigned long long power(unsigned long long x,ll y, ll p){unsigned long long res = 1;x = x % p; while (y > 0){if (y & 1)res = (res * x) % p;y = y >> 1;x = (x * x) % p;}return res;}
+unsigned long long modInverse(unsigned long long n,int p){return power(n, p - 2, p);}
+ll nCr(ll n,ll r, ll p){if (n < r)return 0;if (r == 0)return 1;unsigned long long fac[n + 1];fac[0] = 1;for (int i = 1; i <= n; i++)fac[i] = (fac[i - 1] * i) % p;return (fac[n] * modInverse(fac[r], p) % p* modInverse(fac[n - r], p) % p)% p;}
+ll accumulate(const vector<long long int> &nums){ll sum = 0; for(auto x : nums) sum += x; return sum;}
+ll tmax(ll a, ll b, ll c = 0, ll d = -INF, ll e = -INF, ll f = -INF){return max(a, max(b, max(c, max(d, max(e, f)))));}
+int log2_floor(unsigned long long i) {return i ? __builtin_clzll(1) - __builtin_clzll(i) : -1;}
+string bin(ll n){return bitset<32>(n).to_string();}
+/*--------------------------------------------------------------------------------------------------------------------------*/
+
+//#define int int64_t
+
+// const int MOD = 1e9+7;
+struct Mint {
+    int val;
+ 
+    Mint(long long v = 0) {
+        if (v < 0)
+            v = v % MOD + MOD;
+        if (v >= MOD)
+            v %= MOD;
+        val = v;
+    }
+ 
+    static int mod_inv(int a, int m = MOD) {
+        int g = m, r = a, x = 0, y = 1;
+        while (r != 0) {
+            int q = g / r;
+            g %= r; swap(g, r);
+            x -= q * y; swap(x, y);
+        } 
+        return x < 0 ? x + m : x;
+    } 
+    explicit operator int() const {
+        return val;
+    }
+    Mint& operator+=(const Mint &other) {
+        val += other.val;
+        if (val >= MOD) val -= MOD;
+        return *this;
+    }
+    Mint& operator-=(const Mint &other) {
+        val -= other.val;
+        if (val < 0) val += MOD;
+        return *this;
+    }
+    static unsigned fast_mod(uint64_t x, unsigned m = MOD) {
+           #if !defined(_WIN32) || defined(_WIN64)
+                return x % m;
+           #endif
+           unsigned x_high = x >> 32, x_low = (unsigned) x;
+           unsigned quot, rem;
+           asm("divl %4\n"
+            : "=a" (quot), "=d" (rem)
+            : "d" (x_high), "a" (x_low), "r" (m));
+           return rem;
+    }
+    Mint& operator*=(const Mint &other) {
+        val = fast_mod((uint64_t) val * other.val);
+        return *this;
+    }
+    Mint& operator/=(const Mint &other) {
+        return *this *= other.inv();
+    }
+    friend Mint operator+(const Mint &a, const Mint &b) { return Mint(a) += b; }
+    friend Mint operator-(const Mint &a, const Mint &b) { return Mint(a) -= b; }
+    friend Mint operator*(const Mint &a, const Mint &b) { return Mint(a) *= b; }
+    friend Mint operator/(const Mint &a, const Mint &b) { return Mint(a) /= b; }
+    Mint& operator++() {
+        val = val == MOD - 1 ? 0 : val + 1;
+        return *this;
+    }
+    Mint& operator--() {
+        val = val == 0 ? MOD - 1 : val - 1;
+        return *this;
+    }
+    Mint operator++(int32_t) { Mint before = *this; ++*this; return before; }
+    Mint operator--(int32_t) { Mint before = *this; --*this; return before; }
+    Mint operator-() const {
+        return val == 0 ? 0 : MOD - val;
+    }
+    bool operator==(const Mint &other) const { return val == other.val; }
+    bool operator!=(const Mint &other) const { return val != other.val; }
+    Mint inv() const {
+        return mod_inv(val);
+    }
+    Mint power(long long p) const {
+        assert(p >= 0);
+        Mint a = *this, result = 1;
+        while (p > 0) {
+            if (p & 1)
+                result *= a;
+ 
+            a *= a;
+            p >>= 1;
+        }
+        return result;
+    }
+    friend ostream& operator << (ostream &stream, const Mint &m) {
+        return stream << m.val;
+    }
+    friend istream& operator >> (istream &stream, Mint &m) {
+        return stream>>m.val;   
+    }
+};
+
+const int MAXN = 2e5+5;
+
+struct FenwickTreeOneBasedIndexing 
+{
+    vector<Mint> bit;
+    int n;
+
+    FenwickTreeOneBasedIndexing(int n) 
+    {
+        this->n = n + 1;
+        bit.assign(n + 1, 0);
+    }
+
+    // mode 1
+    
+    Mint sum(int idx) 
+    {
+        Mint ret = 0;
+        for (++idx; idx > 0; idx -= idx & -idx)
+            ret += bit[idx];
+        return ret;
+    }
+
+    Mint sum(int l, int r) 
+    {
+        if(l > r)   return 0;
+        return sum(r) - sum(l - 1);
+    }
+
+    void add(int idx, Mint delta) 
+    {
+        for (++idx; idx < n; idx += idx & -idx) bit[idx] += delta;
+    }
+};
+
+
+int n, t;
+set<int> s;
+vector<int> a(MAXN), b(MAXN), id(2*MAXN);
+vector<Mint> dp(2*MAXN, 0);
+
+//code starts
+int32_t main()
+{
+    fastio();
+    
+    cin >> n;
+    for(int i = 1; i <= n; i ++)    cin >> a[i] >> b[i], id[a[i]] = -i, id[b[i]] = i;
+    id[2*n + 1] = n + 1;
+
+    cin >> t;
+    for(int i = 1, x; i <= t; i ++) cin >> x, s.insert(x);
+
+    FenwickTreeOneBasedIndexing rq(2*MAXN);
+
+    set<int> behind = {2*n + 1};
+    
+    for(int i = 1; i <= 2*n; i ++)
+    {
+        int x = id[i];
+        if(x > 0)
+        {
+            int last = *behind.upper_bound(a[x]);
+
+            dp[b[x]] = 1 + rq.sum(last, 2*n);
+            rq.add(a[x], dp[b[x]]);
+            
+            behind.insert(a[x]);
+        }
+    }
+
+    int last = 0;
+    for(auto task : s)  last = max(last, b[task]);
+    int first = a[id[last]];
+    
+    Mint ans = 0;
+    for(int i = 1; i <= last; i ++) ans += dp[i];
+
+    while(first < last)
+    {
+        if(id[last] > 0 and a[id[last]] >= first)
+        {
+            if(s.count(id[last]))
+                first = a[id[last]];
+            else
+                ans -= dp[last];
+        }
+        -- last;
+    }
+
+    cout << ans << endl;
+}
+
+
+
+
+
+// There is an idea of a Patrick Bateman. Some kind of abstraction.
+// But there is no real me. Only an entity. Something illusory. 
+// And though I can hide my cold gaze, and you can shake my hand and
+// feel flesh gripping yours, and maybe you can even sense our lifestyles
+// are probably comparable, I simply am not there.
