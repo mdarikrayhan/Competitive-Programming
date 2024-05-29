cd0f135b90eeef516537f51bddef1a96467b3f6f
mdarikrayhan <mdarikrayhan@gmail.com>
2024-05-29 14:05:00 +0600

    Add solution for problem `J - Hero to Zero` Link: https://codeforces.com/contest/1765/problem/J Tags: graph matchings, math, *2900 Ref: https://codeforces.com/contest/1765/submission/263108341

--- codeforces/1765/J.cpp
+++ codeforces/1765/J.cpp
@@ -1,46 +1,26 @@
-/**
- *    author:  gary
- *    created: 27.11.2022 22:57:14
-**/
+// LUOGU_RID: 158363068
 #include<bits/stdc++.h>
-#define rb(a,b,c) for(int a=(b);a<=(c);++a)
-#define rl(a,b,c) for(int a=(b);a>=(c);--a)
-#define rep(a,b) for(int a=0;a<(b);++a)
-#define LL long long
-#define II(a,b) make_pair(a,b)
-#define SRAND mt19937 rng(chrono::steady_clock::now().time_since_epoch().count())
-#define random(a) rng()%a
-#define ALL(a) a.begin(),a.end()
-#define check_min(a,b) a=min(a,b)
-#define check_max(a,b) a=max(a,b)
+#define F first
+#define S second
 using namespace std;
-const int INF=0x3f3f3f3f;
-typedef pair<int,int> mp;
-int n;
-int a[200000+20],b[200000+20];
+typedef long long LL;
+typedef pair<LL,LL> pr;
+inline LL rd(){
+	LL x=0,y=1;char c=getchar();
+	for(;c<'0'||c>'9';c=getchar())if(c=='-')y=-1;
+	for(;c>='0'&&c<='9';c=getchar())x=(x<<1)+(x<<3)+(c^48);
+	return x*y;
+}
+const LL N=200005;
+LL n,m,a[N],b[N],qz[N];
 int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    cin>>n;
-    rb(i,1,n){
-        cin>>a[i];
-    }
-    rb(i,1,n){
-        cin>>b[i];
-    }
-    sort(a+1,a+1+n);
-    sort(b+1,b+1+n);
-    LL ans=0;
-    rb(i,1,n){
-        int l=lower_bound(b+1,b+1+n,a[i])-b;
-        ans+=1ll*(l-1)*a[i]-1ll*(n-l+1)*a[i];
-    }
-    rb(i,1,n){
-        int l=lower_bound(a+1,a+1+n,b[i]+1)-a;
-        ans+=1ll*(l-1)*b[i]-1ll*(n-l+1)*b[i];
-    }
-    LL temp=0;
-    rb(i,1,n) temp+=abs(a[i]-b[i]);
-    cout<<ans-temp*(n-1)<<endl;
-    return 0;
+	n=rd();for(LL i=1;i<=n;++i)a[i]=rd();for(LL i=1;i<=n;++i)b[i]=rd();
+	sort(a+1,a+1+n);sort(b+1,b+1+n);
+	for(LL i=1;i<=n;++i)qz[i]=qz[i-1]+b[i];
+	for(LL i=1,j=0;i<=n;++i){
+		while(j<n&&b[j+1]<=a[i])++j;
+		m+=a[i]*j-qz[j]+(qz[n]-qz[j])-a[i]*(n-j);
+	}
+	for(LL i=1;i<=n;++i)m-=abs(a[i]-b[i])*(n-1);
+	cout<<m;return 0;
 }
