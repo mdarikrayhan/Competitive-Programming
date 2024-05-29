f7eeb935cddcc23941931a1722cff77510fe0824
mdarikrayhan <mdarikrayhan@gmail.com>
2024-05-29 06:28:00 +0600

    Add solution for problem `D - Misha and Apples` Link: https://codeforces.com/contest/1819/problem/D Tags: brute force, data structures, dp, two pointers, *2800 Ref: https://codeforces.com/contest/1819/submission/263073008

--- codeforces/1819/D.cpp
+++ codeforces/1819/D.cpp
@@ -1,55 +1,95 @@
-# include <bits/stdc++.h>
+#include <bits/stdc++.h>
+ 
 using namespace std;
-int lst[200010],que[200010];
-bool flag[200010],vis[200010];
-vector<int> a[200010];
-inline void chkmax(int &x,const int &y){x=max(x,y);}
-int main()
-{
-	flag[0]=1;
-	int T,n,m;
-	cin>>T;
-	while(T--)
-	{
-		int sz,t=0,l=0,r=0,ans=0;
-		scanf("%d%d",&n,&m);
-		que[r++]=1;
-		for(int i=1;i<=n;i++)
-		{
-			scanf("%d",&sz);
-			a[i].resize(sz);
-			if(sz)
-			{
-				for(int &j:a[i])
-				{
-					scanf("%d",&j);
-					flag[i]|=(l<r && que[l]<=lst[j]);
-					while(l<r && que[l]<=lst[j]) l++;
-					lst[j]=i;
-				}
-				flag[i]|=(l<r && t>=que[l]);
-				if(flag[i]) que[r++]=i+1;
+ 
+typedef long long lo; 
+ 
+#define fi first
+#define se second
+#define endl "\n"
+#define pb push_back
+#define int long long
+#define fio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define FOR for(int i=1;i<=n;i++)
+#define mid ((start+end)/2)
+#define ort ((bas+son)/2)
+#define _ << " " <<
+ 
+const lo inf = 1000000000;
+const lo li = 500005;
+const lo mod = 1000000007;
+ 
+int n,m,a[li],k,flag,t,dp[li],dp2[li],last[li];
+int cev;
+string s;
+vector<int> v[li];
+ 
+int32_t main(void){
+	fio();
+	cin>>t;
+	while(t--){
+		cin>>n>>m;
+		for(int i=0;i<=n;i++){
+			v[i].clear();
+			dp[i]=0,dp2[i]=0;
+		}
+		last[0]=-1;
+		FOR{
+			cin>>k;
+			for(int j=1;j<=k;j++){
+				int x;
+				cin>>x;
+				v[i].pb(x);
+				last[x]=-1;
 			}
-			else t=i,flag[i]=1,que[r++]=i+1;
 		}
-		bool have0=0;
-		for(int i=n,cnt=0;i;i--)
-		{
-			bool ok=1;
-			for(int j:a[i])
-				if(vis[j]) ok=0;
-				else vis[j]=1,cnt++;
-			if(!ok) break;
-			have0|=a[i].empty();
-			if(flag[i-1]) chkmax(ans,have0?m:cnt);
+		set<int> st,oldu;
+		dp[0]=1;
+		oldu.insert(0);
+		for(int i=1;i<=n;i++){
+			int mx=last[0];
+			for(auto go:v[i]){
+				mx=max(mx,last[go]);
+				
+			}
+			if(v[i].empty()){dp[i]	=1;last[0]=i;oldu.insert(i);}
+			if(~mx){
+				auto it=oldu.lower_bound(mx);
+				if(it!=oldu.begin()){
+					it--;
+					//~ cout<<*it _ "BBB" _ i _ st.size() _ endl;
+					auto itt=st.upper_bound(*it);
+					if(itt==st.end()){
+						dp[i]=1;
+						oldu.insert(i);
+					}
+				}
+			}
+			for(auto go:v[i]){
+				if(~last[go])st.insert(last[go]);
+				last[go]=i;
+			}
 		}
-		printf("%d\n",ans);
-		for(int i=1;i<=n;i++)
-		{
-			flag[i]=0;
-			for(int j:a[i]) lst[j]=vis[j]=0;
-			a[i].clear();
+		map<int,int> mpp;
+		flag=0;
+		int tut=0,say=0;
+		for(int i=n;i>=1;i--){
+			for(auto go:v[i]){
+				say++;
+				if(mpp.find(go)!=mpp.end()){flag=1;break;}
+				mpp[go]=1;
+			}
+			if(flag)break;
+			if(v[i].empty())tut=1;
+			if(tut)dp2[i-1]=m;
+			else dp2[i-1]=say;
 		}
+		cev=dp2[0];
+		FOR{
+			//~ cout<<i _ dp[i] _ dp2[i] _ "AA"<<endl;
+			if(dp[i])cev=max(cev,dp2[i]);
+		}
+		cout<<cev<<endl;
 	}
 	return 0;
 }
